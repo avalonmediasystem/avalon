42047d73d8c0f7db8212c0ee12ae42d5d22bd744
Chris Colvard <cjcolvar@indiana.edu>
2012-04-23 23:53:34 -0400

    Fixed issues with file_objects

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,6 @@
 class Video < ActiveFedora::Base
   include Hydra::ModelMethods
+  include ActiveFedora::FileManagement
 
   has_metadata :name => "descMetadata", :type => VideoDCDatastream
   has_metadata :name => "rightsMetadata", :type => Hydra::RightsMetadata
