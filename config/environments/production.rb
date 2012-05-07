847c414814c52c862ae9d3fd5dd4f21d02a0637c
Chris Colvard <cjcolvar@indiana.edu>
2012-05-07 12:08:05 -0400

    Changed setting on asset precompilation for easy setup of production env...will probably want to change this later (VOV-112)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Hydrant::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true # FIXME Chris 20120507 -> set this temporarily
 
   # Generate digests for assets URLs
   config.assets.digest = true
