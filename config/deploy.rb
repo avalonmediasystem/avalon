fa57aca97da94a54a5c2effa782f56127de27b6e
Chris Colvard <cjcolvar@indiana.edu>
2012-05-16 11:40:40 -0400

    Adding capistrano support for deploying to pawpaw (VOV-81)

---
+++ config/deploy.rb
@@ -0,0 +1,43 @@
+set :application, "hydrant"
+set :repository,  "git://github.com/variations-on-video/hydrant.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "pawpaw.dlib.indiana.edu"                          # Your HTTP server, Apache/etc
+role :app, "pawpaw.dlib.indiana.edu"                          # This may be the same as your `Web` server
+role :db,  "pawpaw.dlib.indiana.edu", :primary => true # This is where Rails migrations will run
+
+set :deploy_to, "/srv/rails/hydrant-test"
+set :user, "vov"
+set :use_sudo, false
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
+
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+end
