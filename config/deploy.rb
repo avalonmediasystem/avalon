50b76f020cc934744f72e1ee53e0c880eaba5168
Chris Colvard <cjcolvar@indiana.edu>
2012-05-17 09:34:21 -0400

    Test commit to see if jira<->git link is working (VOV-81)

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,7 @@ set :deploy_to, "/srv/rails/hydrant-test"
 set :user, "vov"
 set :use_sudo, false
 
+
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
