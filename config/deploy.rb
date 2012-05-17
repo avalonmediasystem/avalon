4b80b7260fd3af2ddc3e05d529333b8720674aa0
Chris Colvard <cjcolvar@indiana.edu>
2012-05-17 12:33:05 -0400

    More work on getting the bamboo deploy build to work properly...added rvm-capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,10 @@ set :deploy_to, "/srv/rails/hydrant-test"
 set :user, "vov"
 set :use_sudo, false
 
+set :rvm_ruby_string, 'ruby-1.9.3@hydrant'                     # Or:
+#set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"") # Read from local system
+
+require "rvm/capistrano" 
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
