aeb4da6e0444cc0e141524dbd1823ef6bcbe9529
Chris Colvard <cjcolvar@indiana.edu>
2012-06-11 11:08:46 -0400

    Fixed tests (I hope) by using example from headless docs

--- features/support/env.rb
+++ features/support/env.rb
@@ -54,15 +54,12 @@ ActionController::Base.allow_rescue = false
 # warning: regexp match /.../n against to UTF-8 string
 $VERBOSE = nil
 
-require 'selenium-webdriver'
-
-Capybara.default_driver = :selenium
-
 Before('@javascript') do
-  if ENV["SELENIUM_HEADLESS"] == 'true'
-    require "headless"
-    @headless = Headless.new
-    @headless.start
+  if Capybara.current_driver == :selenium
+    require 'headless'
+
+    headless = Headless.new
+    headless.start
   end
 end
 
