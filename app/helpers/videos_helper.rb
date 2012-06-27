17cee174416f12630373495be4ae9391e368b419
Nathan Rogers <rogersna@indiana.edu>
2012-06-27 10:49:43 -0400

    Cleaning up the user interface to provide better information

---
+++ app/helpers/videos_helper.rb
@@ -0,0 +1,17 @@
+module VideosHelper
+	  # Creates a hot link to the downloadable file if it is available. File names longer
+	  # than 25 characters are truncated although this can be overridden by passing in a
+	  # different value
+	  def file_download_label(video_asset)
+		# Check to see if the file name is longer than 25 characters
+		if 25 > video_asset.descMetadata.title[0].length 
+		  label_display = video_asset.descMetadata.title[0]
+		else
+		  label_display = truncate(video_asset.descMetadata.title[0], length: 25)
+                  label_display << "."
+		  label_display << video_asset.descMetadata.title[0].split('.').last
+		end
+	  end
+end
+
+
