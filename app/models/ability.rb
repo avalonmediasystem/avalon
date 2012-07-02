234a0d50c015c73648cfcf27ed355bf3cae1ff85
phuongdh <phuongdh@gmail.com>
2012-07-02 10:54:21 -0400

    Added enforce_create_permisions and enforce_new_permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,4 +7,17 @@ class Ability
 			can :create, Video
 		end
 	end
+
+  def enforce_create_permissions(opts={})
+    if cannot? :create
+      flash[:notice] = "You do not have sufficient priviledges to add resources"
+      redirect_to root_path
+    else 
+      session[:viewing_context] = "create"
+    end
+  end
+  
+  def enforce_new_permissions(opts={})
+    enforce_new_permissions(opts)
+  end
 end
