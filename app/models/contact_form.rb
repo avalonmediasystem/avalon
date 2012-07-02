5126527c2a70317761bc061a82aa281d73bc7c71
phuongdh <phuongdh@gmail.com>
2012-07-02 16:20:31 -0400

    Added contact form to send email from hydrant to vovcomment@dlib.indiana.edu

---
+++ app/models/contact_form.rb
@@ -0,0 +1,15 @@
+class ContactForm < MailForm::Base
+  attribute :fullname, :validate => true
+  attribute :email, :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :comment
+	attribute :nickname, :captcha => true
+
+  def headers
+    { 
+		  :subject => "Comment for VoV",
+      :from => %("#{fullname}" <#{email}>),
+			:to => "phuongdh@gmail.com"
+			#:to => "vovcomment@dlib.indiana.edu" 
+		}
+  end
+end
