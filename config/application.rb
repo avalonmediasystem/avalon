810ec22ed4306fa6ef23f18c91e9643d6d1aa746
Chris Colvard <cjcolvar@indiana.edu>
2012-07-03 12:55:12 -0400

    Added smtp settings and changed default icons for search results

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,12 @@ module Hydrant
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => "mail-relay.iu.edu",
+  :port                 => 465,
+  :enable_starttls_auto => true  }
+
   end
 end
