b26d54b0c266a6e7f53cae8466aa617a579467f3
Nathan Rogers <rogersna@indiana.edu>
2012-07-13 15:43:02 -0400

    Made the contact form fit the Twitter Bootstrap model

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -1,12 +1,18 @@
 class ContactForm < MailForm::Base
   attribute :fullname, :validate => true
+  
+  # TODO : Use a better email validation like a Gem instead of a brittle regular
+  #        expression
   attribute :email, :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  # TODO : Validate that this is equal to the original email
+  attribute :email_confirmation
+
   attribute :comment
-	attribute :nickname, :captcha => true
+  attribute :nickname, :captcha => true
 
   def headers
     { 
-		  :subject => "Comment for VoV",
+		  :subject => "Comment from VoV system",
       :from => %("#{fullname}" <#{email}>),
 			:to => "vovcomment@dlib.indiana.edu" 
 		}
