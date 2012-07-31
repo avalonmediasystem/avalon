d493eb2c3879432e9ca9f76926a4390bb9524219
cjcolvar <cjcolvar@lancelot.dlib.indiana.edu>
2012-07-31 12:27:21 -0400

    Making rspec tests work (VOV-430)

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,16 +17,16 @@ ActiveRecord::Schema.define(:version => 20120202180831) do
     t.integer  "user_id",     :null => false
     t.string   "document_id"
     t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.string   "user_type"
   end
 
   create_table "searches", :force => true do |t|
     t.text     "query_params"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
     t.string   "user_type"
   end
 
@@ -36,21 +37,11 @@ ActiveRecord::Schema.define(:version => 20120202180831) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string   "username",   :default => "", :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
--- spec/factories/devise.rb
+++ spec/factories/devise.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
   factory :user do
-		email 'testuser@example.com'
-		password '123456'
+		username 'testuser@example.com'
+#		password '123456'
         
     # Child of :user factory, since it's in the `factory :user` block
     factory :archivist do
-      email 'archivist1@example.com'
+      username 'archivist1@example.com'
     end
   end
 end
