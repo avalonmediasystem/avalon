b71a6b1bbb073d4c1ef09aa573a31cbc956d7412
Chris Colvard <cjcolvar@indiana.edu>
2012-08-16 12:02:22 -0400

    Turned simplecov on to get coverage results from rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] = 'test'
+require 'simplecov'
+SimpleCov.start
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-require 'simplecov'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
