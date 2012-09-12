3df6138958c327a71121d373179b48d403df26ea
Chris Colvard <cjcolvar@indiana.edu>
2012-09-12 17:17:34 -0400

    Fix small issue in cas config but some problems with the ticket validation

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -198,7 +198,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
 
-  config.omniauth :cas, :host => "cas.iu.edu", :login_url => "cas/login", :service_validate_url => "cas/validate", :logout_url => "cas/logout", :ssl => TRUE
+  config.omniauth :cas, :host => "cas.iu.edu", :login_url => "/cas/login", :service_validate_url => "/cas/validate", :logout_url => "/cas/logout", :ssl => TRUE
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
