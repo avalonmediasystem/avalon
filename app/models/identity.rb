805059a5d1f640c924110640af24e4f69be54682
Michael Klein <mbklein@gmail.com>
2012-09-18 11:59:12 -0500

    Local database authentication/user creation

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,5 +1,5 @@
 class Identity < OmniAuth::Identity::Models::ActiveRecord
-  attr_accessible :email, :password_digest
+  attr_accessible :email, :password_digest, :password, :password_confirmation
   validates_uniqueness_of :email
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
 end
