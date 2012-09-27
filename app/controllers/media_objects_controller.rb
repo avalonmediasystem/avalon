8258a579b2da7967f9f9cc89dd3b7ecc204458fe
Nathan Rogers <rogersna@indiana.edu>
2012-09-27 15:09:28 -0400

    First cut of label peristence is in place though ugly

--- app/controllers/media_objects_controller.rb
+++ app/controllers/media_objects_controller.rb
@@ -71,13 +71,8 @@ class MediaObjectsController < ApplicationController
     
     case @active_step
       when 'file-upload'
-        logger.debug "<< Processing file-upload step >>"
-        if params[:parts]
-          logger.debug "<< Parts hash includes #{params[:parts]} >>"
-          params[:parts].each do |part|
-            logger.debug "<< #{inspect part} >>" 
-          end
-        end
+        logger.debug "<< PROCESSING file-upload STEP >>"
+        update_master_files(@mediaobject, params[:parts]) unless params[:parts].nil?
         
         unless @mediaobject.parts.empty?
           @mediaobject.format = @mediaobject.parts.first.media_type
@@ -118,9 +113,6 @@ class MediaObjectsController < ApplicationController
           masterFiles.each do |mf|
             mf.container = @mediaobject
             mf.save
-          end
-          unless @mediaobject.parts.empty?
-            @mediaobject.format = @mediaobject.parts.first.media_type
           end
           @mediaobject.save(validate: false)
         end
@@ -254,5 +246,33 @@ class MediaObjectsController < ApplicationController
 
     @ingest_status.save
     @ingest_status
+  end
+  
+  # Passing in an ordered array of values update the master files below a
+  # MediaObject. Accepted hash keys are
+  #
+  # remove - Set to true to delete this item
+  # label - Display label in the interface
+  # pid - Identifier for the masterFile to help with mapping
+  def update_master_files(mediaobject, files = [])
+        logger.debug "<< files => #{files} >>"
+        if not files.blank?
+          files.each do |part|
+            logger.debug "<< #{part} >>"
+            selected_part = nil
+            mediaobject.parts.each do |current_part|
+              selected_part = current_part if current_part.pid == part[:pid]
+            end
+            next unless not selected_part.blank?
+            
+            if part[:remove]
+              logger.info "<< Deleting master file #{part[:pid]} from the system >>"
+              selected_part.delete
+            else
+              selected_part.label = part[:label]
+              selected_part.save
+            end            
+          end
+        end
   end
 end
