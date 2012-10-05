d35c9754cbffde3f6879907ca524aec3670b95ff
Michael Klein <mbklein@gmail.com>
2012-10-05 16:50:20 -0500

    JSON-ify AJAX switching; isolate switch_streams logic in a closure.

--- app/assets/javascripts/switch_streams.js
+++ app/assets/javascripts/switch_streams.js
@@ -1,37 +1,40 @@
-function setActiveSection(activeSegment) {
-  $('a.stream.current').removeClass('current');
-  $(activeSegment).addClass('current');
-}
+(function() {
+    function setActiveSection(activeSegment) {
+      $('a.stream').removeClass('current');
+      $('#link_'+activeSegment).addClass('current');
+    }
 
-function setActiveTitle(title) {
-  target = $('#stream_label');
-  if (target) {
-    target.text = title;
-  }
-}
+    function setActiveLabel(title) {
+      target = $('#stream_label');
+      if (target) {
+        target.html(title);
+      }
+    }
 
-/*
- * This method should take care of the heavy lifting involved in passing a message
- * to the player
- */
-function refreshStream(stream, package_id) {
-}
+    /*
+     * This method should take care of the heavy lifting involved in passing a message
+     * to the player
+     */
+    function refreshStream(stream, package_id) {
+    }
 
-$(document).ready(function() {
-    $('a.stream').click(function(event) {
-        event.preventDefault();
-        var target = $(this);
-        /*
-         * Do three things to update a stream
-         *
-         * 1) Replace the H2 container's text with the new section name
-         * 2) Set the active stream
-         * 3) Refresh the stream by passing a message to the player
-         */
-        $.get("" + document.location.pathname + "?content=" + (target.data('segmentId')), function(data) {
-            setActiveLabel(data('title'));
-            setActiveSection(data('segmentId'));
-            refreshStream(data('stream'), data('package_id'));
+    $(document).ready(function() {
+        $('a.stream').click(function(event) {
+            event.preventDefault();
+            var target = $(this);
+            /*
+             * Do three things to update a stream
+             *
+             * 1) Replace the H2 container's text with the new section name
+             * 2) Set the active stream
+             * 3) Refresh the stream by passing a message to the player
+             */
+            var uri = target.attr('href').split('?')
+            $.getJSON(uri[0], uri[1], function(data) {
+                setActiveLabel(data.label);
+                setActiveSection(data.mediapackage_id);
+                refreshStream(data.stream, data.mediapackage_id);
+            });
         });
     });
-});
+}());
