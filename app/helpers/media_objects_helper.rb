57782b753ff9c406e4292f084cde02a66de3a8a9
Michael Klein <mbklein@gmail.com>
2012-10-17 15:28:37 -0500

    Move MediaObjectsController#show code/views/helpers into CatalogController

--- app/helpers/media_objects_helper.rb
+++ app/helpers/media_objects_helper.rb
@@ -1,53 +1,4 @@
 module MediaObjectsHelper
-	  # Creates a hot link to the downloadable file if it is available. File names longer
-	  # than 25 characters are truncated although this can be overridden by passing in a
-	  # different value
-	  def file_download_label(masterfile)
-		# Check to see if the file name is longer than 25 characters
-		if 20 > masterfile.descMetadata.title[0].length 
-		  label_display = masterfile.descMetadata.title[0]
-		else
-		  label_display = truncate(masterfile.descMetadata.title[0], length: 15)
-                  label_display << "."
-		  label_display << masterfile.descMetadata.title[0].split('.').last
-		end
-	  end
-	  
-	  # Not the best way to do this but it works for the time being
-	  def wrap_text(content)
-	    unless content.nil? or content.empty?
-	      content.gsub(/\n/, '<br />').html_safe
-	    else
-	      "<em>Not provided</em>".html_safe
-	    end
-	  end
-
-    def display_multiple(value, delim='; ')
-      value.select { |v| not (v.nil? or v.strip.empty?) }.join(delim)
-    end
-
-	  def search_result_label(item)
-	     label = ''
-	     unless item["title_t"].nil? or item["title_t"].empty?
-	       label << truncate(item["title_t"].first, length: 35)
-	     else
-	       label << item.id
-	     end
-	     
-	     label
-	  end
-
-      # Retrieve the current status of processing and display a concise version
-      # for use in the interface
-      def conversion_status_for(mediaobject)
-        unless mediaobject.parts.empty?
-          masterfile = mediaobject.parts.first.pid
-          masterfile.status
-        else
-          "No files have been selected"
-        end
-      end	  
-      
       # Quick and dirty solution to the problem of displaying the right template.
       # Quick and dirty also gets it done faster.
       def current_step_for(status=nil)
@@ -75,17 +26,6 @@ module MediaObjectsHelper
         HYDRANT_STEPS.previous(status)
       end  
       
-      def stream_label_for(resource)
-        label = ''
-        unless resource.nil?
-          if resource.label.blank?
-            label = File.basename(resource.descMetadata.identifier[0])
-          else
-            label = resource.label
-          end
-        end
-        return label
-      end
 end
 
 
