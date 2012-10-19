b27095b28104d271cdd13ba4471fc07fc8904b86
Chris Colvard <cjcolvar@indiana.edu>
2012-10-19 01:00:05 -0400

    Don't force to edit mode if authorized to edit

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -10,7 +10,7 @@ class CatalogController < ApplicationController
 
   # These before_filters apply the hydra access controls
   before_filter :enforce_access_controls
-  before_filter :enforce_viewing_context_for_show_requests, :only=>:show
+#  before_filter :enforce_viewing_context_for_show_requests, :only=>:show
   # This applies appropriate access controls to all solr queries
   solr_search_params_logic << :add_access_controls_to_solr_params
   # This filters out objects that you want to exclude from search results, like FileAssets
