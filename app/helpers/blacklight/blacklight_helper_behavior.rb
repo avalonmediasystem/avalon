e96415dec22444bc23493e1f00913026be1b966a
Phuong Dinh <phuongdh@140-182-233-222.dhcp-bl.indiana.edu>
2012-10-25 12:35:52 -0400

    Removed bookmarks, align metadata of search results to the right

---
+++ app/helpers/blacklight/blacklight_helper_behavior.rb
@@ -0,0 +1,10 @@
+module Blacklight::BlacklightHelperBehavior
+
+  def render_index_doc_actions(document, options={})   
+    wrapping_class = options.delete(:wrapping_class) || "documentFunctions" 
+
+    content = []
+    content_tag("div", content.join("\n").html_safe, :class=> wrapping_class)
+  end
+
+end
