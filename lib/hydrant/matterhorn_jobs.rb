769b01b5a67b98e65796839b46b2214bf1a4935d
Phuong Dinh <phuongdh@gmail.com>
2012-11-12 13:04:39 -0500

    Prevent duplicate calls to Matterhorn when one times out

--- lib/hydrant/matterhorn_jobs.rb
+++ lib/hydrant/matterhorn_jobs.rb
@@ -1,8 +1,10 @@
 class MatterhornJobs
   def send_request args
     logger.debug "<< Calling Matterhorn with arguments: #{args} >>"
-    logger.debug Rubyhorn.client.methods.inspect
     workflow_doc = Rubyhorn.client.addMediaPackageWithUrl(args)
+    rescue Timeout::Error => e
+      logger.debug "<< Call to Matterhorn has timed out, but it's ok >>"
+    end
   end
   handle_asynchronously :send_request
 end
