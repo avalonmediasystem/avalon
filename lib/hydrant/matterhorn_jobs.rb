5c37b5494cf7f0e20a46375ed99ac82a17f7e3b9
Phuong Dinh <phuongdh@gmail.com>
2012-11-12 13:20:48 -0500

    Fixed syntax error

--- lib/hydrant/matterhorn_jobs.rb
+++ lib/hydrant/matterhorn_jobs.rb
@@ -4,7 +4,6 @@ class MatterhornJobs
     workflow_doc = Rubyhorn.client.addMediaPackageWithUrl(args)
     rescue Timeout::Error => e
       logger.debug "<< Call to Matterhorn has timed out, but it's ok >>"
-    end
   end
   handle_asynchronously :send_request
 end
