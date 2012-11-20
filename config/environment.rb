ed91310084cf70a03fa101bc337647c9720e5b18
Nathan Rogers <rogersna@indiana.edu>
2012-11-20 10:16:23 -0500

    Tests should no longer run in development environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
-ENV['RAILS_ENV'] = 'development'
+ENV['RAILS_ENV'] ||= 'development'
 
 # Load the rails application
 require File.expand_path('../application', __FILE__)
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Hydrant::Application.routes.draw do
 
   root :to => "mediaObjects#index"
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do 
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :users do 
     match '/users/sign_in', :to => "users/sessions#new", :as => :new_user_session
     match '/users/sign_out', :to => "users/sessions#destroy", :as => :destroy_user_session
   end
