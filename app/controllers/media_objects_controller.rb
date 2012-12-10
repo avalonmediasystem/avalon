97baa447fab368960cc7efd1d3ce6f160e8e84b2
Michael Klein <mbklein@gmail.com>
2012-12-10 13:05:45 -0600

    Make sure all streams get tokenized

--- app/controllers/media_objects_controller.rb
+++ app/controllers/media_objects_controller.rb
@@ -22,12 +22,13 @@ class MediaObjectsController < CatalogController
     @masterFiles = load_master_files
 
     if 'preview' == @active_step 
-      @currentStream = set_active_file(params[:content])
-      if (not @masterFiles.blank? and @currentStream.blank?) then
-        @currentStream = @masterFiles.first
-        flash[:notice] = "The stream was not recognized. Defaulting to the first available stream for the resource"
-      end
-    end 
+        @currentStream = set_active_file(params[:content])
+        if (not @masterFiles.blank? and @currentStream.blank?) then
+          @currentStream = @masterFiles.first
+          flash[:notice] = "The stream was not recognized. Defaulting to the first available stream for the resource"
+        end
+        @token = StreamToken.find_or_create_session_token(session, @currentStream.mediapackage_id)
+    end
   end
 
   def show
