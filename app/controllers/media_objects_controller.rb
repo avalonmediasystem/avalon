230f8405b94fcc3003e80b40c3f97a89d1b62c75
Phuong Dinh <phuongdh@gmail.com>
2012-12-13 13:03:47 -0500

    Fixed stream_detail being null

--- app/controllers/media_objects_controller.rb
+++ app/controllers/media_objects_controller.rb
@@ -37,7 +37,7 @@ class MediaObjectsController < CatalogController
     @masterFiles = load_master_files
     @currentStream = params[:content] ? set_active_file(params[:content]) : @masterFiles.first
     @token = @currentStream.nil? ? "" : StreamToken.find_or_create_session_token(session, @currentStream.mediapackage_id)
-    @currentStreamInfo = @currentStream.derivatives.first.stream_details(@token) rescue nil
+    @currentStreamInfo = @currentStream.derivatives.first.stream_details(@token) rescue {}
 
     respond_to do |format|
       # The flash notice is only set if you are returning HTML since it makes no
