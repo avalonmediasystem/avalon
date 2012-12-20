40d4191c0431ee03dc6773a70c9a173afe39f10b
Adam Hallett <adam.t.hallett@gmail.com>
2012-12-20 15:30:37 -0600

    adding email_sent field

--- db/migrate/20121219173255_create_table_ingest_batch.rb
+++ db/migrate/20121219173255_create_table_ingest_batch.rb
@@ -2,9 +2,10 @@ class CreateTableIngestBatch < ActiveRecord::Migration
 
   def change
     create_table :ingest_batches do |t|
-      t.text :media_object_ids
       t.string :email
+      t.text :media_object_ids
       t.boolean :finished, :default => false
+      t.boolean :email_sent, :default => false
       t.timestamps
     end
   end
