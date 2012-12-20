5940cd2ba368a719202ef9aade8e88e6b9dbf807
Adam Hallett <adam.t.hallett@gmail.com>
2012-12-20 15:31:14 -0600

    more tests

--- spec/models/ingest_batch_spec.rb
+++ spec/models/ingest_batch_spec.rb
@@ -20,20 +20,44 @@ describe IngestBatch do
     it 'returns false when one or more master files are not finished' do
       media_object = MediaObject.new(pid:'changeme:1')
 
-      media_object.parts << MasterFile.create(status_code: ['STOPPED'])
+      media_object.add_relationship(:has_part, MasterFile.new(status_code: ['STOPPED']))
       media_object.parts << MasterFile.create(status_code: ['RUNNING'])
 
       media_object.save(validate: false)
+
+
 
       ingest_batch = IngestBatch.new(media_object_ids: ['changeme:1'], email: 'email@something.com')
       ingest_batch.finished?.should be_false
     end
   end
 
-  describe "#media_objects" do
+  describe '#media_objects' do
     it 'returns an empty array if media_object_ids is nil' do
       n = IngestBatch.new
       n.media_objects.should == []
+    end
+
+    it 'returns an array of media objects based on ids passed in' do
+      media_objects = (0..10).map{
+          media_object = MediaObject.new
+          media_object.save(validate: false)
+          media_object 
+        }
+      ingest_batch = IngestBatch.new(media_object_ids:media_objects.map(&:id))
+      ingest_batch.media_objects.should == media_objects
+    end
+  end
+
+  describe '#email_sent?' do
+    let(:ingest_batch){ IngestBatch.new }
+    it 'returns true if email_sent is true' do
+      ingest_batch.email_sent = true
+      ingest_batch.email_sent?.should be_true
+    end
+    it 'returns false if email_sent is false' do
+      ingest_batch.email_sent = false
+      ingest_batch.email_sent?.should be_false
     end
   end
 end
