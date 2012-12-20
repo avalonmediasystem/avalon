8d541076a89f2449fad2c445f95ff14a67fba458
Nathan Rogers <rogersna@indiana.edu>
2012-12-20 12:30:48 -0500

    Placeholder for permissions and publish steps

--- lib/hydrant/batch_ingest.rb
+++ lib/hydrant/batch_ingest.rb
@@ -1,6 +1,8 @@
 module Hydrant
   module Batch
 
+    include Hydrant::Controller::ControllerBehavior
+
     def self.ingest
       # Scans dropbox for new batch packages
       logger.debug "============================================"
@@ -15,20 +17,20 @@ module Hydrant
 
         package.process do |fields, files|
           # Creates and processes MasterFiles
-          mediaobject = MediaObject.new(avalon_uploader: 'batch')
+          mediaobject = initialize_media_object('batch')
           mediaobject.workflow.origin = "batch"
-          mediaobject.access = "restricted"
-          mediaobject.edit_groups = ["archivist"]
           mediaobject.save(:validate => false)
           logger.debug "<< Created MediaObject #{mediaobject.pid} >>"
 
+          # Simluate the uploading of the files using the workflow step so that
+          # changes only have to be made in one place. This may mean some
+          # refactoring of the master_file_controller class eventually.
           files.each do |file_path|
-            logger.debug file_path.inspect
             mf = MasterFile.new
             mf.container = mediaobject
             mf.setContent(File.open(file_path, 'rb'))
             if mf.save
-              logger.debug "<< Created & associated MasterFile #{mf.pid} >>"
+              logger.debug "<< Created and associated MasterFile #{mf.pid} >>"
               mf.process
             end
           end
@@ -39,6 +41,13 @@ module Hydrant
           context = {mediaobject: mediaobject, media_object: fields}
           context = HYDRANT_STEPS.get_step('resource-description').execute context
 
+          # Here we need to skip the structure step and go straight to the
+          # permissions. Structure is implicit from the order the files were
+          # listed in the batch manifest
+          #
+          # Afterwards, if the auto-publish flag is true then publish the
+          # media objects. In either case here is where the notifications
+          # should take place
           if mediaobject.save
             logger.debug "Done processing package #{index}"
           else 
