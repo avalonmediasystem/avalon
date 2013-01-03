07d69cab774b4986fd88e7d3dcc69a9c869be759
Chris Colvard <cjcolvar@indiana.edu>
2013-01-03 10:39:39 -0500

    Updated Gemfile.lock and second pass at schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -75,14 +75,13 @@ ActiveRecord::Schema.define(:version => 20121219173255) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "username",             :default => "", :null => false
-    t.datetime "created_at",                           :null => false
-    t.datetime "updated_at",                           :null => false
+    t.string   "username",   :default => "", :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
     t.string   "provider"
     t.string   "uid"
     t.string   "email"
     t.string   "guest"
-    t.string   "authentication_token"
   end
 
   add_index "users", ["username"], :name => "index_users_on_username", :unique => true
