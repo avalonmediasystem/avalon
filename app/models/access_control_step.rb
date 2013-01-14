1ac310819e09c05fe7a522bfdf463f98b46aa04f
Chris Colvard <cjcolvar@indiana.edu>
2013-01-14 11:52:07 -0500

    VOV-996: user/group additions and deletions should be singular instead of bulk

--- app/models/access_control_step.rb
+++ app/models/access_control_step.rb
@@ -10,10 +10,18 @@
 		  mediaobject = context[:mediaobject]
 	          # TO DO: Implement me
         	  logger.debug "<< Access flag = #{context[:access]} >>"
-              	  mediaobject.access = context[:access]        
+              	  mediaobject.access = context[:access] unless context[:access].blank? 
 
-		  mediaobject.groups = context[:groups]
-		  mediaobject.users = context[:users]
+		  groups = mediaobject.groups
+                  groups << context[:add_group] unless context[:add_group].blank?
+                  groups.delete context[:del_group] unless context[:del_group].blank?
+                  mediaobject.groups = groups
+
+		  users = mediaobject.users
+                  users << context[:add_user] unless context[:add_user].blank?
+                  users.delete context[:del_user] unless context[:del_user].blank?
+                  mediaobject.users = users
+
         
 	          mediaobject.save
         	  logger.debug "<< Groups : #{mediaobject.read_groups} >>"
