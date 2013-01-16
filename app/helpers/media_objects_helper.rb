8dd2e984c776f0d02aa8169501e1a71c6c5ce1f3
Nathan Rogers <rogersna@indiana.edu>
2013-01-16 16:54:52 -0500

    Ingest workflow steps should be much more low key now

--- app/helpers/media_objects_helper.rb
+++ app/helpers/media_objects_helper.rb
@@ -2,9 +2,6 @@ module MediaObjectsHelper
       # Quick and dirty solution to the problem of displaying the right template.
       # Quick and dirty also gets it done faster.
       def current_step_for(status=nil)
-        logger.debug "<< CURRENT_STEP_FOR >>"
-        logger.debug "<< STEP => #{status} >>"
-        
         if status.nil?
           status = HYDRANT_STEPS.first
         end
@@ -25,7 +22,27 @@ module MediaObjectsHelper
         end
         HYDRANT_STEPS.previous(status)
       end  
-      
+     
+      # Based on the current context it will choose which class should be
+      # applied to the display. If you are not using Twitter Bootstrap or
+      # want different defaults then change them here.
+      #
+      # The context here is the media_object you are working with.
+      def class_for_step(context, step)  
+        logger.debug "<< Current step test is #{step} >>"
+        logger.debug "<< Current? #{context.workflow.current?(step)} >>"
+        logger.debug "<< Completed? #{context.workflow.completed?(step)} >>"
+
+        css_class = case 
+          when context.workflow.current?(step)
+            'nav-info'
+          when context.workflow.completed?(step)
+            'nav-success'
+          else 'nav-disabled' 
+          end
+
+        css_class
+     end
 end
 
 
