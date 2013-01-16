d372fab0efe78e17467437ae822a54d399dda691
Michael Klein <mbklein@gmail.com>
2013-01-15 21:40:22 -0600

    Override UnsavedDigitalObject#assign_pid to allow for a non-default pid namespace

---
+++ config/initializers/assign_pid.rb
@@ -0,0 +1,11 @@
+module ActiveFedora
+  class UnsavedDigitalObject
+    def assign_pid
+    	pid_opts = {}
+    	if Hydrant::Configuration['fedora'] && ns = Hydrant::Configuration['fedora']['namespace']
+    		pid_opts[:namespace] = ns
+    	end
+      @pid ||= Nokogiri::XML(ActiveFedora::Base.connection_for_pid(0).next_pid(pid_opts)).at_xpath('//xmlns:pid').text
+    end
+  end
+end
