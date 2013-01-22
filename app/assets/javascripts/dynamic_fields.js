0c06bb6dd5ddae4df6455be4a1414748c588d736
Nathan Rogers <rogersna@indiana.edu>
2013-01-22 15:29:04 -0500

    Halfway to a new solution for adding fields

--- app/assets/javascripts/dynamic_fields.js
+++ app/assets/javascripts/dynamic_fields.js
@@ -2,11 +2,10 @@
     initialize: function() {
       /* Any fields marked with the class 'dynamic_field' will have an add button appended
        * to the DOM after the label */	
+      this.add_controls_to_label();
       this.add_remove_buttons_to_dynamic_fields();
 
       $(document).on('click', '.add-dynamic-field', function(event){
-        event.preventDefault();
-
         // find all the input controls after the add button
         var fields = $(this).next().find('.controls');
         var inputs = $(fields).find('input');
@@ -20,36 +19,46 @@
             attr('value','').
             after(DynamicFields.remove_button_html)
         );
+        
+        if (2 == inputs.length) {
+          /* Do stuff */
+        }
       });
 
       $(document).on('click', '.remove-dynamic-field', function(event){
-        event.preventDefault();
-
-	/*
-	 * For efficiency's sake cache $(this) instead of making repeated calls against
-	 * the DOM
-	 */
-	node = $(this)
+	    /*
+	     * For efficiency cache $(this) instead of making repeated calls
+	     * against the DOM
+	     */
+	    node = $(this)
         input_fields_in_control_group = node.parent('.controls').find('input');
-        // only remove the input field if there is another input field in the list      
         if (input_fields_in_control_group.length > 1) {
-          //remove input field
           node.prev().remove();
-          //remove remove icon
           node.remove();
         }
 
-	/*
+	    /*
 	 * If there is only one item then hide the controls since you can't actually
 	 * click the button in that case */
-       if (1 == input_fields_in_control_group.length) {
+          if (1 == input_fields_in_control_group.length) {
          node.hide();
        }
       });
     },
 
+    add_controls_to_label: function() {
+      $('.controls[data-dynamic="true"]').each(function() {
+	label = $(this).parent().children('label').append(this.add_button_html);
+      });
+    },
+
+    /* This probably does not work with the new markup */
     add_remove_buttons_to_dynamic_fields: function(){
-      $('.dynamic_field').after(this.remove_button_html);
+      $('.controls[data-dynamic="true"]').each(function() {
+	$(this).parent().children('input').each(function(node) {
+	  $(node).append(this.remove_button_htmli);
+	})
+      });
     },
 
     copy_and_increment_rails_collection_id: function( current_id ) {
@@ -59,6 +68,7 @@
       return string_portion_of_id + String(integer_portion_of_id + 1);
     },
 
-    remove_button_html: '<a href="#" class="remove-dynamic-field"><i class="icon-remove-sign"></i></a>'
+    add_button_html: '<span class="add-dynamic-field"><i class="icon-plus-sign"></i></span>',
+    remove_button_html: '<span class="remove-dynamic-field"><i class="icon-remove-sign"></i></span>'
 
   }
