ebcc72b16838d3cca28bb7b6760862ed7a2c9694
Nathan Rogers <rogersna@indiana.edu>
2013-01-22 13:05:52 -0500

    Wrapping metadata scripts in a content_for block so they do not render inline

--- app/assets/javascripts/dynamic_fields.js
+++ app/assets/javascripts/dynamic_fields.js
@@ -1,7 +1,7 @@
   window.DynamicFields = {
-
-    initialize: function(){
-      
+    initialize: function() {
+      /* Any fields marked with the class 'dynamic_field' will have an add button appended
+       * to the DOM after the label */	
       this.add_remove_buttons_to_dynamic_fields();
 
       $(document).on('click', '.add-dynamic-field', function(event){
@@ -20,24 +20,31 @@
             attr('value','').
             after(DynamicFields.remove_button_html)
         );
-
       });
 
       $(document).on('click', '.remove-dynamic-field', function(event){
-        
         event.preventDefault();
 
-        input_fields_in_control_group = $(this).parent('.controls').find('input');
-
+	/*
+	 * For efficiency's sake cache $(this) instead of making repeated calls against
+	 * the DOM
+	 */
+	node = $(this)
+        input_fields_in_control_group = node.parent('.controls').find('input');
         // only remove the input field if there is another input field in the list      
-        if ( input_fields_in_control_group.length > 1 ) {
-          
+        if (input_fields_in_control_group.length > 1) {
           //remove input field
-          $(this).prev().remove();
-          
+          node.prev().remove();
           //remove remove icon
-          $(this).remove();
+          node.remove();
         }
+
+	/*
+	 * If there is only one item then hide the controls since you can't actually
+	 * click the button in that case */
+       if (1 == input_fields_in_control_group.length) {
+         node.hide();
+       }
       });
     },
 
@@ -52,6 +59,6 @@
       return string_portion_of_id + String(integer_portion_of_id + 1);
     },
 
-    remove_button_html: '<a href="#" class="remove-dynamic-field"><i class="icon-remove"></i></a>'
+    remove_button_html: '<a href="#" class="remove-dynamic-field"><i class="icon-remove-sign"></i></a>'
 
   }
