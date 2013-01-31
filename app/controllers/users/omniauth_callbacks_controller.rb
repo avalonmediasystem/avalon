def57cadb49cbcdb70214ce74783414cdaedcd35
Michael Klein <mbklein@gmail.com>
2013-01-31 15:41:43 -0600

    Fix login failure redirect path

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,8 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def after_omniauth_failure_path_for(scope)
+    new_user_session_path(scope)
+  end
+
   def cas
     find_user('CAS')
   end
