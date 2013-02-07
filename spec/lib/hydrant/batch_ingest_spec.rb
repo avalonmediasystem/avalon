1270c5f47f20dc5b0beb145529743931367bd0e0
Nathan Rogers <rogersna@indiana.edu>
2013-02-07 15:56:58 -0500

    Cleaning up more spec tests to further reduce noise when running tests

--- spec/lib/hydrant/batch_ingest_spec.rb
+++ spec/lib/hydrant/batch_ingest_spec.rb
@@ -3,19 +3,27 @@ require 'hydrant/dropbox'
 require 'hydrant/batch_ingest'
 
 describe Hydrant::Batch do
-  
-  before do
+  # Get rid of all .processing stubs left over in case something was aborted
+  # mid test
+  before :all do
+    # TODO : Locate all .processing files and delete them from the spec
+    #        dropbox    
+  end
+
+  before :each do
+    Hydrant::DropboxService = Hydrant::Dropbox.new 'spec/fixtures/dropbox'
+  end
+
+  after :each do
     # this file is created to signify that the file has been processed
     # we need to remove it so can re-run the tests
     processed_status_file = 'spec/fixtures/dropbox/example_batch_ingest/batch_manifest.xlsx.processed'
     File.delete( processed_status_file ) if File.exists?( processed_status_file)
     
-    Hydrant::DropboxService = Hydrant::Dropbox.new 'spec/fixtures/dropbox'
-
     # this is a test environment, we don't want to kick off
     # generation jobs if possible
     MasterFile.any_instance.stub(:save).and_return( true )
-  end
+   end
 
   it 'creates an ingest batch object' do
     Hydrant::Batch.ingest
