849bebe50f55a7d9952a8b5d625938fac6a0aee5
Phuong Dinh <phuongdh@gmail.com>
2013-02-20 10:52:39 -0500

    Fixed parts_with_order not showing up

--- app/controllers/master_files_controller.rb
+++ app/controllers/master_files_controller.rb
@@ -42,7 +42,6 @@ class MasterFilesController < ApplicationController
 
         master_file = MasterFile.new
         master_file.mediaobject = media_object
-        media_object.parts_with_order += [master_file]
         master_file.setContent(file)
         MasterFilesController.set_default_item_permissions(master_file, user_key)
  
@@ -61,7 +60,8 @@ class MasterFilesController < ApplicationController
         unless master_file.save
           flash[:errors] = "There was a problem storing the file"
         else
-          media_object.save
+          media_object.parts_with_order += [master_file]
+          media_object.save(validate: false)
           master_file.process
           @master_files << master_file
         end
@@ -73,14 +73,14 @@ class MasterFilesController < ApplicationController
         file_path = Hydrant::DropboxService.find(file[:id])
         master_file = MasterFile.new
         master_file.mediaobject = media_object
-        media_object.parts_with_order += [master_file]
         master_file.setContent(File.open(file_path, 'rb'))
         MasterFilesController.set_default_item_permissions(master_file, user_key)
         
         unless master_file.save
           flash[:errors] = "There was a problem storing the file"
         else
-          media_object.save
+          media_object.parts_with_order += [master_file]
+          media_object.save(validate: false)
           master_file.process
           @master_files << master_file
         end
