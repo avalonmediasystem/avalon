7fc2cc28fd68d171cab849401792fb54081e60ef
Michael Klein <mbklein@gmail.com>
2013-02-21 12:02:27 -0600

    Provide reasonable defaults and top-level hashes in configuration hash

--- config/initializers/hydrant.rb
+++ config/initializers/hydrant.rb
@@ -6,8 +6,16 @@
 require 'hydrant/dropbox'
 
 module Hydrant
+  DEFAULT_CONFIGURATION = {
+    "dropbox"=>{},
+    "matterhorn"=>{},
+    "mediainfo"=>{"path"=>"/usr/local/bin/mediainfo"},
+    "email"=>{},
+    "security"=>{"stream_token_ttl"=>20}
+   }
+
   env = ENV['RAILS_ENV'] || 'development'
-  Configuration = YAML::load(File.read(Rails.root.join('config', 'hydrant.yml')))[env]
+  Configuration = DEFAULT_CONFIGURATION.merge(YAML::load(File.read(Rails.root.join('config', 'hydrant.yml')))[env])
   ['dropbox','matterhorn','mediainfo','email'].each { |key| Configuration[key] ||= {} }
   DropboxService = Dropbox.new Hydrant::Configuration['dropbox']['path']
   begin
@@ -20,33 +28,33 @@ module Hydrant
   end
 
   def self.matterhorn_config
-  	mh_server = Configuration['matterhorn']['root'].sub(%r{/+$},'')
-		{
-			"plugin_urls"=>{
-				"analytics"=>"#{mh_server}/usertracking/footprint.xml",
-				"annotation"=>"#{mh_server}/annotation/annotations.json",
-				"description"=>{
-					"episode"=>"#{mh_server}/search/episode.json",
-					"stats"=>"#{mh_server}/usertracking/stats.json",
-				},
-				"search"=>"#{mh_server}/search/episode.json",
-				"segments_text"=>"#{mh_server}/search/episode.json",
-				"segments_ui"=>"#{mh_server}/search/episode.json",
-				"segments"=>"#{mh_server}/search/episode.json",
-				"series"=>{
-					"series"=>"#{mh_server}/search/series.json",
-					"episode"=>"#{mh_server}/search/episode.json"
-				}
-			},
-			"mediaDebugInfo"=>{
-				"mediaPackageId"=>"",
-				"mediaUrlOne"=>"",
-				"mediaUrlTwo"=>"",
-				"mediaResolutionOne"=>"",
-				"mediaResolutionTwo"=>"",
-				"mimetypeOne"=>"",
-				"mimetypeTwo"=>""
-			}
-		}
+    mh_server = Configuration['matterhorn']['root'].sub(%r{/+$},'')
+    {
+      "plugin_urls"=>{
+        "analytics"=>"#{mh_server}/usertracking/footprint.xml",
+        "annotation"=>"#{mh_server}/annotation/annotations.json",
+        "description"=>{
+          "episode"=>"#{mh_server}/search/episode.json",
+          "stats"=>"#{mh_server}/usertracking/stats.json",
+        },
+        "search"=>"#{mh_server}/search/episode.json",
+        "segments_text"=>"#{mh_server}/search/episode.json",
+        "segments_ui"=>"#{mh_server}/search/episode.json",
+        "segments"=>"#{mh_server}/search/episode.json",
+        "series"=>{
+          "series"=>"#{mh_server}/search/series.json",
+          "episode"=>"#{mh_server}/search/episode.json"
+        }
+      },
+      "mediaDebugInfo"=>{
+        "mediaPackageId"=>"",
+        "mediaUrlOne"=>"",
+        "mediaUrlTwo"=>"",
+        "mediaResolutionOne"=>"",
+        "mediaResolutionTwo"=>"",
+        "mimetypeOne"=>"",
+        "mimetypeTwo"=>""
+      }
+    }
   end
 end
