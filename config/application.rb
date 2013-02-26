7372b3f9b16eaab27181ad8cccca8a58e7fd8b37
Nathan Rogers <rogersna@indiana.edu>
2013-02-26 11:39:32 -0500

    Cleaning up some repetition in the initializer

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require 'bootstrap-sass'
-require 'bootstrap-sass'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -55,8 +54,8 @@ module Avalon
 
     # Enable the asset pipeline
     config.assets.enabled = true
-	  config.assets.logger = false 
-		config.assets.debug = false
+    config.assets.logger = false 
+    config.assets.debug = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
