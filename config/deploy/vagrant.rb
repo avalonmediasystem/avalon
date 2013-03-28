09edcc9ccf95947d9c43fc174b01f57395c307e3
Michael Klein <mbklein@gmail.com>
2013-03-28 16:03:26 -0500

    Switch vagrant deploy to release branch

--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -2,7 +2,7 @@ set(:rails_env) { "production" }
 set(:deployment_host) { "localhost" }  # Host(s) to deploy to
 set(:deploy_to) { "/var/www/avalon" }  # Directory to deploy into
 set(:user) { 'avalon' }                # User to deploy as
-set(:branch) { "bugfix/vov-1397" }     # Git branch to deploy
+set(:branch) { "release/1.0.0" }       # Git branch to deploy
 ssh_options[:keys] = ["/vagrant/files/deployment_key"]
 
 set :bundle_without, [:development,:test]
