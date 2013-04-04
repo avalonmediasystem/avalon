a47a0028ed3f7f6e07d343175a40cdbbfeb341ce
Michael Klein <mbklein@gmail.com>
2013-04-04 12:07:54 -0500

    Fix location/permissions on puppet deployment key

--- config/deploy/puppet.rb
+++ config/deploy/puppet.rb
@@ -3,7 +3,7 @@ set(:deployment_host) { "localhost" }  # Host(s) to deploy to
 set(:deploy_to) { "/var/www/avalon" }  # Directory to deploy into
 set(:user) { 'avalon' }                # User to deploy as
 set(:branch) { "release/1.0.0" }       # Git branch to deploy
-ssh_options[:keys] = ["/etc/puppet/files/deployment_key"]
+ssh_options[:keys] = ["/opt/staging/avalon/deployment_key"]
 
 set :bundle_without, [:development,:test]
 
