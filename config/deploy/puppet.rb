e9c8a610706054ac878cfc1b3d823060c3666537
Michael Klein <mbklein@gmail.com>
2013-04-26 16:40:38 -0500

    Deploy via puppet from a configurable source branch

--- config/deploy/puppet.rb
+++ config/deploy/puppet.rb
@@ -1,8 +1,8 @@
 set(:rails_env, ENV['RAILS_ENV'] || "production")
-set(:deployment_host) { "localhost" }  # Host(s) to deploy to
-set(:deploy_to) { "/var/www/avalon" }  # Directory to deploy into
-set(:user) { 'avalon' }                # User to deploy as
-set(:branch) { "release/1.0.0" }       # Git branch to deploy
+set(:deployment_host) { "localhost" }          # Host(s) to deploy to
+set(:deploy_to) { "/var/www/avalon" }          # Directory to deploy into
+set(:user) { 'avalon' }                        # User to deploy as
+set(:branch, ENV['AVALON_BRANCH'] || "master") # Git branch to deploy
 ssh_options[:keys] = ["/opt/staging/avalon/deployment_key"]
 
 set :bundle_without, [:development,:test] if rails_env == 'production'
