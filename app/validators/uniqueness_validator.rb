c048b10f2ff1b200c13a0623595a0bb170b82fb8
Chris Colvard <cjcolvar@indiana.edu>
2013-06-12 13:57:00 -0400

    Validate presence and uniqueness

--- app/validators/uniqueness_validator.rb
+++ app/validators/uniqueness_validator.rb
@@ -1,11 +1,17 @@
 class UniquenessValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless options[:solr_name].present? 
+    unless options[:solr_name].present?
       raise ArgumentError, "UniquenessValidator was not passed :solr_name. Example: validates :uniqueness => { :solr_name => 'name_t' }"
     end
-
-    if ! record.persisted? && ! value.empty? && record.class.where( options[:solr_name] => value ).to_a.count > 0
-      record.errors.add attribute, 'Must be unique'
+    @solr_field = options[:solr_name]
+    klass = record.class
+    existing_doc = find_doc(klass, value)
+    
+    if ! existing_doc.nil?
+      record.errors.add attribute, :taken, value: value
     end
+  end
+  def find_doc(klass, value)
+    klass.where(@solr_field => value).first
   end
 end
--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -1,5 +1,6 @@
  require 'spec_helper'
 
  describe Unit do
-   it {should validate_presence_of :name}
+   it {should validate_presence_of(:name)}
+   it {should validate_uniqueness_of(:name)}
  end
