e46fce4c43bd59990463a3eefe4eec7e5d4ecfc0
Adam Hallett <adam.t.hallett@gmail.com>
2013-06-13 22:49:15 -0500

    farewell activerecord

--- db/migrate/20130524192849_create_unit_users_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateUnitUsersJoinTable < ActiveRecord::Migration
-  def change
-    create_table 'units_users', :id => false do |t|
-      t.column 'unit_id', :integer
-      t.column 'user_id', :integer
-    end
-  end
-end
