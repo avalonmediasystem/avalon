b61494a803c33d3568d64560fb1d1e0dabe0bcb6
Michael Klein <mbklein@gmail.com>
2013-07-17 11:14:25 -0500

    Fix RightsMetadata inheritance/load order issues

---
+++ app/models/rights_metadata_exceptions.rb
@@ -0,0 +1,27 @@
+module RightsMetadataExceptions
+  extend ActiveSupport::Concern
+
+  included do
+    extend_terminology do |t|
+      t.root(:path=>"rightsMetadata", :xmlns=>"http://hydra-collab.stanford.edu/schemas/rightsMetadata/v1", :schema=>"http://github.com/projecthydra/schemas/tree/v1/rightsMetadata.xsd")
+      t.exceptions_access(:ref=>[:access], :attributes=>{:type=>"exceptions"})
+    end
+  end
+
+  # @param [Symbol] type (either :group or :person)
+  # @return 
+  # This method limits the response to known access levels.  Probably runs a bit faster than .permissions().
+  module ClassMethods
+    def quick_search_by_type(type)
+      result = {}
+      [{:discover_access=>"discover"},{:read_access=>"read"},{:edit_access=>"edit"},{:exceptions_access=>"exceptions"}].each do |access_levels_hash|
+        access_level = access_levels_hash.keys.first
+        access_level_name = access_levels_hash.values.first
+        self.find_by_terms(*[access_level, type]).each do |entry|
+          result[entry.text] = access_level_name
+        end
+      end
+      return result
+    end
+  end
+end
