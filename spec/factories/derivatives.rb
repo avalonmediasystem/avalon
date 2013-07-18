37bc358adb8aa429c6713105757e822e9ae0fe0a
Michael Klein <mbklein@gmail.com>
2013-07-18 09:26:27 -0500

    Add missing derivatives.rb factory file

--- spec/factories/derivatives.rb
+++ spec/factories/derivatives.rb
@@ -1,7 +1,11 @@
 FactoryGirl.define do
   factory :derivative do
+    duration "21575"
+    location_url "rtmp://localhost/vod/mp4:6f69c008-06a4-4bad-bb60-26297f0b4c06/35bddaa0-fbb4-404f-ab76-58f22921529c/warning"
+    track_id "track-6"
+    hls_url "http://avalon.dev/6f69c008-06a4-4bad-bb60-26297f0b4c06/35bddaa0-fbb4-404f-ab76-58f22921529c/warning.mp4.m3u8"
+    hls_track_id "track-8"
     after(:create) do |d|
-      d.masterfile = FactoryGirl.create(:master_file)
       d.save
     end
   end
