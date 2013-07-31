6ac15411a667e3b377602895cd5349d8290c2f29
Michael Klein <mbklein@gmail.com>
2013-07-31 00:32:40 -0500

    Add database, media info, streaming server, and delayed job monitoring.

--- config/initializers/about_page.rb
+++ config/initializers/about_page.rb
@@ -1,5 +1,3 @@
-require 'about_page/matterhorn'
-
 AboutPage.configure do |config|
   config.app          = { :name => Avalon.name, :version => Avalon::VERSION }
   config.environment  = AboutPage::Environment.new({ 
@@ -8,8 +6,12 @@ AboutPage.configure do |config|
   config.request = AboutPage::RequestEnvironment.new({
     'HTTP Server' => /^(SERVER_|POW_)/
   })
-  config.fedora       = AboutPage::Fedora.new(ActiveFedora::Base.connection_for_pid(0))
-  config.solr         = AboutPage::Solr.new(ActiveFedora.solr.conn, :numDocs => 1)
-  config.matterhorn   = AboutPage::Matterhorn.new(Rubyhorn)
-  config.dependencies = AboutPage::Dependencies.new(1)
+  config.fedora           = AboutPage::Fedora.new(ActiveFedora::Base.connection_for_pid(0))
+  config.solr             = AboutPage::Solr.new(ActiveFedora.solr.conn, :numDocs => 1)
+  config.database         = Avalon::About::Database.new(User)
+  config.matterhorn       = Avalon::About::Matterhorn.new(Rubyhorn)
+  config.mediainfo        = Avalon::About::MediaInfo.new(:version => '>=0.7.59')
+  config.streaming_server = Avalon::About::RTMPServer.new(URI.parse(Avalon::Configuration['streaming']['rtmp_base']).host)
+  config.delayed_job      = Avalon::About::DelayedJob.new(:min => 1)
+  config.dependencies     = AboutPage::Dependencies.new
 end
