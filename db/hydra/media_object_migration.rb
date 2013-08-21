70a4b43a1c3c4a5fc6b2194cda237b0a1f31c3dd
Michael Klein <mbklein@gmail.com>
2013-08-21 15:42:18 -0500

    First stab at R1=>R2 MediaObject, MasterFile, and Derivative migrations

---
+++ db/hydra/media_object_migration.rb
@@ -0,0 +1,35 @@
+class MediaObjectMigration < Hydra::Migrate::Migration
+  def initialize(*args)
+    super
+    
+    if Admin::Collection.count == 0
+      # Gather list of collections to be created and their managers
+
+      new_collections = MediaObject.find(:all).inject(Hash.new {|h,k|h[k]=[]}) { |h,obj| 
+        k = obj.descMetadata.collection.last
+        h[k] = (h[k]+obj.edit_users).uniq
+        h 
+      }
+
+      # Make sure all managers are in the managers group
+
+      manager_group = Admin::Group.find('manager') or Admin::Group.find('collection_manager')
+      manager_group.name = 'manager'
+      manager_group.users = (manager_group.users + new_collections.values.flatten).uniq
+      manager_group.save
+
+      # Create collections
+
+      new_collections.each_pair { |name, managers|
+        name ||= 'NO COLLECTION'
+        Admin::Collection.create(name: name, managers: managers)
+      }
+    end  
+  end
+
+  migrate nil => 'R2' do |obj,ver,dispatcher|
+    dispatcher.migrate!(obj.parts)
+    collection_name = obj.descMetadata.collection.last || 'NO COLLECTION'
+    obj.collection = Admin::Collection.find_by_name(collection_name)
+  end
+end
