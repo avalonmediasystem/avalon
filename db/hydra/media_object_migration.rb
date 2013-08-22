5f2b3ea19fa0b99ae1d626badaaaf86ff29d80d7
Michael Klein <mbklein@gmail.com>
2013-08-22 10:06:05 -0500

    Smarter, just-in-time collection creation

--- db/hydra/media_object_migration.rb
+++ db/hydra/media_object_migration.rb
@@ -1,36 +1,28 @@
 class MediaObjectMigration < Hydra::Migrate::Migration
-  def initialize(*args)
-    super
-    
-    if Admin::Collection.count == 0
-      # Gather list of collections to be created and their managers
-
-      new_collections = MediaObject.find(:all).inject(Hash.new {|h,k|h[k]=[]}) { |h,obj| 
-        k = obj.descMetadata.collection.last
-        h[k] = (h[k]+obj.edit_users).uniq
-        h 
-      }
-
-      # Make sure all managers are in the managers group
-
-      manager_group = Admin::Group.find('manager') or Admin::Group.find('collection_manager')
+  def find_or_create_collection(name, managers)
+    # Make sure all managers are in the global manager group
+    manager_group = Admin::Group.find('manager') or Admin::Group.find('collection_manager')
+    if manager_group.name == 'collection_manager' or managers.any? { |m| ! manager_group.users.include? m }
       manager_group.name = 'manager'
-      manager_group.users = (manager_group.users + new_collections.values.flatten).uniq
+      manager_group.users = (manager_group.users + managers).uniq
       manager_group.save
+    end
 
-      # Create collections
-
-      new_collections.each_pair { |name, managers|
-        name ||= 'NO COLLECTION'
-        Admin::Collection.create(name: name, managers: managers)
-      }
-    end  
+    # Find or create the collection with the specified name
+    name ||= 'NO COLLECTION'
+    collection = Admin::Collection.find(:name_tesim => collection_name).first
+    if collection.nil?
+      Admin::Collection.create(name: name, managers: managers, unit: Admin::Collection.units.first)
+    end
+    collection.managers = (collection.managers + managers).uniq
+    collection.save
+    collection
   end
 
   migrate nil => 'R2' do |obj,ver,dispatcher|
     dispatcher.migrate!(obj.parts)
-    collection_name = obj.descMetadata.collection.last || 'NO COLLECTION'
-    obj.collection = Admin::Collection.find_by_name(collection_name)
+    collection = find_or_create_collection(obj.descMetadata.collection.last, obj.edit_users)
+    obj.collection = collection
     obj.descMetadata.collection = []
     obj.descMetadata.remove_empty_nodes!
   end
