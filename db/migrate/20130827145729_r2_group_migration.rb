4bd1bb756fdec8ef25b4d82b3b3bf2321c84ab78
Nathan Rogers <rogersna@indiana.edu>
2013-08-27 11:21:27 -0400

    First cut at a migration of the system groups

--- db/migrate/20130827145729_r2_group_migration.rb
+++ db/migrate/20130827145729_r2_group_migration.rb
@@ -1,6 +1,21 @@
 class R2GroupMigration < ActiveRecord::Migration
   def up
-    # Stuff goes here
+    # Start by looking for an existing manager group and failing the 
+    # migration until it is dealt with
+    if Admin::Group.exists?("manager")
+      puts "WARNING: You already have a manager group"
+      raise ActiveRecord::ConfigurationError
+    end
+
+    # Get the list of collection managers and create a new manager group
+    # for them to exist under
+    r1_managers = Admin::Group.find("collection_manager")
+    r2_managers = Admin::Group.create(name: 'manager')
+    r2_managers.users += r1_managers.users 
+    r2_managers.save
+
+    # Finally if all goes well we delete the collection_manager group
+    r1_managers.delete
   end
 
   def down
