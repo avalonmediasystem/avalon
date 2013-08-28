9551b344e124233c8d3858293e65922e39b37434
Michael Klein <mbklein@gmail.com>
2013-08-27 22:53:21 -0500

    Don't try to migrate content if there are no MediaObjects – solr gets confused

--- db/migrate/20130827150031_r1_content_to_r2.rb
+++ db/migrate/20130827150031_r1_content_to_r2.rb
@@ -1,10 +1,12 @@
 class R1ContentToR2 < ActiveRecord::Migration
   def up
     ActiveFedora::Base.reindex_everything
-    migration_path = File.join(Rails.root,'db/hydra')
-    Hydra::Migrate::Dispatcher.migrate_all!(MediaObject, to: 'R2', path: migration_path) do |o,m,d|
-      current = o.current_migration.blank? ? 'unknown version' : o.current_migration
-      Rails.logger.info "Migrating #{o.class} #{o.pid} from #{current} to #{m[:to]}"
+    if MediaObject.count > 0
+      migration_path = File.join(Rails.root,'db/hydra')
+      Hydra::Migrate::Dispatcher.migrate_all!(MediaObject, to: 'R2', path: migration_path) do |o,m,d|
+        current = o.current_migration.blank? ? 'unknown version' : o.current_migration
+        Rails.logger.info "Migrating #{o.class} #{o.pid} from #{current} to #{m[:to]}"
+      end
     end
   end
 
