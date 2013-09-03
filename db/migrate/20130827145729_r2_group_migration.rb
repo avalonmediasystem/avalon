d91e3c59502e83e31edfb4dd8bf4b15109b048de
Michael Klein <mbklein@gmail.com>
2013-09-03 16:15:24 -0500

    The 'manager' group can exist at migration time, as long as 'collection_manager' doesn't.

--- db/migrate/20130827145729_r2_group_migration.rb
+++ db/migrate/20130827145729_r2_group_migration.rb
@@ -1,22 +1,24 @@
 class R2GroupMigration < ActiveRecord::Migration
   def up
-    # Start by looking for an existing manager group and failing the 
-    # migration until it is dealt with
-    if Admin::Group.exists?("manager")
-      puts "WARNING: You already have a manager group"
-      raise ActiveRecord::ConfigurationError
+    if Admin::Group.exists?("collection_manager")
+      # Start by looking for an existing manager group and failing the 
+      # migration until it is dealt with
+      if Admin::Group.exists?("manager")
+        puts "WARNING: You already have a manager group"
+        raise ActiveRecord::ConfigurationError
+      end
+
+      # Get the list of collection managers and create a new manager group
+      # for them to exist under
+      r1_managers = Admin::Group.find("collection_manager")
+      r2_managers = Admin::Group.create(name: 'manager')
+      r2_managers.users += r1_managers.users 
+      r2_managers.save
+
+      # Finally if all goes well we delete the collection_manager group
+      r1_managers.delete
     end
-
-    # Get the list of collection managers and create a new manager group
-    # for them to exist under
-    r1_managers = Admin::Group.find("collection_manager")
-    r2_managers = Admin::Group.create(name: 'manager')
-    r2_managers.users += r1_managers.users 
-    r2_managers.save
-
-    # Finally if all goes well we delete the collection_manager group
-    r1_managers.delete
-
+    
     # Now test for the existance of the administrator group. If it does not
     # exist we assume that the first user in the group_manager list should
     # be promoted
