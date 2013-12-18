09eafee24e0f0d24441c4455a01e3b41bc355616
phuongdh <phuongdh@gmail.com>
2013-12-18 12:07:09 -0500

    More fix for migration

--- db/migrate/20131218142013_add_display_aspect_ratio_to_master_file.rb
+++ db/migrate/20131218142013_add_display_aspect_ratio_to_master_file.rb
@@ -2,10 +2,14 @@ class AddDisplayAspectRatioToMasterFile < ActiveRecord::Migration
   def up
     MasterFile.find_each({},{batch_size:5}) do |masterfile|
       if ! masterfile.display_aspect_ratio && masterfile.workflow_id 
-        workflow = Rubyhorn.client.instance_xml(masterfile.workflow_id)
-        if workflow && (resolutions = workflow.streaming_resolution).any?
-          masterfile.display_aspect_ratio = resolutions.first.split(/x/).collect(&:to_f).reduce(:/).to_s 
-          masterfile.save
+        begin
+          workflow = Rubyhorn.client.instance_xml(masterfile.workflow_id)
+          if workflow && (resolutions = workflow.streaming_resolution).any?
+            masterfile.display_aspect_ratio = resolutions.first.split(/x/).collect(&:to_f).reduce(:/).to_s 
+            masterfile.save
+          end
+        rescue Rubyhorn::RestClient::Exceptions::HTTPNotFound
+          # Workflow must have been deleted, do nothing
         end
       end
     end
