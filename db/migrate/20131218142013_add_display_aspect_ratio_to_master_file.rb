e4f4d6ce1fd8100923e258df2cef8fd5b6a98667
phuongdh <phuongdh@gmail.com>
2013-12-18 11:33:43 -0500

    Fixed display_aspect_ratio migration

--- db/migrate/20131218142013_add_display_aspect_ratio_to_master_file.rb
+++ db/migrate/20131218142013_add_display_aspect_ratio_to_master_file.rb
@@ -3,7 +3,7 @@ class AddDisplayAspectRatioToMasterFile < ActiveRecord::Migration
     MasterFile.find_each({},{batch_size:5}) do |masterfile|
       if ! masterfile.display_aspect_ratio && masterfile.workflow_id 
         workflow = Rubyhorn.client.instance_xml(masterfile.workflow_id)
-        if workflow && (resolutions = workflow.streaming_resolution)
+        if workflow && (resolutions = workflow.streaming_resolution).any?
           masterfile.display_aspect_ratio = resolutions.first.split(/x/).collect(&:to_f).reduce(:/).to_s 
           masterfile.save
         end
