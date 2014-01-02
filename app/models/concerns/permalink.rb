1d9b28cd2b8de1aea8793da56720411cb311f1cf
Adam Hallett <adam.t.hallett@gmail.com>
2014-01-02 11:40:22 -0600

    adding exception handling for permalink

--- app/models/concerns/permalink.rb
+++ app/models/concerns/permalink.rb
@@ -2,16 +2,6 @@ module Permalink
 
   extend ActiveSupport::Concern
 
-  included do
-    belongs_to :permalink, property: :has_permalink
-
-    attr_reader :permalink
-
-    def permalink
-      self.relationships(:has_permalink).first.to_s
-    end
-  end
-
   @@generator = Proc.new { |obj| nil }
   def self.permalink_for(obj)
     @@generator.call(obj)
@@ -24,26 +14,43 @@ module Permalink
   def self.on_generate(&block)
     @@generator = block
   end 
+  
 
-  def create_or_update_permalink( object )
-    updated = false
-    if object.published? 
-      permalink = permalink_for(object)
+  included do
+    belongs_to :permalink, property: :has_permalink
 
-      if permalink
-        current_permalink = object.relationships(:has_permalink)
-        if ! current_permalink || current_permalink.empty? || current_permalink.try(:first) != permalink
-          create_or_update_permalink!(object, permalink)
-          updated = true
+    attr_reader :permalink
+
+    def permalink
+      self.relationships(:has_permalink).first.to_s
+    end
+
+    def create_or_update_permalink( object )
+      updated = false
+      if object.published?
+
+        begin
+          permalink = Permalink.permalink_for(object)
+        rescue Exception => e
+          permalink = nil
+          logger.error "Permalink.permalink_for() raised an exception for #{object.inspect}: #{e}"
+        end
+        
+        if permalink
+          current_permalink = object.relationships(:has_permalink)
+          if ! current_permalink || current_permalink.empty? || current_permalink.try(:first) != permalink
+            create_or_update_permalink!(object, permalink)
+            updated = true
+          end
         end
       end
+      updated
     end
-    updated
-  end
 
-  def self.create_or_update_permalink!( object, permalink )
-    object.remove_relationship(:has_permalink, nil)
-    object.add_relationship(:has_permalink, permalink, true)
+    def create_or_update_permalink!( object, permalink )
+      object.remove_relationship(:has_permalink, nil)
+      object.add_relationship(:has_permalink, permalink, true)
+    end
   end
 
 end
