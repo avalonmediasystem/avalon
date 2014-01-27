d8db8812b535b2b7af55c2be8ffdc0422f8b1396
Chris Colvard <cjcolvar@indiana.edu>
2014-01-26 23:16:56 -0500

    Don't modify the params hash

--- app/controllers/object_controller.rb
+++ app/controllers/object_controller.rb
@@ -2,12 +2,10 @@ class ObjectController < ApplicationController
   def show
     obj = ActiveFedora::Base.find('dc_identifier_tesim' => params[:id])
     obj ||= ActiveFedora::Base.find(params[:id], cast: true) rescue ActiveFedora::ObjectNotFoundError
-    params.delete(:controller)
-    params.delete(:action)
     if obj.blank?
       redirect_to root_path
     else
-      redirect_to polymorphic_path(obj, params)
+      redirect_to polymorphic_path(obj, params.except(:controller, :action))
     end
   end
 end
