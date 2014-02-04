47c3adbfed1f13b84454b9223be8211c6551e7fb
phuongdh <phuongdh@gmail.com>
2014-02-04 09:29:19 -0500

    Replaced cookie with param to indicate logging in through popup

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -15,7 +15,7 @@
 class Users::SessionsController < Devise::SessionsController
   def new
     if Avalon::Authentication::Providers.length == 1
-      redirect_to user_omniauth_authorize_path(Avalon::Authentication::Providers.first[:provider])
+      redirect_to user_omniauth_authorize_path(Avalon::Authentication::Providers.first[:provider], params)
     else
       super
     end
