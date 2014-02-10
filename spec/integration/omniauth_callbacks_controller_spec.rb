67ddd81a79f36fdd73b9456b364d08451bc01d21
Michael Klein <mbklein@gmail.com>
2014-02-10 10:00:03 -0600

    Partially revert e09d2c6; improve tests around it

--- spec/integration/omniauth_callbacks_controller_spec.rb
+++ spec/integration/omniauth_callbacks_controller_spec.rb
@@ -4,8 +4,13 @@ describe Users::OmniauthCallbacksController do
 
   context 'lti' do
     let(:lti_fixtures) { YAML.load(File.read(File.expand_path('../../fixtures/lti_params.yml', __FILE__))) }
-    let(:lti_config)   { lti_fixtures[:config] }
-    let(:foo_hash) { OmniAuth::AuthHash.new(lti_fixtures[:foo]) }
+    let(:lti_config)   { lti_fixtures[:config]                               }
+    let(:foo_hash)     { lti_fixtures[:foo]                                  }
+    let(:foo_config)   { lti_config[foo_hash['tool_consumer_instance_guid']] }
+    let(:user_uid)     { foo_hash[foo_config[:uid]]                          }
+    let(:user_email)   { foo_hash[foo_config[:email]]                        }
+    let(:course_id)    { foo_hash[foo_config[:context_id]]                   }
+    let(:course_name)  { foo_hash[foo_config[:context_name]]                 }
 
     before :each do
       IMS::LTI::ToolProvider.any_instance.stub(:valid_request!) { true }
@@ -19,20 +24,25 @@ describe Users::OmniauthCallbacksController do
 
     it 'should create the user if necessary' do
       expect { post '/users/auth/lti/callback', foo_hash }.to change { User.all.count }
+      new_user = User.last
+      expect(new_user.username).to eq(user_uid)
+      expect(new_user.email).to eq(user_email)
     end
 
     it 'should create the course if necessary' do
       expect { post '/users/auth/lti/callback', foo_hash }.to change { Course.all.count }
+      new_course = Course.last
+      expect(new_course.context_id).to eq(course_id)
+      expect(new_course.label).to eq(course_name)
     end
 
     it 'should use an existing user if possible' do
-      u = FactoryGirl.build(:user, username: foo_hash[:lis_person_sourcedid], email: foo_hash[:lis_person_contact_email_primary])
-      u.save
+      FactoryGirl.build(:user, username: user_uid, email: user_email).save
       expect { post '/users/auth/lti/callback', foo_hash }.not_to change { User.all.count }
     end
 
     it 'should use an existing course if possible' do
-      FactoryGirl.build(:course, context_id: foo_hash[:context_id], label: foo_hash[:context_title]).save
+      FactoryGirl.build(:course, context_id: course_id, label: course_name).save
       expect { post '/users/auth/lti/callback', foo_hash }.not_to change { Course.all.count }
     end
 
