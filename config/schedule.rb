beccdb285837f3289644a7f384a52e06d6a6bd9f
Chris Colvard <cjcolvar@indiana.edu>
2014-02-13 09:40:29 -0500

    Simple bash script wrapping rake with locking via pid file in tmp/pids

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,7 @@
 set :output, "log/whenever_cron.log"
+job_type :locking_runner, "cd :path && :environment_variable=:environment script/locking_runner :name :task :output"
+job_type :locking_rake, "cd :path && :environment_variable=:environment script/locking_runner :task bundle exec rake :task --silent :output"
 
 every 1.minute do
-  rake 'avalon:batch:ingest'
+  locking_rake "avalon:batch:ingest"
 end
