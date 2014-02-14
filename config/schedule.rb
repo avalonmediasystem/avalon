3a17214470807516fdd068a2e062e55395b82fbf
Michael Klein <mbklein@gmail.com>
2014-02-14 09:48:49 -0600

    Add the :lock_name parameter to the whenever config file

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 set :output, "log/whenever_cron.log"
-job_type :locking_runner, "cd :path && :environment_variable=:environment script/locking_runner :name :task :output"
-job_type :locking_rake, "cd :path && :environment_variable=:environment script/locking_runner :task bundle exec rake :task --silent :output"
+job_type :locking_runner, "cd :path && :environment_variable=:environment script/locking_runner :lock_name :task :output"
+job_type :locking_rake, "cd :path && :environment_variable=:environment script/locking_runner :lock_name bundle exec rake :task --silent :output"
 
 every 1.minute do
-  locking_rake "avalon:batch:ingest"
+  locking_rake "avalon:batch:ingest", :lock_name => "batch_ingest"
 end
