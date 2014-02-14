7e83c239530e3cde61c4d982d1d8be5e2d5a715f
lealeelu <lmb@iu.edu>
2014-02-14 09:45:58 -0500

    Fix migration script for Masterfile.display_aspect_ratio
    If there is no workflow to reference to find the display_aspect ratio, Mediainfo is ranon the absolute_location of a derivative instead.
    If that fails, the user is warned that setting the display_aspect_ratio failed for that pid.

--- db/migrate/20131218142013_add_display_aspect_ratio_to_master_file.rb
+++ db/migrate/20131218142013_add_display_aspect_ratio_to_master_file.rb
@@ -1,15 +1,26 @@
 class AddDisplayAspectRatioToMasterFile < ActiveRecord::Migration
   def up
     MasterFile.find_each({},{batch_size:5}) do |masterfile|
-      if ! masterfile.display_aspect_ratio && masterfile.workflow_id 
+      if masterfile.is_video? && masterfile.display_aspect_ratio.blank? 
         begin
           workflow = Rubyhorn.client.instance_xml(masterfile.workflow_id)
           if workflow && (resolutions = workflow.streaming_resolution).any?
-            masterfile.display_aspect_ratio = resolutions.first.split(/x/).collect(&:to_f).reduce(:/).to_s 
-            masterfile.save
+            ratio = resolutions.first
           end
-        rescue Rubyhorn::RestClient::Exceptions::HTTPNotFound
-          # Workflow must have been deleted, do nothing
+        rescue
+          # no workflow available, resort to using mediainfo on a derivative
+          d = masterfile.derivatives.first
+          if !d.nil? 
+            d_info = Mediainfo.new d.absolute_location
+            ratio = d_info.video_display_aspect_ratio
+          end
+        ensure
+          if ratio.nil? 
+            logger.warn("#{masterfile.pid} aspect ratio not found")
+          else
+            masterfile.display_aspect_ratio = ratio.split(/[x:]/).collect(&:to_f).reduce(:/).to_s 
+            masterfile.save(validate: false)
+          end
         end
       end
     end
