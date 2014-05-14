9dc483c6c7a754531770b9835eb677ebeea92cc5
lealeelu <lmb@iu.edu>
2014-05-14 13:10:25 -0400

    Add ObjectController tests

--- spec/controllers/object_controller_spec.rb
+++ spec/controllers/object_controller_spec.rb
@@ -1,20 +1,36 @@
 require 'spec_helper'
 
 describe ObjectController do
-  it "should redirect you to root if no object is found" do
-    get :show, id: 'avalon:bad-pid'
-    response.should redirect_to root_path
+  describe "#show" do
+    it "should redirect you to root if no object is found" do
+      get :show, id: 'avalon:bad-pid'
+      response.should redirect_to root_path
+    end
+
+    it "should redirect you to the object show page" do
+      obj = FactoryGirl.create(:media_object)
+      get :show, id: obj.pid
+      response.should redirect_to(media_object_path(obj)) 
+    end
+
+    it "should pass along request params" do
+      obj = FactoryGirl.create(:media_object)
+      get :show, id: obj.pid, foo: 'bar'
+      response.should redirect_to(media_object_path(obj, {foo: 'bar'}))
+    end
+    
+    it "should redirect to appended url" do
+      obj = FactoryGirl.create(:media_object)
+      get :show, id: obj.pid, urlappend: 'test'
+      response.should redirect_to(media_object_path(obj)+"/test")
+    end
   end
 
-  it "should redirect you to the object show page" do
-    obj = FactoryGirl.create(:media_object)
-    get :show, id: obj.pid
-    response.should redirect_to(media_object_path(obj)) 
-  end
-
-  it "should pass along request params" do
-    obj = FactoryGirl.create(:media_object)
-    get :show, id: obj.pid, foo: 'bar'
-    response.should redirect_to(media_object_path(obj, {foo: 'bar'}))
+  describe "#autocomplete" do
+    it "should call autocomplete on the specified model" do
+      FactoryGirl.create(:user, email: "test@example.com")
+      get :autocomplete, t: 'user', q: 'test'
+      expect(response.body).to include "test@example.com" 
+    end
   end
 end
