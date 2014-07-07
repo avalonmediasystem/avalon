e9b6880baf99caeb02608df0700c5faa27ef26ff
Adam Hallett <adam.t.hallett@gmail.com>
2014-07-07 13:31:48 -0500

    remove whitespace from group name

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -55,14 +55,15 @@ class Admin::GroupsController < ApplicationController
   end
   
   def create
-    if Admin::Group.exists?(params["admin_group"])
-      flash[:error] = "Group name #{params["admin_group"]} is taken."
+    name = params['admin_group'].strip
+    if Admin::Group.exists?(name)
+      flash[:error] = "Group name #{name} is taken."
       redirect_to admin_groups_path
       return
     end
 
     @group = Admin::Group.new
-    @group.name = params["admin_group"]
+    @group.name = name
     if @group.save
       redirect_to edit_admin_group_path(@group)
     else
