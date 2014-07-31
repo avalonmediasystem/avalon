0c4458301e48d59133960bc6f847b399905199ab
Chris Colvard <cjcolvar@indiana.edu>
2014-07-31 08:19:21 -0400

    Facet cleanup; basic metadata cleanup

--- Gemfile
+++ Gemfile
@@ -6,10 +6,10 @@
   #gem 'protected_attributes'
   gem 'builder', '~>3.1.0'
 
-  gem 'hydra', '~>7.0'
+  gem 'hydra', git: 'https://github.com/projecthydra/hydra.git' #'~>7.0'
   gem 'active-fedora'
   gem 'activerecord-session_store'
-  gem 'bcrypt-ruby', '~> 3.0.0'
+  gem 'bcrypt-ruby', '~> 3.1.0'
   gem 'kaminari', '~> 0.15.0'
 
   gem 'avalon-workflow', git: 'https://github.com/avalonmediasystem/avalon-workflow.git', tag: 'avalon-r3'
--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -89,9 +89,9 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display 
-    config.add_index_field 'creator_ssim', :label => 'Main contributors:', :helper_method => :contributor_index_display 
-    config.add_index_field 'date_ssi', :label => 'Date:' 
-    config.add_index_field 'summary_ssi', label: 'Summary:', :helper_method => :description_index_display
+    config.add_index_field 'creator_ssim', :label => 'Main contributors', :helper_method => :contributor_index_display 
+    config.add_index_field 'date_ssi', :label => 'Date' 
+    config.add_index_field 'summary_ssi', label: 'Summary', :helper_method => :description_index_display
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display 
