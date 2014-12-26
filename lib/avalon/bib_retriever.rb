7e845722c31d83909eafe28831053c5c2887ca7f
Michael Klein <mbklein@gmail.com>
2014-12-26 16:02:31 -0600

    Use MARCXML and always transform to MODS locally

--- lib/avalon/bib_retriever.rb
+++ lib/avalon/bib_retriever.rb
@@ -1,20 +1,44 @@
 module Avalon
   class BibRetriever
-    extend Forwardable
-    def_delegator :@driver, :get_record
+    XSLT_FILE = File.expand_path('../bib_retriever/MARC21slim2MODS3-5-avalon.xsl',__FILE__)
+    attr_reader :config
     
-    def initialize
-      config = Avalon::Configuration.lookup('bib_retriever')
-      case config['protocol'].downcase
-      when 'sru', /^yaz/
-        require 'avalon/bib_retriever/sru'
-        @driver = Avalon::BibRetriever::SRU.new config
-      when /^z(oom|39)/
-        require 'avalon/bib_retriever/zoom'
-        @driver = Avalon::BibRetriever::Zoom.new config
-      else
-        raise ArgumentError, "Unknown bib retriever protocol: #{config['protocol']}"
+    class << self
+      def instance
+        if @instance.nil?
+          config = Avalon::Configuration.lookup('bib_retriever')
+          case config['protocol'].downcase
+          when 'sru', /^yaz/
+            require 'avalon/bib_retriever/sru'
+            @instance = Avalon::BibRetriever::SRU.new config
+          when /^z(oom|39)/
+            require 'avalon/bib_retriever/zoom'
+            @instance = Avalon::BibRetriever::Zoom.new config
+          else
+            raise ArgumentError, "Unknown bib retriever protocol: #{config['protocol']}"
+          end
+        end
+        @instance
       end
+      
+      protected :new, :allocate
+    end
+    
+    def initialize config
+      @config = config
+    end
+
+    def marcxml2mods(marcxml)
+      doc = Nokogiri::XML(marcxml)
+      xsl = Nokogiri::XSLT.parse(File.read(XSLT_FILE))
+      mods = xsl.transform(doc)
+      mods.to_s
+    end
+    
+    def marc2mods(marc)
+      record = MARC::Reader.decode marc
+      marcxml = record.to_xml.to_s
+      marcxml2mods(marcxml)
     end
   end
 end
