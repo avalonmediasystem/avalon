012d1a79fca3e3adb06f9668196e42dc44b3a50a
Michael Klein <mbklein@gmail.com>
2015-01-23 11:49:57 -0600

    url-encode the SRU URL before sending

--- lib/avalon/bib_retriever/sru.rb
+++ lib/avalon/bib_retriever/sru.rb
@@ -28,7 +28,7 @@ module Avalon
       
       def url_for(bib_id)
         uri = URI.parse config['url']
-        query_param = config['query'] % { bib_id: bib_id.to_s }
+        query_param = URI.encode(config['query'] % { bib_id: bib_id.to_s })
         uri.query = "version=1.1&operation=searchRetrieve&maximumRecords=1&recordSchema=marcxml&query=#{query_param}"
         uri.to_s
       end
