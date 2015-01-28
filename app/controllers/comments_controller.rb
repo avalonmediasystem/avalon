46217b53ac6934e3711a87a89793d39079e1c62c
Chris Colvard <cjcolvar@indiana.edu>
2015-01-28 12:17:12 -0500

    Fix honeypot

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,36 +15,37 @@
 class CommentsController < ApplicationController
   before_filter :set_subjects
 
-    # Index replaces new in this context
-	def index 
-	  @comment = Comment.new
-	end
+  # Index replaces new in this context
+  def index 
+    @comment = Comment.new
+  end
 
-	def create
-	  @comment = Comment.new
-	  @comment.name = params[:comment][:name]
-	  @comment.email = params[:comment][:email]
-	  @comment.email_confirmation = params[:comment][:email_confirmation]
-	  @comment.subject = params[:comment][:subject]
-	  @comment.comment = params[:comment][:comment]
-	  
-		if (@comment.valid? && !@comment.spam?)
-		    begin
-			  CommentsMailer.contact_email(@comment).deliver
-			rescue Errno::ECONNRESET => e
-			  logger.warn "The mail server does not appear to be responding \n #{e}"
-			  
-			  flash[:notice] = "The message could not be sent in a timely fashion. Contact us at #{Avalon::Configuration.lookup('email.support')} to report the problem."
-			  render action: "index"
-			end
-		else
-		   flash[:error] = "There were problems submitting your comment. Please correct the errors and try again."
-			render action: "index"
-		end 
-	end
+  def create
+    @comment = Comment.new
+    @comment.name = params[:comment][:name]
+    @comment.nickname = params[:comment][:nickname]
+    @comment.email = params[:comment][:email]
+    @comment.email_confirmation = params[:comment][:email_confirmation]
+    @comment.subject = params[:comment][:subject]
+    @comment.comment = params[:comment][:comment]
+
+    if (@comment.valid?)
+      begin
+	CommentsMailer.contact_email(@comment).deliver
+      rescue Errno::ECONNRESET => e
+	logger.warn "The mail server does not appear to be responding \n #{e}"
 	
-	protected
-	def set_subjects
-		@subjects = Comment::SUBJECTS
-	end
+	flash[:notice] = "The message could not be sent in a timely fashion. Contact us at #{Avalon::Configuration.lookup('email.support')} to report the problem."
+	render action: "index"
+      end
+    else
+     flash[:error] = "There were problems submitting your comment. Please correct the errors and try again."
+     render action: "index"
+    end 
+  end
+  
+  protected
+  def set_subjects
+    @subjects = Comment::SUBJECTS
+  end
 end
