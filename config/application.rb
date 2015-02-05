90b7fdb8c278becc40ba9a06ee614a3dcf2a7d69
Chris Colvard <cjcolvar@indiana.edu>
2015-02-05 12:05:34 -0500

    Set LANG environment variable

--- config/application.rb
+++ config/application.rb
@@ -43,6 +43,7 @@ module Avalon
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+    ENV["LANG"] = "en_US.utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
