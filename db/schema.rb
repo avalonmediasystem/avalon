c1febebb8eccb0f61de61ba259a1677225f3eaf5
Michael Klein <mbklein@gmail.com>
2015-06-01 10:08:57 -0500

    Add migration to change masterfile status SUCCEEDED to COMPLETED and back

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150430131441) do
+ActiveRecord::Schema.define(version: 20150601145809) do
 
   create_table "bookmarks", force: true do |t|
     t.integer  "user_id",       null: false
