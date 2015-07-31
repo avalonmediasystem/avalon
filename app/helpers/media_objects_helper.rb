dffff9b48c88c24b5aa8af1e4c3de876aa23fabc
Brian Keese <bkeese@indiana.edu>
2015-07-31 15:28:53 -0400

    Fix bug

--- app/helpers/media_objects_helper.rb
+++ app/helpers/media_objects_helper.rb
@@ -154,7 +154,7 @@ EOF
        # If there is no structural metadata associated with this masterfile return the stream info
        if section.structuralMetadata.empty?
          label = "<ul><li><span>#{index+1}. #{stream_label_for(section)} #{duration}</span></li></ul>".html_safe
-         link = link_to label, share_link_for( section ), data: data, class: current ? 'current-stream playable' : nil
+         link = link_to label, share_link_for( section ), data: data, class: current ? 'current-stream playable' : 'playable'
          return "#{headeropen}#{link}#{headerclose}"
        end
 
@@ -223,7 +223,7 @@ EOF
          native_url = "#{pid_section_media_object_path(@mediaobject, section.pid)}?t=#{start},#{stop}"
          url = "#{share_link_for( section )}?t=#{start},#{stop}"
          data =  {segment: section.pid, is_video: section.is_video?, native_url: native_url, fragmentbegin: start, fragmentend: stop}
-         link = link_to label, url, data: data, class: is_current_section?(section) ? 'current-stream playable' : nil
+         link = link_to label, url, data: data, class: is_current_section?(section) ? 'current-stream playable' : 'playable'
          return "<li class='stream-li'>#{link}</li>", tracknumber
        end
      end
