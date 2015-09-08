42fb478d3e3af824d0e64a4106bef297094157e7
Carrick Rogers <carrickr@iu.edu>
2015-09-08 11:56:29 -0400

    Add in coveralls support.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,14 @@
 # Copyright 2011-2015, The Trustees of Indiana University and Northwestern
 #   University.  Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
-# 
+#
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software distributed 
+#
+# Unless required by applicable law or agreed to in writing, software distributed
 #   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-#   CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+#   CONDITIONS OF ANY KIND, either express or implied. See the License for the
 #   specific language governing permissions and limitations under the License.
 # ---  END LICENSE_HEADER BLOCK  ---
 
@@ -27,6 +27,10 @@ require 'database_cleaner'
 require 'fakefs/safe'
 require 'fileutils'
 require 'tmpdir'
+require 'coveralls'
+
+#Configure coveralls for CI builds
+Coveralls.wear!('rails')
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -64,7 +68,7 @@ RSpec.configure do |config|
     DatabaseCleaner[:active_record].strategy = :deletion
     DatabaseCleaner[:active_fedora].strategy = :deletion
     DatabaseCleaner.clean
-    Rails.cache.clear 
+    Rails.cache.clear
     Deprecation.default_deprecation_behavior = :silence
 
     # Stub the entire dropbox
@@ -75,7 +79,7 @@ RSpec.configure do |config|
     Avalon::Configuration['dropbox']['path'] = Avalon::Configuration.lookup('spec.fake_dropbox')
 
     ActiveEncode::Base.engine_adapter = :test
-    
+
     server_options = { host: 'test.host', port: nil }
     Rails.application.routes.default_url_options.merge!( server_options )
     ActionMailer::Base.default_url_options.merge!( server_options )
@@ -100,7 +104,7 @@ RSpec.configure do |config|
   config.after(:each) do
     Rails.cache.clear
     DatabaseCleaner.clean
-   end 
+   end
 
   config.include Devise::TestHelpers, :type => :controller
   config.include ControllerMacros, :type => :controller
