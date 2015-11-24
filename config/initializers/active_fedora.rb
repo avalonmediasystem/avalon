0026f16473b1505df7f4790a6cece9a60233f271
Chris Colvard <cjcolvar@indiana.edu>
2015-11-24 13:57:09 -0500

    Allow MediaObject to use kaminari for pagination

--- config/initializers/active_fedora.rb
+++ config/initializers/active_fedora.rb
@@ -1,3 +1,13 @@
 ActiveFedora::Base.class_eval do
   has_metadata name: 'DC', type: DublinCoreDocument
 end
+
+ActiveFedora::QueryMethods.module_eval do
+    def extending(*modules, &block)
+      if modules.any? || block
+        spawn.extending!(*modules, &block)
+      else
+        self
+      end
+    end
+end
