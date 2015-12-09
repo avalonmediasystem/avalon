2108ee717dd62345ba73dfe2a6f618e6d873ca38
Chris Colvard <cjcolvar@indiana.edu>
2015-12-08 23:25:02 -0500

    Monkey-patch ActiveFedora to fix chaining relations on associations

--- config/initializers/active_fedora.rb
+++ config/initializers/active_fedora.rb
@@ -2,6 +2,7 @@ ActiveFedora::Base.class_eval do
   has_metadata name: 'DC', type: DublinCoreDocument
 end
 
+#Added for Kaminari paging
 ActiveFedora::QueryMethods.module_eval do
     def extending(*modules, &block)
       if modules.any? || block
@@ -10,4 +11,12 @@ ActiveFedora::QueryMethods.module_eval do
         self
       end
     end
+end
+
+#Cherry-picked 9cf316b71e78e65d95bfe1fa8ce93373da1a7305
+ActiveFedora::Associations::CollectionProxy.class_eval do
+      def scope
+        @association.scope
+      end
+      alias spawn scope
 end
