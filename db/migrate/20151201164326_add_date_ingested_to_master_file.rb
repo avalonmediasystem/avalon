4d68fb5b86beaa18ce633ec8521fc290e8497518
Chris Colvard <cjcolvar@indiana.edu>
2015-12-16 06:08:43 -0500

    WIP Change date_digitized into date_ingested

--- db/migrate/20151201164326_add_date_ingested_to_master_file.rb
+++ db/migrate/20151201164326_add_date_ingested_to_master_file.rb
@@ -1,17 +1,17 @@
-class AddDateDigitizedToMasterFile < ActiveRecord::Migration
+class AddDateIngestedToMasterFile < ActiveRecord::Migration
 
   def up
     MasterFile.find_each({},{batch_size:5}) do |mf|
       encode = ActiveEncode::Base.find(mf.workflow_id)
       next unless encode.present?
-      mf.date_digitized = encode.finished_at
+      mf.date_ingested = encode.finished_at
       mf.save(validate: false)
     end
   end
 
   def down
     MasterFile.find_each({},{batch_size:5}) do |mf|
-      mf.date_digitized = nil
+      mf.date_ingested = nil
       mf.save(validate: false)
     end
   end
