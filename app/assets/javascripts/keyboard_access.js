861a5e8dc6c41be62524e55ba07dbd10123d3e34
Chris Syversen <csyversen@gmail.com>
2016-01-12 16:43:30 -0600

    modified behavior of volume slider

--- app/assets/javascripts/keyboard_access.js
+++ app/assets/javascripts/keyboard_access.js
@@ -1,15 +1,15 @@
-/* 
+/*
  * Copyright 2011-2015, The Trustees of Indiana University and Northwestern
  *   University.  Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
- * 
+ *
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software distributed 
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
  *   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- *   CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ *   CONDITIONS OF ANY KIND, either express or implied. See the License for the
  *   specific language governing permissions and limitations under the License.
  * ---  END LICENSE_HEADER BLOCK  ---
 */
@@ -130,5 +130,10 @@ var keyboardAccess = function() {
       if ( e.shiftKey && e.keyCode == 9 && $( "#content div[itemprop='video']" ).length !== 0 ) {
         $( ".mejs-controls" ).css( "visibility", "hidden" );
       }
+    });
+
+    // Hide the volume slider when tabbing off of the mute button
+    $( ".mejs-button.mejs-volume-button.mejs-mute button:first").on("blur", function( e ) {
+      $( ".mejs-volume-slider" ).hide();
     });
 };
