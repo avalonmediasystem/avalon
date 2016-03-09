87f36fd338a3a212cf7dd02cd01898ad146e151c
Chris Syversen <csyversen@gmail.com>
2016-03-09 10:49:46 -0600

    fixed misnamed variable

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,14 +1,14 @@
 # Copyright 2011-2015, The Trustees of Indiana University and Northwestern
 #   University.  Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
-# 
+#
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software distributed 
+#
+# Unless required by applicable law or agreed to in writing, software distributed
 #   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-#   CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+#   CONDITIONS OF ANY KIND, either express or implied. See the License for the
 #   specific language governing permissions and limitations under the License.
 # ---  END LICENSE_HEADER BLOCK  ---
 
@@ -19,7 +19,7 @@ class Ability
 
   def user_groups
     return @user_groups if @user_groups
-      
+
     @user_groups = default_user_groups
     @user_groups |= current_user.groups if current_user and current_user.respond_to? :groups
     @user_groups |= ['registered'] unless current_user.new_record?
@@ -37,7 +37,7 @@ class Ability
         can :manage, Admin::Group
         can :manage, Admin::Collection
       end
-    
+
       if @user_groups.include? "group_manager"
         can :manage, Admin::Group do |group|
           group.nil? or !['administrator','group_manager'].include?(group.name)
@@ -61,7 +61,7 @@ class Ability
       end
 
       can :read, MasterFile do |master_file|
-        can? :read, masterfile.mediaobject
+        can? :read, master_file.mediaobject
       end
 
       can :read, Derivative do |derivative|
@@ -78,18 +78,18 @@ class Ability
         unless (is_member_of_any_collection? or @user_groups.include? 'manager')
           cannot :read, Admin::Collection
         end
-    
+
         can :update_access_control, MediaObject do |mediaobject|
-          @user.in?(mediaobject.collection.managers) || 
+          @user.in?(mediaobject.collection.managers) ||
             (is_editor_of?(mediaobject.collection) && !mediaobject.published?)
         end
 
         can :unpublish, MediaObject do |mediaobject|
-          @user.in?(mediaobject.collection.managers) 
+          @user.in?(mediaobject.collection.managers)
         end
 
         can :update, Admin::Collection do |collection|
-          is_editor_of?(collection) 
+          is_editor_of?(collection)
         end
 
         can :update_unit, Admin::Collection do |collection|
@@ -109,12 +109,12 @@ class Ability
         end
 
         can :update_depositors, Admin::Collection do |collection|
-          is_editor_of?(collection) 
+          is_editor_of?(collection)
         end
-      
-        can :inspect, MediaObject do |mediaobject| 
-         is_member_of?(mediaobject.collection) 
-        end 
+
+        can :inspect, MediaObject do |mediaobject|
+         is_member_of?(mediaobject.collection)
+        end
 
         # Users logged in through LTI cannot share
         can :share, MediaObject
@@ -127,13 +127,13 @@ class Ability
       end
 
       cannot :update, MediaObject do |mediaobject|
-        (not full_login?) || (!is_member_of?(mediaobject.collection)) || 
+        (not full_login?) || (!is_member_of?(mediaobject.collection)) ||
           ( mediaobject.published? && !@user.in?(mediaobject.collection.managers) )
       end
 
       cannot :destroy, MediaObject do |mediaobject|
-        # non-managers can only destroy mediaobject if it's unpublished 
-        (not full_login?) || (!is_member_of?(mediaobject.collection)) || 
+        # non-managers can only destroy mediaobject if it's unpublished
+        (not full_login?) || (!is_member_of?(mediaobject.collection)) ||
           ( mediaobject.published? && !@user.in?(mediaobject.collection.managers) )
       end
 
@@ -144,12 +144,12 @@ class Ability
   end
 
   def is_member_of?(collection)
-     @user_groups.include?("administrator") || 
+     @user_groups.include?("administrator") ||
        @user.in?(collection.managers, collection.editors, collection.depositors)
   end
 
-  def is_editor_of?(collection) 
-     @user_groups.include?("administrator") || 
+  def is_editor_of?(collection)
+     @user_groups.include?("administrator") ||
        @user.in?(collection.managers, collection.editors)
   end
 
