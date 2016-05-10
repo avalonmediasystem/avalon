48c4237816d5378f721e580f7aa341ce70113561
Weiwei Shi <weiwei.shi@ualberta.ca>
2016-05-10 13:53:49 -0600

    change the find_or_create_by_token_and_target method to find_or_create_by that is introduced in rails 4.1

--- app/models/stream_token.rb
+++ app/models/stream_token.rb
@@ -26,7 +26,7 @@ class StreamToken < ActiveRecord::Base
     self.purge_expired!
     hash_token = Digest::SHA1.new
     hash_token << media_token(session) << target
-    result = self.find_or_create_by_token_and_target(hash_token.to_s, target)
+    result = self.find_or_create_by(token: hash_token.to_s, target: target)
     result.renew!
     session[:hash_tokens] << result.token
     result.token
