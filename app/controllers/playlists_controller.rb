80ebbb89317309ea5032dad521a9ff2fb8badf05
Chris Syversen <csyversen@gmail.com>
2016-05-17 14:47:20 -0500

    drag and dropping now works, need to add tests

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -70,7 +70,7 @@ class PlaylistsController < ApplicationController
 
   # Only allow a trusted parameter "white list" through.
   def playlist_params
-    params.require(:playlist).permit(:title, :comment, :visibility, :annotation_ids)
+    params.require(:playlist).permit(:title, :comment, :visibility, :annotation_ids, items_attributes: [:id, :position])
   end
 
 end
