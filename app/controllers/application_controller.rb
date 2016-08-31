a1e93d1d5d87212485bbff0b250467c946169448
Carrick Rogers <carrickr@iu.edu>
2016-08-31 10:27:31 -0400

    add in cancan rescue from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from CanCan::AccessDenied do |exception|
+    if current_user
+      redirect_to root_path, flash: { notice: 'You are not authorized to perform this action.' }
+    else
+      session[:previous_url] = request.fullpath unless request.xhr?
+      redirect_to new_user_session_path, flash: { notice: 'You are not authorized to perform this action. Try logging in.' }
+    end
+  end
+
 end
