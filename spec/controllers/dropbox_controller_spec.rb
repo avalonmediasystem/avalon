772a7b7eb5ab8e961b1061fa97e896a602d9bcfe
Carrick Rogers <carrickr@iu.edu>
2016-08-31 11:51:43 -0400

    adjust the dropbox spec

--- spec/controllers/dropbox_controller_spec.rb
+++ spec/controllers/dropbox_controller_spec.rb
@@ -1,14 +1,14 @@
 # Copyright 2011-2015, The Trustees of Indiana University and Northwestern
 #   University.  Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
-# 
+#
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software distributed 
+#
+# Unless required by applicable law or agreed to in writing, software distributed
 #   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-#   CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+#   CONDITIONS OF ANY KIND, either express or implied. See the License for the
 #   specific language governing permissions and limitations under the License.
 # ---  END LICENSE_HEADER BLOCK  ---
 
@@ -16,13 +16,13 @@ require 'rails_helper'
 
 describe DropboxController do
   before do
-    
+
     # there is no easy way to build objects because of the way roles are set up
     # therefore this test could fail in the future if the archivist role doesn't
     # have the group admin_policy_object_editor. roles should be refactored into
     # a database backed model SOON so testing of permissions/abilities is more granular
 
-    login_as :administrator 
+    login_as :administrator
     @collection = FactoryGirl.create(:collection)
     @temp_files = (0..20).map{|index| { name: "a_movie_#{index}.mov" } }
     @dropbox = double(Avalon::Dropbox)
@@ -42,7 +42,7 @@ describe DropboxController do
     expect(response.status).to be(200)
   end
 
-  [:policy_editor, :student].each do |group|
+  [:group_manager, :student].each do |group|
     it "should not allow #{group} to delete" do
       login_as group
       delete :bulk_delete, {:collection_id => @collection.id, :filenames => @temp_files.map{|f| f[:name]}}
