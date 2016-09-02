237f5292919b6a0b1a1b2bd483bd5d8604c13d58
Chris Colvard <cjcolvar@indiana.edu>
2016-09-02 11:51:55 -0400

    ApplicationsController test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery unless: proc{|c| request.headers['Avalon-Api-Key'].present? }
 
   helper_method :render_bookmarks_control?
 
---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,40 @@
+# Copyright 2011-2015, The Trustees of Indiana University and Northwestern
+#   University.  Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+#   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+#   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+#   specific language governing permissions and limitations under the License.
+# ---  END LICENSE_HEADER BLOCK  ---
+
+require 'rails_helper'
+
+describe ApplicationController do
+  controller do
+    def create
+      render nothing: true
+    end
+  end
+
+  context "normal auth" do
+    it "should check for authenticity token" do
+      expect(controller).to receive(:verify_authenticity_token)
+      post :create
+    end
+  end
+  context "ingest API" do
+    before do
+      ApiToken.create token: 'secret_token', username: 'archivist1@example.com', email: 'archivist1@example.com'
+    end
+    it "should not check for authenticity token for API requests" do
+      request.headers['Avalon-Api-Key'] = 'secret_token'
+      expect(controller).not_to receive(:verify_authenticity_token)
+      post :create
+    end
+  end
+end
