458b08ba85e0ecfabf6624f47758b0fe4958d450
Leah Lee <lmb@iu.edu>
2016-09-07 15:59:55 -0400

    Add more javascript to precompile list (#1129)
    * Add more javascript to precompile list
    
    * Add marker.js to precompile list

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+#mediaelement
 Rails.application.config.assets.precompile += %w( mediaelement_rails/mediaelement-and-player.js )
 Rails.application.config.assets.precompile += %w( mediaelement-qualityselector/mep-feature-qualities.js )
 Rails.application.config.assets.precompile += %w( me-thumb-selector.js )
@@ -19,7 +21,18 @@ Rails.application.config.assets.precompile += %w( me-thumb-selector.css )
 Rails.application.config.assets.precompile += %w( mediaelement-skin-avalon/mejs-skin-avalon.css )
 Rails.application.config.assets.precompile += %w( me-add-to-playlist.css )
 
-Rails.application.config.assets.precompile += %w( file_browse.js.coffee )
-Rails.application.config.assets.precompile += %w( file_upload_step.js.coffee )
-Rails.application.config.assets.precompile += %w( modernizr.js )
+#avalon
+Rails.application.config.assets.precompile += %w( access_control_step.js )
+Rails.application.config.assets.precompile += %w( android_pre_play.js )
 Rails.application.config.assets.precompile += %w( autocomplete.js )
+Rails.application.config.assets.precompile += %w( avalon_player.js )
+Rails.application.config.assets.precompile += %w( avalon_progress.js )
+Rails.application.config.assets.precompile += %w( dropdown_text_fields.js )
+Rails.application.config.assets.precompile += %w( dynamic_fields.js )
+Rails.application.config.assets.precompile += %w( file_browse.js )
+Rails.application.config.assets.precompile += %w( file_upload_step.js )
+Rails.application.config.assets.precompile += %w( import_button.js )
+Rails.application.config.assets.precompile += %w( marker.js )
+
+#Third party
+Rails.application.config.assets.precompile += %w( modernizr.js )
