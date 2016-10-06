c16237c5ede0bc562263348f4ea691a1e8ed04f9
Brian Keese <bkeese@indiana.edu>
2016-10-06 14:38:43 -0400

    Use latest whenever gem and allow use for different rails environments

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,5 +3,5 @@ job_type :locking_runner, "cd :path && :environment_variable=:environment script
 job_type :locking_rake, "cd :path && :environment_variable=:environment script/locking_runner :lock_name bundle exec rake :task --silent :output"
 
 every 1.minute do
-  locking_rake "avalon:batch:ingest", :lock_name => "batch_ingest"
+  locking_rake "avalon:batch:ingest", :lock_name => "batch_ingest", :environment => ENV['RAILS_ENV'] || 'production'
 end
