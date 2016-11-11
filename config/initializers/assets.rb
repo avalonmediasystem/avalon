e436d7fe0fd801862b60874c9a617e8c7f5132a1
Chris Syversen <csyversen@gmail.com>
2016-11-11 07:44:08 -0600

    master_files_controller#embed uses embed layout which includes updated embed stylesheet (#1304)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,3 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( embed.css )
-Rails.application.config.assets.precompile += %w( embed_player.js )
