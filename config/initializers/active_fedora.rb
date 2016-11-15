e4986bb3593ecdb2e11717aeb52f4da131ad8642
Chris Colvard <cjcolvar@indiana.edu>
2016-11-15 10:19:14 -0500

    Initialize AF::Base.logger with Rails.logger

--- config/initializers/active_fedora.rb
+++ config/initializers/active_fedora.rb
@@ -1,2 +1,3 @@
 ActiveFedora::Base.translate_uri_to_id = ActiveFedora::Noid.config.translate_uri_to_id
 ActiveFedora::Base.translate_id_to_uri = ActiveFedora::Noid.config.translate_id_to_uri
+ActiveFedora::Base.logger = Rails.logger
