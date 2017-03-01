efb8614839992502bb30b904f99b0f9f2f974f3d
Phil Dinh <phuongdh@gmail.com>
2017-03-01 14:08:14 -0500

    Update spruce.rb

--- config/deploy/spruce.rb
+++ config/deploy/spruce.rb
@@ -2,6 +2,7 @@ set :rails_env, ENV['RAILS_ENV'] || 'development'
 set :bundle_flags, '--quiet'
 set :bundle_without, fetch(:rails_env) == "development" ? "production" : "development debug"
 set :branch, ENV['SCM_BRANCH'] || "develop"
+set :deploy_to, ENV['DEPLOY_TO'] || "/srv/avalon/avalon_r6"
 set :hls_dir, "/var/avalon/hls_streams"
 server 'spruce.dlib.indiana.edu', roles: %w{web app resque_worker resque_scheduler}, user: 'avalon'
 ssh_options[:keys] = ["/opt/staging/avalon/vov_deployment_key"] if ENV['CI_DEPLOY']
