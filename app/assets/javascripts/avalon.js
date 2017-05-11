dc8d1f4cd5a04af78e21d60efd96ee794b31f915
cjcolvar <cjcolvar@indiana.edu>
2017-05-11 11:42:07 -0400

    Remove redundant override
    This Blacklight override is already happening in app/assets/javascripts/blacklight/facet_load.js

--- app/assets/javascripts/avalon.js
+++ app/assets/javascripts/avalon.js
@@ -17,17 +17,6 @@
 // Empty file for future js
 /* Override the search_context so it stops POSTing links which confuses
  * Rails and causes it to redirect to the wrong place. */
-$(document).ready(function() {
-  Blacklight.do_search_context_behavior = function() {}
-  // adjust width of facet columns to fit their contents
-  function longer (a,b){ return b.textContent.length - a.textContent.length; }
-  $('ul.facet-values, ul.pivot-facet').map(function(){
-      var longest = $(this).find('.facet-count span').sort(longer).first();
-      var clone = longest.clone().css('visibility','hidden');
-      $('body').append(clone);
-      $(this).find('.facet-count').first().width(clone.width());
-      clone.remove();
-  });
 
   $( document ).on('click', '.btn-stateful-loading', function() { $(this).button('loading'); });
 
