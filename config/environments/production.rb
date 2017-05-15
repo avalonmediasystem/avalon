516fc65812e57388083f6e639c0237635281a111
Chris Colvard <cjcolvar@indiana.edu>
2017-05-15 15:10:49 -0400

    Silence deprecation warnings in production to keep the logs cleaner

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Suppress deprecation warnings
+  Deprecation.default_deprecation_behavior = :silence
 end
