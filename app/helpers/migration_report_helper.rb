2254411582d53f9997e600f1e9e8d2f6590f2579
Chris Colvard <cjcolvar@indiana.edu>
2017-05-23 23:36:50 -0400

    Restore total counts per object

--- app/helpers/migration_report_helper.rb
+++ app/helpers/migration_report_helper.rb
@@ -15,6 +15,7 @@
 module MigrationReportHelper
   def status_link(klass, display_name, status_name=display_name)
     params = { class: klass, status: status_name }.reject { |k,v| v.nil? }
+    params.delete(:status) if status_name == 'total'
     link_to @counts[klass][status_name].to_i, admin_migration_report_by_class_path(params)
   end
   
