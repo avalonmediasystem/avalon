f3682bffeeb30fb94529dbac68c2afdd71e42d3f
cjcolvar <cjcolvar@indiana.edu>
2017-06-06 09:35:41 -0400

    Fail missing mfs (#2000)
    * Fail media objects which don't have all of their master files
    
    * Fail dependent master file and derivative objects

--- app/migration/fedora_migrate/media_object/master_file_aggregation_mover.rb
+++ app/migration/fedora_migrate/media_object/master_file_aggregation_mover.rb
@@ -21,6 +21,10 @@ module FedoraMigrate
         if source.datastreams.has_key?('sectionsMetadata')
           sections_md = Nokogiri::XML(source.datastreams['sectionsMetadata'].content)
           old_pid_order = sections_md.xpath('fields/section_pid').collect(&:text)
+          unless lists_equivalent?(old_pid_order, master_files.collect {|mf| pid_from_obj(mf)})
+            fail_dependent_objects(master_files)
+            raise FedoraMigrate::Errors::MigrationError, "Master files found don't match media object expectations."
+          end
           target.ordered_master_files = master_files.sort do |a,b|
             old_pid_order.index(pid_from_obj(a)) <=> old_pid_order.index(pid_from_obj(b))
           end
@@ -34,6 +38,23 @@ module FedoraMigrate
       private
         def pid_from_obj(obj)
           obj.migrated_from.first.rdf_subject.to_s.split('/').last
+        end
+
+        def lists_equivalent?(a,b)
+          a.size == b.size && ((a-b) + (b-a)).blank?
+        end
+
+        def fail_dependent_objects(master_files)
+          #fail master_files
+          master_files.each do |mf|
+            fail_object(mf, source.pid)
+            ::Derivative.where(isDerivationOf_ssim: mf.id).each {|d| fail_object(d, pid_from_obj(mf))}
+          end
+        end
+        def fail_object(obj, parent_pid)
+            status_record = MigrationStatus.where(source_class: obj.class.name, f4_pid: obj.id).first
+            return unless status_record
+            status_record.update_attributes status: 'failed', log: "Parent object (#{parent_pid}) failed to migrate"
         end
     end
   end
