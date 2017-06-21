ebe6973fc20c7046671d5bed6671a1788dec8c11
Phil Dinh <phuongdh@gmail.com>
2017-06-21 11:18:01 -0400

    Add docker-compose file for quick start development #2070
    Allow running tests in Docker container

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -70,7 +70,7 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
 
   config.before :suite do
-    WebMock.disable_net_connect!(allow_localhost: true)
+    WebMock.disable_net_connect!(allow: ['localhost', 'fedora', 'solr'])
     DatabaseCleaner.clean_with(:truncation)
     ActiveFedora::Cleaner.clean!
     disable_production_minter!
