9c7cf6aecaa7a33aa382b20f9391126945c7a5e6
Phil Dinh <phuongdh@gmail.com>
2017-06-21 11:15:30 -0400

    Remove default x-frame-options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,6 +40,9 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  config.action_dispatch.default_headers = {
+    'X-Frame-Options' => nil
+  }
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
