0b04de25a818e63f2bf0674f012fbe717577a349
Chris Colvard <cjcolvar@indiana.edu>
2017-08-23 10:52:59 -0400

    Whitelist params allowed on LTI share links

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -66,7 +66,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:success] = nil
       render inline: '<html><head><script>window.close();</script></head><body></body><html>'.html_safe
     elsif request['target_id']
-      redirect_to objects_path(request['target_id'])
+      # Whitelist params that are allowed to be passed through via LTI
+      params_whitelist = %w{t position token}
+      redirect_to objects_path(request['target_id'], params.slice(*params_whitelist))
     elsif params[:url]
       redirect_to params[:url]
     elsif session[:previous_url] 
