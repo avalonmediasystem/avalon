a3642ede3d32882284349f93ba898818fcee9d13
Chris Colvard <cjcolvar@indiana.edu>
2017-09-07 00:52:01 -0400

    Build out batch entry ingest job without tests

--- app/jobs/ingest_batch_entry_job.rb
+++ app/jobs/ingest_batch_entry_job.rb
@@ -17,15 +17,67 @@ class IngestBatchEntryJob < ActiveJob::Base
 
   # ActiveJob will serialize/deserialize the batch_entry automatically using GlobalIDs
   def perform(batch_entry)
-    # TODO validation checking that it is okay to ingest this batch entry
-    # TODO delete pre-existing media object
-    # TODO Set to process status on the BatchEntries
+    # Validation checking that it is okay to ingest this batch entry
+    if batch_entry.media_object_pid.present? && MediaObject.exists?(batch_entry.media_object_pid)
+      published_error(batch_entry)
+      return
+    end
+
     entry = Avalon::Batch::Entry.from_json(batch_entry.payload)
     if entry.valid?
+      # Set to process status on the BatchEntries
+      update_status(batch_entry, "Processing")
+      # Start processing
       entry.process!
+      # Success handling
+      process_success(batch_entry, entry)
     else
-      # TODO Report error
+      # Report error
+      invalid_error(batch_entry, entry)
     end
     # TODO any post processing to update status?
+
+  rescue RuntimeError => e
+    process_error(batch_entry, e)
   end
+
+  private
+    # Set an error when a mediaobject has already been published and
+    # @param [BatchEntries] the entry to update
+    def published_error(previous_entry)
+      previous_entry.error = true
+      previous_entry.complete = false
+      previous_entry.current_status = 'Update Rejected'
+      previous_entry.error_message = 'Cannot update this item, it has already been published.'
+      previous_entry.save
+    end
+
+    # @param [BatchEntries] the entry to update
+    # @param [String] status to update the batch entry to
+    def update_status(batch_entry, status)
+      batch_entry.current_status = status
+      batch_entry.save!
+    end
+
+    # @param [BatchEntries] the entry to update
+    # @param [Avalon::Batch::Entry] the entry to update
+    def process_success(batch_entry, entry)
+      old_media_object_id = batch_entry.media_object_pid
+      batch_entry.media_object_pid = entry.media_object.id
+      # TODO Status change?
+      batch_entry.save!
+      # Delete pre-existing media object
+      MediaObject.find(old_media_object_id).destroy if old_media_object_id.present? && MediaObject.exists?(old_media_object_id)
+    end
+
+    # Set an error when an error occurs during processing
+    # @param [BatchEntries] the entry to update
+    # @param [RuntimeError] the error raised
+    def invalid_error(batch_entry, error)
+      batch_entry.error = true
+      batch_entry.complete = false
+      batch_entry.current_status = 'Processing Error'
+      batch_entry.error_message = error.message
+      batch_entry.save
+    end
 end
