3f989ea3b4377ad35a254e7712c9dcf7d9977fbc
Chris Colvard <cjcolvar@indiana.edu>
2017-09-07 16:56:16 -0400

    Basic test of the BatchEntries model

---
+++ spec/models/batch_entries_spec.rb
@@ -0,0 +1,33 @@
+# Copyright 2011-2017, The Trustees of Indiana University and Northwestern
+#   University.  Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+#   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+#   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+#   specific language governing permissions and limitations under the License.
+# ---  END LICENSE_HEADER BLOCK  ---
+
+require 'rails_helper'
+
+describe BatchEntries do
+  subject { FactoryGirl.build(:batch_entries) }
+
+  describe '#save' do
+    it 'persists' do
+      subject.save
+      subject.reload
+      expect(subject.batch_registries).to be_present
+      expect(subject.current_status).to be_present
+      expect(subject.media_object_pid).to be_present
+      expect(subject.position).to be_present
+      expect(subject.payload).to be_present
+      expect(subject.complete).not_to be_nil
+      expect(subject.error).not_to be_nil
+    end
+  end
+end
