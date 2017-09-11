be860092ecb6b162b842e14e5a4b921080e9393f
Phil Dinh <phuongdh@gmail.com>
2017-09-11 15:08:00 -0400

    Refactor dynamic cap profile to deploy to all servers (#2306)

--- config/deploy/dynamic.rb
+++ config/deploy/dynamic.rb
@@ -1,15 +1,17 @@
 # Example:
 # DB=postgres REPO=git@github.com:user/avalon.git BRANCH=deploy/prod USER=deploy APP_HOST=avalon.example.edu cap dynamic deploy
 
-set :bundle_flags,  "--with #{ENV['DB']}" if ENV['DB']
-set :bundle_without, 'development test debug'
 set :rails_env, ENV['RAILS_ENV'] || 'production'
+set :bundle_flags,  "--with #{ENV['DB']}" if ENV['DB']
+set :bundle_without, ENV['RAILS_ENV'] == "development" ? "production" : 'development test debug'
 set :repo_url, ENV['REPO']
 set :branch, ENV['BRANCH']
+set :deploy_to, ENV['DEPLOY_TO']
+set :hls_dir, ENV['HLS_DIR']
 set :user, ENV['USER']
-role :web, ENV['APP_HOST']
-role :app, ENV['APP_HOST']
-role :resque_worker, ENV['RESQUE_HOST'] || ENV['APP_HOST']
-role :resque_scheduler, ENV['RESQUE_HOST'] || ENV['APP_HOST']
+server ENV['APP_HOST'], roles: %w{web app}, user: ENV['USER'] || 'avalon'
+server ENV['RESQUE_HOST'] || ENV['APP_HOST'], roles: %w{resque_worker resque_scheduler}, user: ENV['RESQUE_USER'] || 'avalon'
+append :linked_files, ENV['LINKED_FILES'] if ENV['LINKED_FILES'] 
 
 set :workers, { "*" => 2 }
+
