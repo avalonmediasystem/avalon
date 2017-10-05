4fdc98423a690a9c46da3133fa04e2988f27e776
Chris Colvard <cjcolvar@indiana.edu>
2017-10-05 16:42:37 -0400

    Allow only one ALLOWALL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,9 +40,6 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-  config.action_dispatch.default_headers = {
-    'X-Frame-Options' => ''
-  }
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
