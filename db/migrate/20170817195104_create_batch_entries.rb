294f5c4270affc226b74199c749b0d04c2064b8f
Phuong Dinh <phuongdh@gmail.com>
2017-10-11 14:39:01 -0400

    Limit BatchEntry payload field to 1GB so it works with postgres

--- db/migrate/20170817195104_create_batch_entries.rb
+++ db/migrate/20170817195104_create_batch_entries.rb
@@ -2,7 +2,7 @@ class CreateBatchEntries < ActiveRecord::Migration
   def change
     create_table :batch_entries do |t|
       t.belongs_to :batch_registries, index: true
-      t.text 'payload', limit: 4294967295
+      t.text 'payload', limit: 1_073_741_823
       t.boolean 'complete'
       t.boolean 'error'
       t.string 'current_status'
