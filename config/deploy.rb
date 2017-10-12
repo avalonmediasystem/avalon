314b94f39805df966c8cd7f4c83383212c82437d
Phil Dinh <phuongdh@gmail.com>
2017-10-12 12:01:17 -0400

    Include local settings in capistrano linked_files

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, 'git://github.com/avalonmediasystem/avalon.git'
 # If SCM_BRANCH is set, use it. Otherwise, ask for a branch, defaulting to the currently checked out branch.
 set :branch, -> { ENV['SCM_BRANCH'] || ask(:branch, `git rev-parse --abbrev-ref HEAD`.chomp) }
 
-append :linked_files, "Gemfile.local", "config/*.yml", "config/initializers/*.rb", "public/robots.txt"
+append :linked_files, "Gemfile.local", "config/*.yml", "config/*/*.yml", "config/initializers/*.rb", "public/robots.txt"
 append :linked_dirs, 'log', 'tmp'
 
 set :conditionally_migrate, true
