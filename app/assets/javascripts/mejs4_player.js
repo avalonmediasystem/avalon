e8158ddbcaccb5828f30bdfe58635c19fedbdc9d
Adam Arling <adam.arling@northwestern.edu>
2017-10-27 11:21:23 -0500

    Updated the MEJS4 quality selector plugin to fix the hover state from continually showing

--- app/assets/javascripts/mejs4_player.js
+++ app/assets/javascripts/mejs4_player.js
@@ -1,6 +1,6 @@
 //= require mediaelement/mediaelement-and-player
 //= require mediaelement/plugins/markers
-//= require mediaelement/plugins/quality
+//= require mediaelement/plugins/quality-avalon
 //= require mediaelement/plugins/quality-i18n
 //= require media_player_wrapper/mejs4_add_to_playlist
 //= require media_player_wrapper/mejs4_add_marker_to_playlist
