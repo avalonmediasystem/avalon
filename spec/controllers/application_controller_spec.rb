145729b0d4b352bad59380bb78f411aa3eca7240
Chris Colvard <cjcolvar@indiana.edu>
2017-11-28 11:52:54 -0500

    Test important redirects from Fedora 3 pid to Avalon noid

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -79,11 +79,6 @@ describe ApplicationController do
   end
 
   describe "rewrite_v4_ids" do
-    xit 'skips miration controllers' do
-      get :show, id: 'avalon:1234', controller: 'MigrationStatusController'
-      expect(response).to have_http_status(:ok)
-    end
-
     it 'skips when id is not a Fedora 3 pid' do
       get :show, id: 'abc1234'
       expect(response).not_to have_http_status(304)
@@ -92,12 +87,6 @@ describe ApplicationController do
     it 'skips post requests' do
       post :create, id: 'avalon:1234'
       expect(response).not_to have_http_status(304)
-    end
-
-    xit 'redirects' do
-      master_file = FactoryGirl.create(:master_file, identifier: ['avalon:1234'])
-      get :show, id: 'avalon:1234'
-      expect(response).to redirect_to(url_for(master_file.id))
     end
   end
 end
