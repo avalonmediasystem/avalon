fa4e4cab7231728c090175d0426e3a03b0a37dd8
Brian Keese <bkeese@indiana.edu>
2018-01-30 12:02:02 -0500

    Add Intercom tests

--- lib/avalon/intercom.rb
+++ lib/avalon/intercom.rb
@@ -17,34 +17,34 @@ module Avalon
     def initialize(user, avalon = 'default')
       @user = user
       @avalon = Settings.intercom[avalon]
-      @user_collections = nil
+      @collections = nil
     end
 
     def user_collections(reload = false)
-      if reload || @user_collections.nil?
-        @user_collections = fetch_user_collections
+      if reload || @collections.nil?
+        @collections = fetch_user_collections
       end
-      @user_collections
+      @collections
     end
 
     def collection_valid?(collection)
-      @user_collections.collect { |c| c['id'] }.include? collection
+      user_collections.any? { |c| c['id'] == collection }
     end
 
     def push_media_object(media_object, collection_id, include_structure = true)
-      return { message: 'You are not autorized to push to this collection.' } unless collection_valid? collection_id
       return { message: 'Avalon intercom target is not configured.' } unless @avalon.present?
+      return { message: 'You are not autorized to push to this collection.' } unless collection_valid? collection_id
       begin
         resp = RestClient::Request.execute(
           method: :post,
-          url: URI.join(@avalon[:url], 'media_objects.json').to_s,
+          url: URI.join(@avalon['url'], 'media_objects.json').to_s,
           payload: build_payload(media_object, collection_id, include_structure),
-          headers: { content_type: :json, accept: :json, :'Avalon-Api-Key' => @avalon[:api_token] },
+          headers: { content_type: :json, accept: :json, :'Avalon-Api-Key' => @avalon['api_token'] },
           verify_ssl: false
         )
-        { link: URI.join(@avalon[:url], 'media_objects/', JSON.parse(resp.body)['id']).to_s }
+        { link: URI.join(@avalon['url'], 'media_objects/', JSON.parse(resp.body)['id']).to_s }
       rescue StandardError => e
-        { message: e.message, status: e.response.code }
+        { message: e.message, status: e.respond_to?(:response) ? e.response.code : 500 }
       end
     end
 
@@ -53,21 +53,21 @@ module Avalon
       def build_payload(media_object, collection_id, include_structure)
         payload = media_object.to_ingest_api_hash(include_structure)
         payload[:collection_id] = collection_id
-        payload[:import_bib_record] = @avalon[:import_bib_record]
-        payload[:publish] = @avalon[:publish]
+        payload[:import_bib_record] = @avalon['import_bib_record']
+        payload[:publish] = @avalon['publish']
         payload.to_json
       end
 
       def fetch_user_collections
         return [] unless @avalon.present?
-        uri = URI.join(@avalon[:url], 'admin/collections.json')
+        uri = URI.join(@avalon['url'], 'admin/collections.json')
         uri.query = "user=#{@user}"
         resp = RestClient::Request.execute(
           method: :get,
           url: uri.to_s,
           timeout: nil,
           open_timeout: nil,
-          headers: { content_type: :json, accept: :json, :'Avalon-Api-Key' => @avalon[:api_token] },
+          headers: { content_type: :json, accept: :json, :'Avalon-Api-Key' => @avalon['api_token'] },
           verify_ssl: false
         )
         JSON.parse(resp.body).map { |c| c.slice('id', 'name') }.sort_by { |c| c["name"] }
