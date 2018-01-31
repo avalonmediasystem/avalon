00c989b54da907c617bcda5930ae691b7b33f210
Chris Colvard <cjcolvar@indiana.edu>
2018-01-31 09:30:14 -0500

    Avoid duplicate stream token entries in the session

--- spec/models/stream_token_spec.rb
+++ spec/models/stream_token_spec.rb
@@ -1,11 +1,11 @@
 # Copyright 2011-2018, The Trustees of Indiana University and Northwestern
 #   University.  Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
-# 
+#
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software distributed
 #   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 #   CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -22,6 +22,18 @@ describe StreamToken do
 
     it 'should create a token' do
       expect(StreamToken.find_or_create_session_token(session, target)).to match(/^[0-9a-f]{40}$/)
+    end
+
+    it 'stores the token in the session' do
+      token = StreamToken.find_or_create_session_token(session, target)
+      expect(session[:hash_tokens]).to include token
+    end
+
+    it 'stores the token once in the session' do
+      token = StreamToken.find_or_create_session_token(session, target)
+      token2 = StreamToken.find_or_create_session_token(session, target)
+      expect(token).to eq token2
+      expect(session[:hash_tokens].count(token)).to eq 1
     end
   end
 
