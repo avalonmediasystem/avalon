cd2506b73286bacc53d3661e771da79fec8f1e7e
Brian Keese <bkeese@indiana.edu>
2018-03-20 11:17:42 -0400

    Update code version to 6.4.0

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'resolv-replace'
 Bundler.require(*Rails.groups)
 
 module Avalon
-  VERSION = '6.3.2'
+  VERSION = '6.4.0'
 
   class Application < Rails::Application
     require 'avalon/configuration'
