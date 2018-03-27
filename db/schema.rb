a2c246f0ce66374ad2652c5d520574b941eff326
Chris Colvard <cjcolvar@indiana.edu>
2018-03-27 16:38:51 -0400

    Update db/schema.rb so it has the correct timestamp of latest migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120190443) do
+ActiveRecord::Schema.define(version: 20171130201127) do
 
   create_table "annotations", force: :cascade do |t|
     t.string  "uuid"
