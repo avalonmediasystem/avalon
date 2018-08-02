74587a30580e81d8985186fec2688ab34ad3758a
yingfeng-iu <yingfeng@iu.edu>
2018-08-02 15:55:27 -0400

    Issue#3006: add waveform in controller and routes

--- app/controllers/master_files_controller.rb
+++ app/controllers/master_files_controller.rb
@@ -36,6 +36,19 @@ class MasterFilesController < ApplicationController
     end
   end
 
+  # Renders the waveform data for an object or alerts the user that no waveform data is present with html present
+  # @return [String] The rendered template
+  def waveform
+    @master_file = MasterFile.find(params[:id])
+    authorize! :read, @master_file
+    ds = @master_file.waveform
+    if ds.nil? or ds.empty?
+      render :text => 'Not Found', :status => :not_found
+    else
+      send_data ds.content, type: ds.mime_type, filename: ds.original_name
+    end
+  end
+
   def can_embed?
     params[:action] == 'embed'
   end
