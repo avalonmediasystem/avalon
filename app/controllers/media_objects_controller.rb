17d2323730fea0dca5d0ce3f55fe17f346e159a1
Chris Colvard <cjcolvar@indiana.edu>
2018-08-09 13:27:06 -0400

    Replace render :nothing with head :ok

--- app/controllers/media_objects_controller.rb
+++ app/controllers/media_objects_controller.rb
@@ -469,7 +469,7 @@ class MediaObjectsController < ApplicationController
 
   def set_session_quality
     session[:quality] = params[:quality] if params[:quality].present?
-    render nothing: true
+    head :ok
   end
 
   protected
