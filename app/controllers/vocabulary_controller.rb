22cfbad3e25bf4d7530e842d75e701e0f6856f56
Chris Colvard <cjcolvar@indiana.edu>
2018-08-09 13:27:06 -0400

    Replace render :nothing with head :ok

--- app/controllers/vocabulary_controller.rb
+++ app/controllers/vocabulary_controller.rb
@@ -1,11 +1,11 @@
 # Copyright 2011-2018, The Trustees of Indiana University and Northwestern
 #   University.  Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
-# 
+#
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software distributed
 #   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 #   CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -36,7 +36,7 @@ class VocabularyController < ApplicationController
     v[params[:id].to_sym] |= Array(params[:entry])
     result = Avalon::ControlledVocabulary.vocabulary = v
     if result
-      render nothing: true, content_type: 'application/json', status: 200
+      head :ok, content_type: 'application/json'
     else
       render json: {errors: ["Update failed"]}, status: 422
     end
