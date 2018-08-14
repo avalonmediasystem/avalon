acee0420cc85a8eb1ef7bf9a45c0829388c04d89
Chris Colvard <cjcolvar@indiana.edu>
2018-08-14 14:45:52 -0400

    Upgrade to Rails 5.1.6

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
 require 'resolv-replace'
@@ -17,6 +17,8 @@ module Avalon
       g.test_framework :rspec, :spec => true
     end
 
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
