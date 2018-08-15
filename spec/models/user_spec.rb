655d09c82e12ead43da8802b1edcd19a7825a7f1
Chris Colvard <cjcolvar@indiana.edu>
2018-08-15 17:10:53 -0400

    Bookmarks need a user not a user_id

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 # Copyright 2011-2018, The Trustees of Indiana University and Northwestern
 #   University.  Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
-# 
+#
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software distributed
 #   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 #   CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -72,7 +72,7 @@ describe User do
   describe "#destroy" do
     it 'removes bookmarks for user' do
       user = FactoryBot.create(:public)
-      bookmark = Bookmark.create(document_id: Faker::Number.digit, user_id: user.id)
+      bookmark = Bookmark.create(document_id: Faker::Number.digit, user: user)
       expect { user.destroy }.to change { Bookmark.exists? bookmark.id }.from( true ).to( false )
     end
   end
