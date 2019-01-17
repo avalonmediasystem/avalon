f7e91731d5a6df5d0c7d3b0d115922e9ca2d3203
Adam Arling <adam.arling@northwestern.edu>
2019-01-17 14:08:48 -0600

    React Rails way of including a component in a view using the 'react_component' helper

--- app/javascript/components/sme/app.js
+++ app/javascript/components/sme/app.js
@@ -1,7 +1,9 @@
 // app/javascript/structural-metadata-editor/index.js
 import React from 'react'
-import ReactDOM from 'react-dom'
 import Test from './test';
 
-const sme = document.querySelector('#sme')
-ReactDOM.render(<Test />, sme)
+const App = props => {
+  return <Test />
+};
+
+export default App;
