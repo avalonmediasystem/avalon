74248636312e9262badcb9ab16b0e161d512ee13
Adam Arling <adam.arling@northwestern.edu>
2019-01-17 13:54:44 -0600

    First way of rendering a component

---
+++ app/javascript/packs/structural-metadata-editor.js
@@ -0,0 +1,1 @@
+import 'structural-metadata-editor';
---
+++ app/javascript/packs/structural-metadata-editor/index.js
@@ -0,0 +1,7 @@
+// app/javascript/structural-metadata-editor/index.js
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Test from './test';
+
+const sme = document.querySelector('#sme')
+ReactDOM.render(<Test />, sme)
