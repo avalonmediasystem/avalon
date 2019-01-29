4594e92e185462817ba40bf160ce97b87b20bdb4
yingfeng-iu <yingfeng@iu.edu>
2019-01-29 16:51:07 -0500

    Issue#3008: resolve todo items

--- script/waveform_backfill.rb
+++ script/waveform_backfill.rb
@@ -1,37 +1,40 @@
-wb_file = '/srv/avalon/scriptdata/waveform_backfill.txt'
-# TODO use cmdline param for start_index
-start_index = 1
+wb_path = '/srv/avalon/scriptdata/waveform_backfill.txt'
+start_index = ARGV[0] || 1
+row_max = ARGV[1] || 1000000
 
-master_files = []
-unless File.exist? file
-  Rails.logger.info "Getting master files waveform info from solr and writing ids of those need back-fill to waveform_backfill file"
-  master_file_file = File.new(wb_file, "w")
-  solr = RSolr.connect url: 'http://127.0.0.1:8983/solr/avalon'
-  # TODO slor query?
-  result = solr.get 'select', params: { q: "has_master_filedel_ssim:MasterFile", fl: [:id, :has_waveform?_bs], rows: 1000000 }
+# record all master files needing waveform back-fill in a file, so if the script fails we can restart from the last processed master file
+# without this file, we will solely rely on the has_waveform?_bs field, and we could run into a situation when the waveform backfill is scheduled
+# but not finished, and when the script restarts it will pick up that master file again; also having this file will avoid querying solr each time
+# the script needs a restart
+mf_ids = []
+unless File.exist? wb_path
+  Rails.logger.info "Getting master files waveform info from solr and writing ids of those need back-fill to #{wb_path}"
+  wb_file = File.new(wb_path, "w")
+  solr = RSolr.connect url: ENV['SOLR_URL'] || "http://127.0.0.1:8983/solr/avalon"
+  result = solr.get 'select', params: { q: "has_model_ssim:MasterFile", fl: ['id', 'has_waveform?_bs'], rows: row_max }
   count = 0
   result["response"]["docs"].each do |doc|
     next if doc['has_waveform?_bs']
     id = "#{doc['id']}"
-    master_file_file.puts id
-    master_files << id
+    wb_file.puts id
+    mf_ids << id
     count += 1
-    Rails.logger.debug "MasterFile #{id}"
+    Rails.logger.debug "to be back filled: master file #{id}"
   end
-  master_file_file.close
-  Rails.logger.info "Found #{count} master files to have waveform back-filled"
+  wb_file.close
+  Rails.logger.info "Found #{count} master files to be back-filled with waveform"
 end
 
-if master_files.empty?
-  Rails.logger.info "Reading ids of master files to have waveform back-filled from waveform_backfill file"
-  File.readlines(master_file_file).each do |line|
-    master_files << line
-    Rails.logger.info line
+if mf_ids.empty?
+  Rails.logger.info "Reading ids of master files needing waveform back-fill from #{wb_path}"
+  File.readlines(wb_file).each do |id|
+    mf_ids << id
+    Rails.logger.debug "to be back filled: master file #{id}"
   end
 end
 
-master_files.each_with_index do |id, i|
+mf_ids.each_with_index do |id, i|
   next if i+1 < start_index
-  WaveformJob.perform_later(master_file.id)
+  WaveformJob.perform_later(id)
   Rails.logger.info "Scheduled WavefromJob for master file #{id} on line #{i+1}"
 end
