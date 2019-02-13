7c36327e88b70549ce661108e341e4c8cadee1b4
Brian Keese <bkeese@indiana.edu>
2019-02-13 16:48:09 -0500

    address codeclimate complaints

--- app/helpers/timelines_helper.rb
+++ app/helpers/timelines_helper.rb
@@ -1,21 +1,37 @@
+# frozen_string_literal: true
+
+# Copyright 2011-2018, The Trustees of Indiana University and Northwestern
+#   University.  Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+#   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+#   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+#   specific language governing permissions and limitations under the License.
+# ---  END LICENSE_HEADER BLOCK  ---
+
 module TimelinesHelper
   def timeline_human_friendly_visibility(visibility)
     content_tag(:span,
-      safe_join([icon_only_visibility(visibility),t("timeline.#{visibility}Text")], ' '),
-      class: "human_friendly_visibility_#{visibility}",
-      title: timeline_visibility_description(visibility))
+                safe_join([icon_only_visibility(visibility), t("timeline.#{visibility}Text")], ' '),
+                class: "human_friendly_visibility_#{visibility}",
+                title: timeline_visibility_description(visibility))
   end
 
   def timeline_icon_only_visibility(visibility)
     icon = case visibility
-      when Timeline::PUBLIC
-        "fa-globe"
-      when Timeline::PRIVATE
-        "fa-lock"
-      else
-        "fa-link"
-    end
-    content_tag(:span, '', class:"fa #{icon} fa-lg", title: visibility_description(visibility))
+           when Timeline::PUBLIC
+             "fa-globe"
+           when Timeline::PRIVATE
+             "fa-lock"
+           else
+             "fa-link"
+           end
+    content_tag(:span, '', class: "fa #{icon} fa-lg", title: visibility_description(visibility))
   end
 
   def timeline_visibility_description(visibility)
