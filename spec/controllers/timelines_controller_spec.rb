fbd38bd506005415fa74e18095904e6deab1e5d2
Chris Colvard <cjcolvar@indiana.edu>
2019-02-14 09:43:35 -0500

    Test timeline controller

--- spec/controllers/timelines_controller_spec.rb
+++ spec/controllers/timelines_controller_spec.rb
@@ -29,11 +29,11 @@ RSpec.describe TimelinesController, type: :controller do
   # Timeline. As you add validations to Timeline, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    { title: Faker::Lorem.word, visibility: Timeline::PUBLIC, user: user }
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    { visibility: 'unknown' }
   }
 
   # This should return the minimal set of values that should be in the session
@@ -41,26 +41,96 @@ RSpec.describe TimelinesController, type: :controller do
   # TimelinesController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
+  let(:user) { login_as :user }
+
+  describe 'security' do
+    let(:timeline) { FactoryBot.create(:timeline) }
+
+    context 'with unauthenticated user' do
+      # New is isolated here due to issues caused by the controller instance not being regenerated
+      it "should redirect to sign in" do
+        expect(get :new).to redirect_to(new_user_session_path)
+      end
+      it "all routes should redirect to sign in" do
+        expect(get :index).to redirect_to(new_user_session_path)
+        expect(get :edit, params: { id: timeline.id }).to redirect_to(new_user_session_path)
+        expect(post :create).to redirect_to(new_user_session_path)
+        expect(put :update, params: { id: timeline.id }).to redirect_to(new_user_session_path)
+        expect(delete :destroy, params: { id: timeline.id }).to redirect_to(new_user_session_path)
+      end
+      context 'with a public timeline' do
+        let(:timeline) { FactoryBot.create(:timeline, visibility: Timeline::PUBLIC) }
+        it "should return the timeline view page" do
+          expect(get :show, params: { id: timeline.id }).not_to redirect_to(new_user_session_path)
+        end
+      end
+      context 'with a private timeline' do
+        it "should NOT return the timeline view page" do
+          expect(get :show, params: { id: timeline.id }).to redirect_to(/#{Regexp.quote(new_user_session_path)}\?url=.*/)
+        end
+      end
+      context 'with a private timeline and token' do
+        let(:timeline) { FactoryBot.create(:timeline, :with_access_token) }
+        it "should return the timeline view page" do
+          expect(get :show, params: { id: timeline.id, token: timeline.access_token }).not_to redirect_to(root_path)
+        end
+      end
+    end
+    context 'with end-user' do
+      before do
+        login_as :user
+      end
+      it "all routes should redirect to /" do
+        expect(get :edit, params: { id: timeline.id }).to redirect_to(root_path)
+        expect(put :update, params: { id: timeline.id }).to redirect_to(root_path)
+        expect(delete :destroy, params: { id: timeline.id }).to redirect_to(root_path)
+      end
+      context 'with a public timeline' do
+        let(:timeline) { FactoryBot.create(:timeline, visibility: Timeline::PUBLIC) }
+        it "should return the timeline view page" do
+          expect(get :show, params: { id: timeline.id }).not_to redirect_to(root_path)
+        end
+      end
+      context 'with a private timeline' do
+        it "should NOT return the timeline view page" do
+          expect(get :show, params: { id: timeline.id }).to redirect_to(root_path)
+        end
+      end
+      context 'with a private timeline and token' do
+        let(:timeline) { FactoryBot.create(:timeline, :with_access_token) }
+        it "should return the timeline view page" do
+          expect(get :show, params: { id: timeline.id, token: timeline.access_token }).not_to redirect_to(root_path)
+        end
+      end
+    end
+  end
+
   describe "GET #index" do
-    it "returns a success response" do
+    it 'assigns accessible timelines as @timelines' do
+      # TODO: test non-accessible timelines not appearing
       timeline = Timeline.create! valid_attributes
       get :index, params: {}, session: valid_session
-      expect(response).to be_success
+      expect(assigns(:timelines)).to eq([timeline])
     end
   end
 
   describe "GET #show" do
-    it "returns a success response" do
+    it "redirects to the timeliner tool" do
       timeline = Timeline.create! valid_attributes
       get :show, params: {id: timeline.to_param}, session: valid_session
-      expect(response).to be_success
+      expect(response).to redirect_to Settings.timeliner.timeliner_url
     end
   end
 
   describe "GET #new" do
+    before do
+      login_as :user
+    end
+
     it "returns a success response" do
       get :new, params: {}, session: valid_session
       expect(response).to be_success
+      expect(assigns(:timeline)).to be_a_new(Timeline)
     end
   end
 
@@ -69,6 +139,13 @@ RSpec.describe TimelinesController, type: :controller do
       timeline = Timeline.create! valid_attributes
       get :edit, params: {id: timeline.to_param}, session: valid_session
       expect(response).to be_success
+      expect(assigns(:timeline)).to eq(timeline)
+    end
+
+    it 'assigns the requested timeline as @timeline' do
+      timeline = Timeline.create! valid_attributes
+      get :edit, params: { id: timeline.to_param }, session: valid_session
+      expect(assigns(:timeline)).to eq(timeline)
     end
   end
 
@@ -80,16 +157,37 @@ RSpec.describe TimelinesController, type: :controller do
         }.to change(Timeline, :count).by(1)
       end
 
+      it 'assigns a newly created timeline as @timeline' do
+        post :create, params: { timeline: valid_attributes }, session: valid_session
+        expect(assigns(:timeline)).to be_a(Timeline)
+        expect(assigns(:timeline)).to be_persisted
+      end
+
       it "redirects to the created timeline" do
         post :create, params: {timeline: valid_attributes}, session: valid_session
         expect(response).to redirect_to(Timeline.last)
       end
+
+      it 'generates a token if visibility is private-with-token' do
+        post :create, params: { timeline: valid_attributes.merge(visibility: Timeline::PRIVATE_WITH_TOKEN) }, session: valid_session
+        expect(assigns(:timeline).access_token).not_to be_blank
+      end
     end
 
     context "with invalid params" do
+      before do
+        login_as :user
+      end
+
       it "returns a success response (i.e. to display the 'new' template)" do
         post :create, params: {timeline: invalid_attributes}, session: valid_session
         expect(response).to be_success
+        expect(assigns(:timeline)).to be_a_new(Timeline)
+      end
+
+      it "re-renders the 'new' template" do
+        post :create, params: { timeline: invalid_attributes }, session: valid_session
+        expect(response).to render_template('new')
       end
     end
   end
@@ -97,28 +195,50 @@ RSpec.describe TimelinesController, type: :controller do
   describe "PUT #update" do
     context "with valid params" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        { title: Faker::Lorem.word, visibility: Timeline::PUBLIC, description: Faker::Lorem.sentence }
       }
 
       it "updates the requested timeline" do
         timeline = Timeline.create! valid_attributes
         put :update, params: {id: timeline.to_param, timeline: new_attributes}, session: valid_session
         timeline.reload
-        skip("Add assertions for updated state")
+        expect(timeline.title).to eq new_attributes[:title]
+        expect(timeline.visibility).to eq new_attributes[:visibility]
+        expect(timeline.description).to eq new_attributes[:description]
+      end
+
+      it 'assigns the requested timeline as @timeline' do
+        timeline = Timeline.create! valid_attributes
+        put :update, params: { id: timeline.to_param, timeline: new_attributes }, session: valid_session
+        expect(assigns(:timeline)).to eq(timeline)
       end
 
       it "redirects to the timeline" do
         timeline = Timeline.create! valid_attributes
         put :update, params: {id: timeline.to_param, timeline: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(timeline)
+        expect(response).to redirect_to(edit_timeline_path(timeline))
+      end
+
+      it 'generates a token if visibility is private-with-token' do
+        timeline = Timeline.create! valid_attributes
+        put :update, params: { id: timeline.to_param, timeline: { visibility: Timeline::PRIVATE_WITH_TOKEN } }, session: valid_session
+        timeline.reload
+        expect(timeline.access_token).not_to be_blank
       end
     end
 
     context "with invalid params" do
+      it 'assigns the timeline as @timeline' do
+        timeline = Timeline.create! valid_attributes
+        put :update, params: { id: timeline.to_param, timeline: invalid_attributes }, session: valid_session
+        expect(assigns(:timeline)).to eq(timeline)
+      end
+
       it "returns a success response (i.e. to display the 'edit' template)" do
         timeline = Timeline.create! valid_attributes
         put :update, params: {id: timeline.to_param, timeline: invalid_attributes}, session: valid_session
         expect(response).to be_success
+        expect(response).to render_template('edit')
       end
     end
   end
@@ -138,4 +258,30 @@ RSpec.describe TimelinesController, type: :controller do
     end
   end
 
+  describe 'POST #duplicate' do
+    before do
+      login_as :user
+    end
+    let(:new_attributes) do
+      { title: Faker::Lorem.word, visibility: Timeline::PUBLIC, description: Faker::Lorem.sentence, user: user }
+    end
+    let(:timeline) { FactoryBot.create(:timeline, new_attributes) }
+
+    it 'duplicates a timeline' do
+      post :duplicate, params: { format: 'json', old_timeline_id: timeline.id, timeline: { 'title' => timeline.title, 'description' => timeline.description, 'visibility' => timeline.visibility } }
+      expect(response.body).not_to be_empty
+      parsed_response = JSON.parse(response.body)
+
+      new_timeline = Timeline.find(parsed_response['timeline']['id'])
+
+      expect(new_timeline.id).not_to eq timeline.id
+      expect(new_timeline.user_id).to eq timeline.user_id
+      expect(new_timeline.visibility).to eq timeline.visibility
+      expect(new_timeline.title).to eq timeline.title
+      expect(new_timeline.description).to eq timeline.description
+      expect(new_timeline.manifest).to eq timeline.manifest
+      expect(new_timeline.source).to eq timeline.source
+      expect(new_timeline.tags).to eq timeline.tags
+    end
+  end
 end
