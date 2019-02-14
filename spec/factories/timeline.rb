826c834d210515b955cf0de5ac5a87148e11355f
Chris Colvard <cjcolvar@indiana.edu>
2019-02-14 10:41:36 -0500

    Add missing factory.

---
+++ spec/factories/timeline.rb
@@ -0,0 +1,29 @@
+# Copyright 2011-2018, The Trustees of Indiana University and Northwestern
+#   University.  Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+#   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+#   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+#   specific language governing permissions and limitations under the License.
+# ---  END LICENSE_HEADER BLOCK  ---
+
+FactoryBot.define do
+  factory :timeline do
+    user { FactoryBot.create(:user) }
+    title { Faker::Lorem.word }
+    description { Faker::Lorem.sentence }
+    visibility { Playlist::PRIVATE }
+    manifest { {}.to_json }
+    tags { [Faker::Lorem.word, Faker::Lorem.word] }
+
+    trait :with_access_token do
+      visibility { Playlist::PRIVATE_WITH_TOKEN }
+      access_token { Faker::Lorem.characters(10) }
+    end
+  end
+end
