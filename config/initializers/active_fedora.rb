7eaca030aa07c66af13dc4259aac49e168721d3d
Chris Colvard <cjcolvar@indiana.edu>
2019-03-01 19:37:56 -0500

    Another time

--- config/initializers/active_fedora.rb
+++ config/initializers/active_fedora.rb
@@ -1,4 +1,5 @@
 baseparts = 2 + [(Noid::Rails.config.template.gsub(/\.[rsz]/, '').length.to_f / 2).ceil, 4].min
+baseurl = "#{ActiveFedora.fedora.host}#{ActiveFedora.fedora.base_path}"
 ActiveFedora::Base.translate_uri_to_id = lambda do |uri|
                                            uri.to_s.sub(baseurl, '').split('/', baseparts).last
                                          end
