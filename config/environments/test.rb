072018043b5882f25481eb35473a1eebe694d85d
Chris Colvard <cjcolvar@indiana.edu>
2019-03-01 23:31:54 -0500

    Fix last failing test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   config.action_view.raise_on_missing_translations = true
+
+  config.active_record.sqlite3.represent_boolean_as_integer = true
 end
