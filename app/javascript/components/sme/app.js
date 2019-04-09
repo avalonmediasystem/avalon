9def95f60d53900d3a7d762b26149efecf405c54
Chris Colvard <cjcolvar@indiana.edu>
2019-04-09 13:38:22 -0400

    WIP

--- app/javascript/components/sme/app.js
+++ app/javascript/components/sme/app.js
@@ -1,9 +1,10 @@
 // app/javascript/structural-metadata-editor/index.js
 import React from 'react'
 import Test from './test';
+import RSME from 'react-structural-metadata-editor';
 
 const App = props => {
-  return <Test />
+  return <RSME />
 };
 
 export default App;
