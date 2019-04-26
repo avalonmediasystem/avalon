76b2cb52e395f294b50865c57601a58c2b57a516
cjcolvar <cjcolvar@indiana.edu>
2019-04-26 11:18:23 -0400

    Prepare for 6.5 release

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'resolv-replace'
 Bundler.require(*Rails.groups)
 
 module Avalon
-  VERSION = '6.4.5'
+  VERSION = '6.5.0'
 
   class Application < Rails::Application
     require 'avalon/configuration'
