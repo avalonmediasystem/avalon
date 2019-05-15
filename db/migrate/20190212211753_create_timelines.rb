f55d4a16b4ce6efb4ee3568b7ac54134ab62851d
Chris Colvard <cjcolvar@indiana.edu>
2019-05-15 10:59:28 -0400

    Specify type of foreign key to ensure match in mysql

--- db/migrate/20190212211753_create_timelines.rb
+++ db/migrate/20190212211753_create_timelines.rb
@@ -2,7 +2,7 @@ class CreateTimelines < ActiveRecord::Migration[5.1]
   def change
     create_table :timelines do |t|
       t.string :title
-      t.references :user, foreign_key: true
+      t.references :user, foreign_key: true, type: :integer
       t.string :visibility
       t.text :description
       t.string :access_token
