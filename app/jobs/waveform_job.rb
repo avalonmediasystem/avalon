6a0bf27e12fface650d68d644dbc99277a28759f
Brian Keese <bkeese@indiana.edu>
2019-08-14 15:17:21 -0400

    Compress waveforms for space. Serve compressed waveforms if supported.

--- app/jobs/waveform_job.rb
+++ app/jobs/waveform_job.rb
@@ -28,7 +28,7 @@ class WaveformJob < ActiveJob::Base
     json = service.get_waveform_json(uri)
     return unless json.present?
 
-    master_file.waveform.content = json
+    master_file.waveform.content = Zlib::Deflate.deflate(json)
     master_file.waveform.mime_type = 'application/json'
     master_file.waveform.content_will_change!
     master_file.save
