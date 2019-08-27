6d1d863ff40165f1c7a431d1c9f1980b85dfa1d3
Dananji <dan9131@gmail.com>
2019-08-27 14:27:24 -0400

    Fix post url in the rails form

--- app/assets/javascripts/move_section.js
+++ app/assets/javascripts/move_section.js
@@ -1,19 +1,17 @@
 var errorMessage = '<p class="invalid-feedback">Please enter a valid ID</p>';
 
+function showMoveModal(obj) {
+  $('#move_modal').show();
+  var id = $(obj).data('id');
+  // Set the URL for form POST action
+  $('#move_form').attr('action', '/master_files/' + id + '/move');
+}
+
 $('#move_modal').on('shown.bs.modal', function() {
   $('#target').focus();
-  $('#target').addClass('is-invalid');
-  $('#show_target_object').html(errorMessage);
-  var masterFileID = $('#show_move_modal').data('id');
-  $('#masterfile_id').val(masterFileID);
 });
 
-$('#move_modal').on('hidden.bs.modal', function(e) {
-  $('#target').className = '';
-  $('#move_form')[0].reset();
-});
-
-function searchMediaObject(obj) {
+function previewTargetItem(obj) {
   var container = $('#show_target_object');
   var moid = obj.value;
   if (moid.length < 8) {
@@ -40,7 +38,7 @@ function searchMediaObject(obj) {
 
 function buildItemDetails(json) {
   var html = [
-    '<h3>' + json.title + '</h3>',
+    '<br /><h4>' + json.title + '</h4>',
     '<p> In ' + json.collection + '</p>'
   ];
   if (json.main_contributors.length > 0) {
@@ -58,9 +56,19 @@ function buildItemDetails(json) {
   return html;
 }
 
+// Add and remove CSS based on user input
 function toggleCSS(el, addCls, removeCls) {
   el.removeClass(removeCls);
   if (!el.hasClass(addCls)) {
     el.addClass(addCls);
   }
 }
+
+// Reset modal on close
+$('#move_modal').on('hidden.bs.modal', function(e) {
+  $('#move_form')[0].reset();
+  $('#show_target_object').html(errorMessage);
+  toggleCSS($('#target'), '', 'is-valid');
+  toggleCSS($('#target'), '', 'is-invalid');
+  $('#move_action_btn').prop('disabled', true);
+});
