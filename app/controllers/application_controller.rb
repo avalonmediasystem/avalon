avalonmediasystem/avalon

commit 33a932c150880ee124f447d6cc80f3d694ca7ab6

Ensure the user is redirected to original page if required to login first

app/controllers/application_controller.rb
@@ -160,4 +160,14 @@ class ApplicationController < ActionController::Base
       user_params.permit(:login, :password)
     end
   end
+
+  def authenticate_user!
+    return if user_signed_in?
+    if request.format == :json
+      head :unauthorized
+    else
+      session[:previous_url] = request.fullpath unless request.xhr?
+      redirect_to new_user_session_path(url: request.url), flash: { notice: 'You need to login to perform this action.' }
+    end
+  end
 end

app/controllers/admin/collections_controller.rb
@@ -199,7 +199,7 @@ class Admin::CollectionsController < ApplicationController
   # GET /collections/1/remove
   def remove
     @collection = Admin::Collection.find(params['id'])
-    raise CanCan::AccessDenied unless current_ability.can? :destroy, @collection
+    authorize! :destroy, @collection
     @objects    = @collection.media_objects
     @candidates = get_user_collections.reject { |c| c == @collection }
   end

app/controllers/admin/groups_controller.rb
@@ -18,15 +18,10 @@ class Admin::GroupsController < ApplicationController
   before_action :auth
 
   # Currently assumes that to do anything you have to be able to manage Group
-  # TODO: finer controls
   def auth
-    if current_user.nil?
-      flash[:notice] = "You need to login to manage groups"
-      redirect_to new_user_session_path
-    elsif cannot? :manage, Admin::Group
-      flash[:notice] = "You do not have permission to manage groups"
-      redirect_to root_path
-    elsif params['id'].present?
+    authorize! :manage, Admin::Group
+    if params['id'].present?
+      # Replace this with authorize! ?
       g = Admin::Group.find(params['id'])
       if cannot? :manage, g
         flash[:error] = "You must be an administrator to manage the '#{g.name}' group"

app/controllers/dropbox_controller.rb
@@ -17,9 +17,7 @@ class DropboxController < ApplicationController
 
   def bulk_delete
     @collection = Admin::Collection.find(params[:collection_id])
-    unless can? :destroy, @collection
-      raise CanCan::AccessDenied
-    end
+    authorize! :destroy, @collection
 
     # failsafe for spaces that might be attached to string
     filenames = params[:filenames].map(&:strip)
@@ -30,7 +28,7 @@ class DropboxController < ApplicationController
 
     filenames.each do |filename|
       if dropbox_filenames.include?( filename )
-        if dropbox.delete( filename ) 
+        if dropbox.delete( filename )
           deleted_filenames << filename
           logger.info "The user #{current_user.username} deleted #{filename} from the dropbox."
         end
@@ -38,8 +36,7 @@ class DropboxController < ApplicationController
         logger.warn "The user #{current_user.username} attempted to delete #{filename} from the dropbox. File does not exist."
       end
     end
-    
+
     render :json => { deleted_filenames: deleted_filenames }
   end
-  
 end

app/controllers/media_objects_controller.rb
@@ -343,7 +343,7 @@ class MediaObjectsController < ApplicationController
 
   def show_stream_details
     load_current_stream
-    raise CanCan::AccessDenied unless current_ability.can? :read, @currentStream
+    authorize! :read, @currentStream
     render json: @currentStreamInfo
   end
 

app/controllers/migration_status_controller.rb
@@ -62,13 +62,7 @@ class MigrationStatusController < ApplicationController
   end
 
   def auth
-    if current_user.nil?
-      flash[:notice] = "You need to login to view migration reports"
-      redirect_to new_user_session_path
-    elsif cannot? :read, MigrationStatus
-      flash[:notice] = "You do not have permission to view migration reports"
-      redirect_to root_path
-    end
+    authorize! :read, MigrationStatus
   end
 
   def without_layout_if_xhr

spec/controllers/avalon_marker_controller_spec.rb
@@ -29,8 +29,8 @@ describe AvalonMarkerController, type: :controller do
     let(:playlist_item) { FactoryBot.create(:playlist_item, playlist: playlist) }
     context 'with unauthenticated user' do
       it "all routes should redirect to sign in" do
-        expect(post :create, params: { marker: { playlist_item_id: playlist_item.id, master_file_id: master_file.id, title: Faker::Lorem.word, start_time: 0.0 } }).to redirect_to(new_user_session_path)
-        expect(put :update, params: { id: avalon_marker.id, marker: { title: Faker::Lorem.word } }).to redirect_to(new_user_session_path)
+        expect(post :create, params: { marker: { playlist_item_id: playlist_item.id, master_file_id: master_file.id, title: Faker::Lorem.word, start_time: 0.0 } }).to redirect_to(/#{Regexp.quote(new_user_session_path)}\?url=.*/)
+        expect(put :update, params: { id: avalon_marker.id, marker: { title: Faker::Lorem.word } }).to redirect_to(/#{Regexp.quote(new_user_session_path)}\?url=.*/)
       end
     end
     context 'with end-user' do

spec/controllers/groups_controller_spec.rb
@@ -20,21 +20,50 @@ describe Admin::GroupsController do
   test_group = "rspec_test_group"
   test_group_new = "rspec_test_group_new"
 
+  describe 'index' do
+    it "index should redirect to sign in page with a notice when unauthenticated" do
+      get 'index'
+      expect(flash[:notice]).not_to be_nil
+      expect(response).to redirect_to(/#{Regexp.quote(new_user_session_path)}\?url=.*/)
+    end
+
+    it "index new should redirect to home page with a notice when authenticated but unauthorized" do
+      login_as('student')
+      get 'index'
+      expect(response).to redirect_to(root_path)
+      expect(flash[:notice]).not_to be_nil
+    end
+  end
+
   describe "creating a new group" do
-  	it "should redirect to sign in page with a notice when unauthenticated" do
+    it "new should redirect to sign in page with a notice when unauthenticated" do
   	  expect { get 'new' }.not_to change { Admin::Group.all.count }
   	  expect(flash[:notice]).not_to be_nil
-  	  expect(response).to redirect_to(new_user_session_path)
+  	  expect(response).to redirect_to(/#{Regexp.quote(new_user_session_path)}\?url=.*/)
   	end
 
-    it "should redirect to home page with a notice when authenticated but unauthorized" do
+    it "new should redirect to home page with a notice when authenticated but unauthorized" do
       login_as('student')
       expect { get 'new' }.not_to change {Admin::Group.all.count}
       expect(response).to redirect_to(root_path)
       expect(flash[:notice]).not_to be_nil
     end
 
-    it "should redirect to group index page with a notice when group name is already taken" do
+    it "create should redirect to sign in page with a notice on when unauthenticated" do
+      expect { post 'create', params: { admin_group: test_group } }.not_to change {Admin::Group.all.count }
+      expect(flash[:notice]).not_to be_nil
+      expect(response).to redirect_to(/#{Regexp.quote(new_user_session_path)}\?url=.*/)
+    end
+
+    it "create should redirect to home page with a notice when authenticated but unauthorized" do
+      login_as('student')
+
+      expect { post 'create', params: { admin_group: test_group } }.not_to change {Admin::Group.all.count }
+      expect(flash[:notice]).not_to be_nil
+      expect(response).to redirect_to(root_path)
+    end
+
+    it "create should redirect to group index page with a notice when group name is already taken" do
       group = FactoryBot.create(:group)
       login_as('group_manager')
       expect { post 'create', params: { admin_group: group.name } }.not_to change {Admin::Group.all.count }
@@ -62,16 +91,32 @@ describe Admin::GroupsController do
     let!(:group) {Admin::Group.find(FactoryBot.create(:group).name)}
 
     context "editing a group" do
-      it "should redirect to sign in page with a notice on when unauthenticated" do
+      it "edit should redirect to sign in page with a notice on when unauthenticated" do
         get 'edit', params: { id: group.name }
         expect(flash[:notice]).not_to be_nil
-        expect(response).to redirect_to(new_user_session_path)
+        expect(response).to redirect_to(/#{Regexp.quote(new_user_session_path)}\?url=.*/)
       end
 
-      it "should redirect to home page with a notice when authenticated but unauthorized" do
+      it "edit should redirect to home page with a notice when authenticated but unauthorized" do
         login_as('student')
 
         get 'edit', params: { id: group.name }
+        expect(flash[:notice]).not_to be_nil
+        expect(response).to redirect_to(root_path)
+      end
+
+      it "update should redirect to sign in page with a notice on when unauthenticated" do
+        new_user = FactoryBot.build(:user).user_key
+        put 'update', params: { group_name: group.name, id: group.name, new_user: new_user }
+        expect(flash[:notice]).not_to be_nil
+        expect(response).to redirect_to(/#{Regexp.quote(new_user_session_path)}\?url=.*/)
+      end
+
+      it "update should redirect to home page with a notice when authenticated but unauthorized" do
+        login_as('student')
+        new_user = FactoryBot.build(:user).user_key
+
+        put 'update', params: { group_name: group.name, id: group.name, new_user: new_user }
         expect(flash[:notice]).not_to be_nil
         expect(response).to redirect_to(root_path)
       end
@@ -162,7 +207,7 @@ describe Admin::GroupsController do
 
         expect { put 'update_multiple', params: { group_ids: [group.name] } }.not_to change { Avalon::RoleControls.users(group.name) }
         expect(flash[:notice]).not_to be_nil
-        expect(response).to redirect_to(new_user_session_path)
+        expect(response).to redirect_to(/#{Regexp.quote(new_user_session_path)}\?url=.*/)
       end
 
       it "should redirect to home page with a notice when authenticated but unauthorized" do

commit aa04ac04b71fc1b5dfaf83304f8870b815d26313

Invite was broken by 33a932c15, this fixes it

app/controllers/application_controller.rb
@@ -161,7 +161,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def authenticate_user!
+  def authenticate_user!(opts={})
     return if user_signed_in?
     if request.format == :json
       head :unauthorized
@@ -169,5 +169,9 @@ class ApplicationController < ActionController::Base
       session[:previous_url] = request.fullpath unless request.xhr?
       redirect_to new_user_session_path(url: request.url), flash: { notice: 'You need to login to perform this action.' }
     end
+  end
+
+  def after_invite_path_for(inviter, invitee = nil)
+    main_app.persona_users_path
   end
 end