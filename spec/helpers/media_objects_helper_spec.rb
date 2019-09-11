5054d415d1b433f74dfef08c99898979f8289606
cjcolvar <cjcolvar@indiana.edu>
2019-09-11 11:54:44 -0400

    Make test match last commit.

--- spec/helpers/media_objects_helper_spec.rb
+++ spec/helpers/media_objects_helper_spec.rb
@@ -85,7 +85,7 @@ describe MediaObjectsHelper do
     subject { helper.display_rights_statement(media_object) }
 
     it 'links to the rights statement page' do
-      expect(subject).to eq "<a href=\"#{rights_statement_uri}\">#{rights_statement_label}</a>"
+      expect(subject).to eq "<a target=\"_blank\" href=\"#{rights_statement_uri}\">#{rights_statement_label}</a>"
     end
 
     context 'when rights statement is not set' do
