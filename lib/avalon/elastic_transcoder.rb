c986967c5fba281dcf42a1498664ac2986e60e4f
Phuong Dinh <phuongdh@gmail.com>
2019-10-04 12:09:53 -0400

    Replace custom ElasticTranscoder job with active-encode adapter

---
+++ lib/avalon/elastic_transcoder.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+require 'singleton'
+
+module Avalon
+  class ElasticTranscoder
+    include Singleton
+
+    def initialize
+      @etclient = Aws::ElasticTranscoder::Client.new
+    end
+
+    def find_preset(container, format, quality)
+      container_description = container == 'ts' ? 'hls' : container
+      key = "avalon-#{format}-#{quality}-#{container_description}"
+      find_preset_by_name(key)
+    end
+
+    def find_preset_by_name(key)
+      Rails.cache.fetch("transcoder-preset-for-#{key}") do
+        next_token = nil
+        loop do
+          resp = @etclient.list_presets page_token: next_token
+          result = resp.presets.find { |p| p.name == key }
+          next_token = resp.next_page_token
+          break if result.present? || next_token.nil?
+        end
+        result
+      end
+    end
+
+    def create_preset(template)
+      @etclient.create_preset(template)
+    end
+
+    def read_templates(path)
+      templates = YAML.safe_load(File.read(path))
+      et_templates = []
+      [:audio, :video].product([:low, :medium, :high], ['ts', 'mp4']).collect do |format, quality, container|
+        next unless templates[:settings][format][quality].present?
+
+        template = templates[:templates][format].deep_dup.deep_merge(templates[:settings][format][quality])
+        container_description = container == 'ts' ? 'hls' : container
+        template.merge!(
+          name: "avalon-#{format}-#{quality}-#{container_description}",
+          description: "Avalon Media System: #{format}/#{quality}/#{container_description}",
+          container: container
+        )
+
+        et_templates << template
+      end
+    end
+  end
+end
