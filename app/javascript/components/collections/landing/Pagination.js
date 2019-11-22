5d9029f44467cbcf0197237c6025e8c8ad5941b6
Adam Arling <adam.arling@northwestern.edu>
2019-11-22 10:32:09 -0600

    Collection landing cleanup more; remove debounce network request for pagination links and Card styling fixes

--- app/javascript/components/collections/landing/Pagination.js
+++ app/javascript/components/collections/landing/Pagination.js
@@ -14,17 +14,39 @@ function Pagination(props) {
     return Math.min(pages.offset_value + pages.limit_value, pages.total_count);
   };
 
-  const paginationBlock = <div className="sort-pagination mb-3 pull-right">
-                            {props.pages.prev_page != null ?
-                              (<a href="#" onClick={event => handleClick(props.pages.prev_page, event) }>Previous</a>) : ( <span>Previous</span> )}
-                            <span>{' '}| {pageStart(props.pages)}-{pageEnd(props.pages)} of {props.pages.total_count} |{' '}</span>
-                            {props.pages.next_page != null ?
-                              (<a href="#" onClick={event => handleClick(props.pages.next_page, event) }>Next</a>) : ( <span>Next</span> )}
-                          </div>;
+  const paginationBlock = (
+    <div className="sort-pagination mb-3 pull-right">
+      {props.pages.prev_page != null ? (
+        <a
+          href="#"
+          onClick={event => handleClick(props.pages.prev_page, event)}
+        >
+          Previous
+        </a>
+      ) : (
+        <span>Previous</span>
+      )}
+      <span>
+        {' '}
+        | {pageStart(props.pages)}-{pageEnd(props.pages)} of{' '}
+        {props.pages.total_count} |{' '}
+      </span>
+      {props.pages.next_page != null ? (
+        <a
+          href="#"
+          onClick={event => handleClick(props.pages.next_page, event)}
+        >
+          Next
+        </a>
+      ) : (
+        <span>Next</span>
+      )}
+    </div>
+  );
   if (props.pages.total_count) {
     return paginationBlock;
   } else if (props.pages.total_count === 0) {
-    return ( <p className="no-search-results">No results matched your search.</p> );
+    return <p className="no-search-results">No results matched your search.</p>;
   } else {
     return null;
   }
