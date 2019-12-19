5050768907f16561230ec1f79d60a708d4ac77c4
Chris Colvard <cjcolvar@indiana.edu>
2019-12-19 16:47:10 -0500

    Override controller since monkey-patch doesn't work in some environments

--- config/initializers/samvera_persona.rb
+++ config/initializers/samvera_persona.rb
@@ -1,7 +1,3 @@
 Samvera::Persona.setup do |config|
   config.soft_delete = false
 end
-
-Samvera::Persona::UsersController.class_eval do
-  prepend_view_path 'views/samvera/persona/index.html.erb'
-end
