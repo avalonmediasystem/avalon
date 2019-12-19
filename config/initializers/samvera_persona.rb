9d80f3b31d9b2a56fb66e2893d68a82df94e77b6
Chris Colvard <cjcolvar@indiana.edu>
2019-12-19 11:07:17 -0500

    Override persona to customize the table

--- config/initializers/samvera_persona.rb
+++ config/initializers/samvera_persona.rb
@@ -1,3 +1,7 @@
 Samvera::Persona.setup do |config|
   config.soft_delete = false
 end
+
+Samvera::Persona::UsersController.class_eval do
+  prepend_view_path 'views/samvera/persona/index.html.erb'
+end
