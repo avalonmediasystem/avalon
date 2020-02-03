2da4979443a355da50af255f62f3438d2ea7b8cc
pdinh <pdinh@indiana.edu>
2020-02-03 15:46:05 -0500

    Replace resque roles with worker role

--- config/deploy/iu.rb
+++ config/deploy/iu.rb
@@ -3,7 +3,8 @@ set :bundle_flags,  "--with #{ENV['DB']}" if ENV['DB']
 set :bundle_without, fetch(:rails_env) == "development" ? "production" : "development debug test sqlite"
 set :repo_url, ENV['REPO'] || 'git@github.iu.edu:AvalonIU/Avalon.git'
 set :branch, ENV['SCM_BRANCH'] || "mco-staging"
+set :yarn_roles, :worker
 set :deploy_to, ENV['DEPLOY_TO'] || "/srv/avalon/avalon_r6"
-server ENV['HOST'] || 'sodium.dlib.indiana.edu', roles: %w{web app resque_worker resque_scheduler}, user: ENV['DEPLOY_USER'] || 'avalon'
+server ENV['HOST'] || 'sodium.dlib.indiana.edu', roles: %w{web app worker}, user: ENV['DEPLOY_USER'] || 'avalon'
 ssh_options[:keys] = ["/opt/staging/avalon/vov_deployment_key"] if ENV['CI_DEPLOY']
 set :workers, { "*" => 2 }
