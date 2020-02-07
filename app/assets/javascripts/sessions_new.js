05f37939c3a46623e8251fe96c35fdc5aedb1b2a
Dananji <dan9131@gmail.com>
2020-02-07 11:28:23 -0500

    Demo site improvements

--- app/assets/javascripts/sessions_new.js
+++ app/assets/javascripts/sessions_new.js
@@ -14,16 +14,16 @@
  * ---  END LICENSE_HEADER BLOCK  ---
 */
 
-$( document ).ready(function() {
-  $(document).on('click', 'a[data-trigger="show-email"]', function(event){
-    $('#email-box').toggleClass('hidden')
-    $('#sign-in-select').toggleClass('hidden')
-    $('#sign-in-buttons').toggleClass('hidden')
-  })
-  let searchParams = new URLSearchParams(window.location.search)
-  if(searchParams.has('email')){
-    $('#email-box').toggleClass('hidden')
-    $('#sign-in-select').toggleClass('hidden')
-    $('#sign-in-buttons').toggleClass('hidden')
-  }
-})
+// $( document ).ready(function() {
+//   $(document).on('click', 'a[data-trigger="show-email"]', function(event){
+//     $('#email-box').toggleClass('hidden')
+//     $('#sign-in-select').toggleClass('hidden')
+//     $('#sign-in-buttons').toggleClass('hidden')
+//   })
+//   let searchParams = new URLSearchParams(window.location.search)
+//   if(searchParams.has('email')){
+//     $('#email-box').toggleClass('hidden')
+//     $('#sign-in-select').toggleClass('hidden')
+//     $('#sign-in-buttons').toggleClass('hidden')
+//   }
+// })
