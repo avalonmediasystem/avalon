1a5232a2c324706a3b10aa52d8640d4dad4eba34
phuongdh <phuongdh@gmail.com>
2020-03-06 14:39:00 -0500

    Fix ActionMailer not using smtp configs

--- config/initializers/mailer.rb
+++
@@ -1,5 +0,0 @@
-if Settings&.email&.mailer&.to_sym == :aws_sdk
-  require 'aws-sdk-rails'
-  Aws::Rails.add_action_mailer_delivery_method(:aws_sdk)
-  ActionMailer::Base.delivery_method = :aws_sdk
-end
