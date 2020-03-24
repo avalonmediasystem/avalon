97ac9e72fdfd6dc81e149ca9b612af34a7f8f445
Chris Colvard <cjcolvar@indiana.edu>
2020-03-24 15:02:25 -0400

    After the derivative object is destroyed queue a job which deletes the actual file from the streaming server

---
+++ app/jobs/delete_derivative_job.rb
@@ -0,0 +1,35 @@
+# Copyright 2011-2020, The Trustees of Indiana University and Northwestern
+#   University.  Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+#   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+#   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+#   specific language governing permissions and limitations under the License.
+# ---  END LICENSE_HEADER BLOCK  ---
+
+require 'fileutils'
+
+class DeleteDerivativeJob < ActiveJob::Base
+  queue_as :delete_derivative
+
+  def perform(path)
+    Rails.logger.debug "Attempting to delete derivative #{path}"
+
+    path = derivative.absolute_location
+    locator = FileLocator.new(path)
+    if locator.exists?
+      case locator.uri.scheme
+      when 'file' then File.delete(path)
+      when 's3'   then FileLocator::S3File.new(locator.source).object.delete
+      end
+      Rails.logger.info "#{path} has been deleted"
+    else
+      Rails.logger.warn "MasterFile #{path} does not exist"
+    end
+  end
+end
