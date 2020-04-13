a4fd7f8278ea06b9250180f3a72700a13de28dd9
Dananji <dan9131@gmail.com>
2020-04-13 14:36:51 -0400

    Refine inline form edit for label

--- app/assets/javascripts/supplemental_files.js
+++ app/assets/javascripts/supplemental_files.js
@@ -1,37 +1,40 @@
 
-  $('button[name="edit_label"]').on('click', e => {
-    const $row = $(e.target).parents('.row');
-    const fileId = $row.data('file-id');
-    console.log(fileId);
-
-    $(e.target)
-      .parents('.row')
-      .addClass('is-editing');
-  });
+$('button[name="edit_label"]').on('click', e => {
+  const { $row, fileId, masterFileId } = getHTMLInfo(e);
+  const inputField = $row.find('input[name="label_' + masterFileId + '_' + fileId + '"]');
+  inputField.val($row.data('file-label'));
+  $row.addClass('is-editing');
+  inputField.focus();
+});
 
 $('button[name="label_edit_cancel"]').on('click', e => {
-    let $row = $(e.target).parents('.row');
-    const fileId = $row.data('file-id');
-
-    $row.removeClass('is-editing');
+  const { $row, fileId, masterFileId } = getHTMLInfo(e);
+  $row.find('input[name="label_' + masterFileId + '_' + fileId + '"]');
+  $row.removeClass('is-editing');
   });
 
 $('button[name="save_label"]').on('click', e => {
-    const $row = $(e.target).parents('.row');
-    const fileId = $row.data('file-id');
-    const masterFileId = $row.data('masterfile-id');
-    const newLabel = $row.find('input[name="offset_' + fileId + '"]').val();
+  const {  $row, fileId, masterFileId } = getHTMLInfo(e);
+  const newLabel = $row.find('input[name="label_' + masterFileId + '_' + fileId + '"]').val();
+  $row.find('span[name="label_' + masterFileId + '_' + fileId + '"]').text(newLabel);
 
-    let formData = new FormData();
-      formData.append('supplemental_file[label]', newLabel);
-      formData.append('authenticity_token', $('input[name=authenticity_token]').val());
+  let formData = new FormData();
+  formData.append('supplemental_file[label]', newLabel);
+  formData.append('authenticity_token', $('input[name=authenticity_token]').val());
 
-    fetch('/master_files/' + masterFileId + '/supplemental_files/' + fileId, {
-        method: "PUT",
-        body: formData
-      }).then(() => {
-        // Page reload to show the flash message
-        $row.removeClass('is-editing');
-        location.reload();
-      });
+  fetch('/master_files/' + masterFileId + '/supplemental_files/' + fileId, {
+    method: "PUT",
+    body: formData
+  }).then(() => {
+    $row.removeClass('is-editing');
+    // Page reload to show the flash message
+    location.reload();
   });
+});
+
+function getHTMLInfo(e) {
+  const $row = $(e.target).parents('.row');
+  const fileId = $row.data('file-id');
+  const masterFileId = $row.data('masterfile-id');
+  return {  $row, fileId, masterFileId };
+}
