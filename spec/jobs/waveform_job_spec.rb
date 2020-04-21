25b244aa370f18d654c46c0a07bc5abfef745eee
phuongdh <phuongdh@gmail.com>
2020-04-21 15:46:36 -0400

    Attempt to fetch Derivative.absolute_location in WaveformJob

--- spec/jobs/waveform_job_spec.rb
+++ spec/jobs/waveform_job_spec.rb
@@ -89,9 +89,8 @@ describe WaveformJob do
     context 'when processing fails to generate waveform data' do
       let(:waveform_json) { nil }
 
-      it 'logs and does not set the waveform' do
-        expect(Rails.logger).to receive(:error).at_least(:once)
-        expect { job.perform(master_file.id, true) }.not_to change { master_file.reload.waveform.content }
+      it 'raises error and does not set the waveform' do
+        expect { job.perform(master_file.id, true) }.to raise_error.and not_change { master_file.reload.waveform.content }
         expect(service).to have_received(:get_waveform_json)
       end
     end
