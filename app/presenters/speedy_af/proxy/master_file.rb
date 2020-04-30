15ecb3a6c7fd7a95e31551b9f30441c286b2eec7
Chris Colvard <cjcolvar@indiana.edu>
2020-04-30 12:41:34 -0400

    Add tests for SpeedyAF MasterFile Proxy; fix #encoder_class

--- app/presenters/speedy_af/proxy/master_file.rb
+++ app/presenters/speedy_af/proxy/master_file.rb
@@ -14,7 +14,11 @@
 
 class SpeedyAF::Proxy::MasterFile < SpeedyAF::Base
   def encoder_class
-    find_encoder_class(encoder_classname) || find_encoder_class(workflow_name.to_s.classify) || find_encoder_class((Settings.encoding.engine_adapter + "_encode").classify) || MasterFile.default_encoder_class || WatchedEncode
+    find_encoder_class(encoder_classname) ||
+      find_encoder_class("#{workflow_name}_encode".classify) ||
+      find_encoder_class((Settings.encoding.engine_adapter + "_encode").classify) ||
+      MasterFile.default_encoder_class ||
+      WatchedEncode
   end
 
   def find_encoder_class(klass_name)
