84aa0e5bd704d9bbcc364243a104e1af2a26e963
Chris Colvard <cjcolvar@indiana.edu>
2020-05-18 14:25:21 -0400

    Simply and avoid error handling by using safe_constantize

--- app/models/master_file.rb
+++ app/models/master_file.rb
@@ -750,9 +750,8 @@ class MasterFile < ActiveFedora::Base
   end
 
   def find_encoder_class(klass_name)
-    return nil if klass_name.blank? || !Object.const_defined?(klass_name)
-    klass = Object.const_get(klass_name)
-    return klass if klass.ancestors.include?(ActiveEncode::Base)
+    klass = klass_name&.safe_constantize
+    klass if klass&.ancestors&.include?(ActiveEncode::Base)
   end
 
   def stop_processing!
