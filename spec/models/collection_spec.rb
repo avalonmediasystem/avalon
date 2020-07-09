038323328c87d771c13dd8f25e6378ba23e6f3ae
Dananji <dan9131@gmail.com>
2020-07-09 17:23:38 -0400

    [WIP] Delete dropbox directory when deleting a collection

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -549,6 +549,16 @@ describe Admin::Collection do
     end
   end
 
+  # describe '#destroy_dropbox_directory!' do
+  #   let(:collection){ FactoryBot.build(:collection)}
+
+  #   it 'deletes S3 dropbox directory' do
+  #     collection.name = 'black history'
+  #     collection.send(:destroy_s3_dropbox_directory!)
+  #     expect(File.directory?(File.join(Settings.dropbox.path, 'black_history')).to_be falsey)
+  #   end
+  # end
+
   describe 'Unicode' do
     let(:collection_name) { "Collections & Favorites / \u6211\u7684\u6536\u85cf / \u03a4\u03b1 \u03b1\u03b3\u03b1\u03c0\u03b7\u03bc\u03ad\u03bd\u03b1 \u03bc\u03bf\u03c5" }
     let(:collection_dir)  { "Collections___Favorites___\u6211\u7684\u6536\u85cf___\u03a4\u03b1_\u03b1\u03b3\u03b1\u03c0\u03b7\u03bc\u03ad\u03bd\u03b1_\u03bc\u03bf\u03c5" }
