3d3b2170eedaf18f89e74ee55563b4cdf65b9058
Phuong Dinh <phuongdh@gmail.com>
2020-09-14 10:15:24 -0400

    Display access for leases and add tests

--- app/models/media_object.rb
+++ app/models/media_object.rb
@@ -351,17 +351,19 @@ class MediaObject < ActiveFedora::Base
     if visibility == "public"
       actors << "the public"
     else
+      actors << "collection staff" if visibility == "private"
+      actors << "specific users" if read_users.any? || leases('user').any?
+
       if visibility == "restricted"
         actors << "logged-in users"
-      elsif virtual_read_groups.any?
-        actors << "users in specific LDAP groups" 
+      elsif virtual_read_groups.any? || local_read_groups.any? || leases('external').any? || leases('local').any?
+        actors << "users in specific groups"
       end
 
-      actors << "collection staff" if visibility ==  "private"
-      actors << "users in specific IP Ranges" if ip_read_groups.any?
+      actors << "users in specific IP Ranges" if ip_read_groups.any? || leases('ip').any?
     end
 
-    "This item is accessible by: #{ actors.join(", ") }"
+    "This item is accessible by: #{actors.join(', ')}."
   end
 
   private
