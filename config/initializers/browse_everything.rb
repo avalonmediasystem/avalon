0fc3f49379930979e82940bcb3130d09a8a1bc9d
Phuong Dinh <phuongdh@gmail.com>
2020-09-21 12:02:55 -0400

    Update mediainfo and active-encode for Google Drive

--- config/initializers/browse_everything.rb
+++ config/initializers/browse_everything.rb
@@ -1,10 +1,12 @@
-# settings = if Settings.dropbox.path =~ %r{^s3://}
-#   obj = FileLocator::S3File.new(Settings.dropbox.path).object
-#   { 's3' => { name: 'AWS S3 Dropbox', bucket: obj.bucket_name, base: obj.key, response_type: :s3_uri } }
-# else
-#   { 'file_system' => { name: 'File Dropbox', home: Settings.dropbox.path } }
-# end
-# BrowseEverything.configure(settings)
+settings = if Settings.dropbox.google_drive
+  { 'google_drive' => { client_id: Settings.dropbox.google_drive.client_id, client_secret: Settings.dropbox.google_drive.client_secret } }
+elsif Settings.dropbox.path =~ %r{^s3://}
+  obj = FileLocator::S3File.new(Settings.dropbox.path).object
+  { 's3' => { name: 'AWS S3 Dropbox', bucket: obj.bucket_name, base: obj.key, response_type: :s3_uri } }
+else
+  { 'file_system' => { name: 'File Dropbox', home: Settings.dropbox.path } }
+end
+BrowseEverything.configure(settings)
 
 # Patch BrowseEverything to get the correct download URL
 # TODO: use newer version (>= 0.16.0)
