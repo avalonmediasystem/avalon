6efc7c208ea54b1d2462f5a73ec216fad4e9084b
Phuong Dinh <phuongdh@gmail.com>
2020-12-11 13:58:13 -0500

    Fix Google Drive stale token and mediainfo issues

--- config/initializers/browse_everything.rb
+++ config/initializers/browse_everything.rb
@@ -7,23 +7,3 @@ else
   { 'file_system' => { name: 'File Dropbox', home: Settings.dropbox.path } }
 end
 BrowseEverything.configure(settings)
-
-# Patch BrowseEverything to get the correct download URL
-# TODO: use newer version (>= 0.16.0)
-BrowseEverything::Driver::GoogleDrive.class_eval do
-  def link_for(id)
-    file = drive.get_file(id, fields: "id, name, size, mimeType, videoMediaMetadata")
-    auth_header = { 'Authorization' => "Bearer #{auth_client.access_token}" }
-    extras = {
-      auth_header: auth_header,
-      expires: 3.hour.from_now,
-      file_name: file.name,
-      file_size: file.size.to_i
-    }
-    [download_url(id), extras]
-  end
-
-  def download_url(id)
-    "https://www.googleapis.com/drive/v3/files/#{id}?alt=media"
-  end
-end
