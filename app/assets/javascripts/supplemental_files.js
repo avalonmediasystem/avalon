200c728e4272ba33477dfe9895a5635de6fac318
Dananji <dan9131@gmail.com>
2021-02-11 17:38:10 -0500

    Show an inline flash message when supplemental file label is updated

--- app/assets/javascripts/supplemental_files.js
+++ app/assets/javascripts/supplemental_files.js
@@ -1,40 +1,59 @@
+/*
+ * Copyright 2011-2020, The Trustees of Indiana University and Northwestern
+ *   University.  Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ *   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *   specific language governing permissions and limitations under the License.
+ * ---  END LICENSE_HEADER BLOCK  ---
+*/
 
+/** Includes JS functions related to editing the supplemental file labels */
+
+/* Show form to edit label */
 $('button[name="edit_label"]').on('click', e => {
-  const { $row, fileId, masterFileId } = getHTMLInfo(e);
-  const inputField = $row.find('input[name="label_' + masterFileId + '_' + fileId + '"]');
-  inputField.val($row.data('file-label'));
+  const $row = getHTMLInfo(e, '.row');
+  const fileId = $row.data('file-id');
+  const masterfileId = $row.data('masterfile-id');
+  $row.find('small[name="flash-message-' + masterfileId + '-' + fileId + '"]').html('');
   $row.addClass('is-editing');
-  inputField.focus();
+  $row.find('input[id="supplemental_file_input_' + masterfileId + '_'+ fileId +'"]').focus();
 });
 
+/* Hide form when form is cancelled */
 $('button[name="cancel_edit_label"]').on('click', e => {
-  const { $row, fileId, masterFileId } = getHTMLInfo(e);
-  $row.find('input[name="label_' + masterFileId + '_' + fileId + '"]');
+  const $row = getHTMLInfo(e, '.row');
   $row.removeClass('is-editing');
   });
 
+/* After editing, close the form and show the new label */
 $('button[name="save_label"]').on('click', e => {
-  const {  $row, fileId, masterFileId } = getHTMLInfo(e);
-  const newLabel = $row.find('input[name="label_' + masterFileId + '_' + fileId + '"]').val();
-  $row.find('span[name="label_' + masterFileId + '_' + fileId + '"]').text(newLabel);
+  const $row = getHTMLInfo(e, '.row');
+  const { fileId, masterfileId } = getHTMLInfo(e, 'form')[0].dataset;
 
-  let formData = new FormData();
-  formData.append('supplemental_file[label]', newLabel);
-  formData.append('authenticity_token', $('input[name=authenticity_token]').val());
-
-  fetch('/master_files/' + masterFileId + '/supplemental_files/' + fileId, {
-    method: "PUT",
-    body: formData
-  }).then(() => {
-    $row.removeClass('is-editing');
-    // Page reload to show the flash message
-    location.reload();
-  });
+  const newLabel = $row.find('input[id="supplemental_file_input_' + masterfileId + '_'+ fileId +'"]').val();
+  // Set the label to the new value
+  $row.find('span[name="label_' + masterfileId + '_' + fileId + '"]').text(newLabel);
+  $row.removeClass('is-editing');
+  // Remove feedback message after 5 seconds
+  setTimeout( function() { 
+    $row.find('small[name="flash-message-' + masterfileId + '-' + fileId + '"]').html('');
+}, 5000);
 });
 
-function getHTMLInfo(e) {
-  const $row = $(e.target).parents('.row');
-  const fileId = $row.data('file-id');
-  const masterFileId = $row.data('masterfile-id');
-  return {  $row, fileId, masterFileId };
+/* Show feedback message inline after saving */
+$('.supplemental-file-form').on("ajax:success", (event, data, status, xhr) => {
+  $(event.currentTarget.parentElement).find('.visible-inline').html("Supplemental file successfully updated.")
+}).on("ajax:error", (event, xhr, status, error) => {
+  $(event.currentTarget.parentElement).find('.visible-inline').html("Failed to update.")
+});
+
+function getHTMLInfo(e, element) {
+  return $(e.target).parents(element);
 }
