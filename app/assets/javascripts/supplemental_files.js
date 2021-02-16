35a7fcedb3ad7d5c982bdfd8c79361406bba69e5
Dananji <dan9131@gmail.com>
2021-02-16 08:53:51 -0500

    Remember opened sections on page reload

--- app/assets/javascripts/supplemental_files.js
+++ app/assets/javascripts/supplemental_files.js
@@ -19,9 +19,8 @@
 /* Show form to edit label */
 $('button[name="edit_label"]').on('click', e => {
   const $row = getHTMLInfo(e, '.row');
-  const fileId = $row.data('file-id');
-  const masterfileId = $row.data('masterfile-id');
-  $row.find('small[name="flash-message-' + masterfileId + '-' + fileId + '"]').html('');
+  const { masterfileId, fileId } = $row[0].dataset;
+
   $row.addClass('is-editing');
   $row.find('input[id="supplemental_file_input_' + masterfileId + '_'+ fileId +'"]').focus();
 });
@@ -37,22 +36,68 @@ $('button[name="save_label"]').on('click', e => {
   const $row = getHTMLInfo(e, '.row');
   const { fileId, masterfileId } = getHTMLInfo(e, 'form')[0].dataset;
 
-  const newLabel = $row.find('input[id="supplemental_file_input_' + masterfileId + '_'+ fileId +'"]').val();
-  // Set the label to the new value
-  $row.find('span[name="label_' + masterfileId + '_' + fileId + '"]').text(newLabel);
   $row.removeClass('is-editing');
+
   // Remove feedback message after 5 seconds
   setTimeout( function() { 
-    $row.find('small[name="flash-message-' + masterfileId + '-' + fileId + '"]').html('');
+    var alert = $row.find('small[name="flash-message-' + masterfileId + '-' + fileId + '"]')
+    alert.html('');
+    alert.removeClass();
+    alert.addClass('visible-inline');
 }, 5000);
 });
 
 /* Show feedback message inline after saving */
 $('.supplemental-file-form').on("ajax:success", (event, data, status, xhr) => {
-  $(event.currentTarget.parentElement).find('.visible-inline').html("Supplemental file successfully updated.")
+  var $row = $(event.currentTarget.parentElement);
+  const { masterfileId, fileId } = event.currentTarget.dataset;
+
+  // Set the label to the new value
+  var newLabel = $row.find('input[id="supplemental_file_input_' + masterfileId + '_' + fileId + '"]').val()
+  $row.find('span[name="label_' + masterfileId + '_' + fileId + '"]').text(newLabel);
+
+  // Show flash message for success
+  $row.find('.visible-inline').html("Supplemental file successfully updated.");
+  $row.find('.visible-inline').addClass('success-alert alert');
 }).on("ajax:error", (event, xhr, status, error) => {
-  $(event.currentTarget.parentElement).find('.visible-inline').html("Failed to update.")
+  var alert = $(event.currentTarget.parentElement).find('.visible-inline');
+
+  // Show flash warning for failed attempt
+  alert.html("Failed to update.");
+  alert.addClass('warning-alert alert');
 });
+
+/* Store collapsed section ids in localStorage */
+$('button[id^=edit_section').on('click', e => {
+  // Active sections
+  var activeSections = JSON.parse(localStorage.getItem('activeSections')) || [];
+
+  var currentSection = e.target.dataset['sectionId'];
+  var isCollapsed = e.target.getAttribute('aria-expanded') == "true";
+  if(isCollapsed) {
+    for (var i = 0; i < activeSections.length; i++) {
+      if (activeSections[i] == currentSection) {
+        activeSections.splice(i, 1);
+      }
+    }
+  } else {
+    activeSections.push(currentSection);
+  }
+  localStorage.setItem('activeSections', JSON.stringify(activeSections));
+});
+
+/* On page reload; collapse sections which were collapsed previously */
+$(document).ready(function() {
+  var activeSections = JSON.parse(localStorage.getItem('activeSections')) || [];
+
+  // Collapse active sections on page
+  activeSections.forEach(section => {
+    const sectionDiv = $("#collapseExample" + section);
+    if(sectionDiv) {
+      sectionDiv.collapse();
+    }
+  })
+})
 
 function getHTMLInfo(e, element) {
   return $(e.target).parents(element);
