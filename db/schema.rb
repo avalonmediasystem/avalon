ececd3e94d39513c323449747559575b1a0e8c05
Michael Johnson <apertome@gmail.com>
2021-12-21 11:19:43 -0500

    Roll back db/schema.rb (accidentally committed)

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2021_02_02_160327) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "active_encode_encode_records", id: :serial, force: :cascade do |t|
+  create_table "active_encode_encode_records", force: :cascade do |t|
     t.string "global_id"
     t.string "state"
     t.string "adapter"
@@ -36,8 +33,8 @@ ActiveRecord::Schema.define(version: 2021_02_02_160327) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -57,7 +54,7 @@ ActiveRecord::Schema.define(version: 2021_02_02_160327) do
   create_table "annotations", force: :cascade do |t|
     t.string "uuid"
     t.string "source_uri"
-    t.bigint "playlist_item_id"
+    t.integer "playlist_item_id"
     t.text "annotation"
     t.string "type"
     t.index ["playlist_item_id"], name: "index_annotations_on_playlist_item_id"
@@ -75,12 +72,12 @@ ActiveRecord::Schema.define(version: 2021_02_02_160327) do
   end
 
   create_table "batch_entries", force: :cascade do |t|
-    t.bigint "batch_registries_id"
-    t.text "payload"
+    t.integer "batch_registries_id"
+    t.text "payload", limit: 1073741823
     t.boolean "complete", default: false, null: false
     t.boolean "error", default: false, null: false
     t.string "current_status"
-    t.text "error_message"
+    t.text "error_message", limit: 65535
     t.string "media_object_pid"
     t.integer "position"
     t.datetime "created_at", null: false
@@ -160,7 +157,7 @@ ActiveRecord::Schema.define(version: 2021_02_02_160327) do
     t.string "namespace", default: "default", null: false
     t.string "template", null: false
     t.text "counters"
-    t.bigint "seq", default: 0
+    t.integer "seq", default: 0
     t.binary "rand"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -168,8 +165,8 @@ ActiveRecord::Schema.define(version: 2021_02_02_160327) do
   end
 
   create_table "playlist_items", force: :cascade do |t|
-    t.bigint "playlist_id", null: false
-    t.bigint "clip_id", null: false
+    t.integer "playlist_id", null: false
+    t.integer "clip_id", null: false
     t.integer "position"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -179,7 +176,7 @@ ActiveRecord::Schema.define(version: 2021_02_02_160327) do
 
   create_table "playlists", force: :cascade do |t|
     t.string "title"
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.string "comment"
     t.string "visibility"
     t.datetime "created_at", null: false
@@ -227,13 +224,13 @@ ActiveRecord::Schema.define(version: 2021_02_02_160327) do
 
   create_table "timelines", force: :cascade do |t|
     t.string "title"
-    t.bigint "user_id"
+    t.integer "user_id"
     t.string "visibility"
     t.text "description"
     t.string "access_token"
     t.string "tags"
     t.string "source"
-    t.text "manifest"
+    t.text "manifest", limit: 16777215
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_timelines_on_user_id"
@@ -271,6 +268,4 @@ ActiveRecord::Schema.define(version: 2021_02_02_160327) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "timelines", "users"
 end
