2b59c5b15d9f21eb2e5be81f2b5a794077e944a5
Chris Colvard <chris.colvard@gmail.com>
2022-02-18 14:11:58 -0500

    Apply new defaults for Rails 6

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Avalon
     end
 
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
