551062a0b368568a12eca57fe75b78451420924c
Chris Colvard <chris.colvard@gmail.com>
2022-03-14 16:44:55 -0400

    fetch is available through search_service

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -116,7 +116,7 @@ class BookmarksController < CatalogController
   def action_documents
     bookmarks = token_or_current_or_guest_user.bookmarks
     bookmark_ids = bookmarks.collect { |b| b.document_id.to_s }
-    fetch(bookmark_ids, rows: bookmark_ids.count)
+    search_service.fetch(bookmark_ids, rows: bookmark_ids.count)
   end
 
   def access_control_action documents
