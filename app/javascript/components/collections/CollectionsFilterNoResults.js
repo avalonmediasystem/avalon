e6c0dbbd913a8c7f0e1e16da0233c360f6cfdeeb
dananji <dwithana@iu.edu>
2022-05-05 16:57:42 -0400

    Fix info alert color theme

--- app/javascript/components/collections/CollectionsFilterNoResults.js
+++ app/javascript/components/collections/CollectionsFilterNoResults.js
@@ -18,7 +18,7 @@ import React from 'react';
 
 const CollectionsFilterNoResults = () => (
   <p className="alert alert-info text-left">
-    <i className="fa-circle-exclamation"></i> No results returned
+    <i className="fa fa-exclamation-circle"></i> No results returned
     for your search
   </p>
 );
