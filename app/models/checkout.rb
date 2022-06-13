b4862dda4f86593efe49dcb631baaac76e732297
Dananji Withana <dan9131@gmail.com>
2022-06-13 10:48:53 -0400

    Update app/models/checkout.rb
    Co-authored-by: Chris Colvard <chris.colvard@gmail.com>

--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -3,8 +3,8 @@ class Checkout < ApplicationRecord
 
   validates :user, :media_object_id, :checkout_time, :return_time, presence: true
 
-  scope :active_for, ->(media_object_id) { where(media_object_id: media_object_id).where("return_time > now()") }
-  scope :checkouts, ->(user_id) { where(user_id: user_id).where("return_time > now()") }
+  scope :active_for_media_object, ->(media_object_id) { where(media_object_id: media_object_id).where("return_time > now()") }
+  scope :active_for_user, ->(user_id) { where(user_id: user_id).where("return_time > now()") }
 
   def media_object
     MediaObject.find(media_object_id)
