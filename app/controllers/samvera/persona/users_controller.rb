3231a648e8c89159326b0dc55301ee8fcd2b0465
Mason Ballengee <masaball@iu.edu>
2022-06-14 14:35:56 -0400

    Rollback unnecessary param variable assignment

--- app/controllers/samvera/persona/users_controller.rb
+++ app/controllers/samvera/persona/users_controller.rb
@@ -207,8 +207,7 @@ module Samvera
     end
 
     def user_params
-      new_params = params.require(:user).permit(:email, :username, :password, :password_confirmation, :is_superadmin, :facebook_handle, :twitter_handle, :googleplus_handle, :display_name, :address, :department, :title, :office, :chat_id, :website, :affiliation, :telephone, :avatar, :group_list, :linkedin_handle, :orcid, :arkivo_token, :arkivo_subscription, :zotero_token, :zotero_userid, :preferred_locale, role_ids: [])
-      new_params
+      params.require(:user).permit(:email, :username, :password, :password_confirmation, :is_superadmin, :facebook_handle, :twitter_handle, :googleplus_handle, :display_name, :address, :department, :title, :office, :chat_id, :website, :affiliation, :telephone, :avatar, :group_list, :linkedin_handle, :orcid, :arkivo_token, :arkivo_subscription, :zotero_token, :zotero_userid, :preferred_locale, role_ids: [])
     end
   end
 end
