4d7aea06c6cd6267ce360fd97acd81bfd66122e4
dananji <dwithana@iu.edu>
2022-06-14 10:04:14 -0400

    Fix view for name change

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -4,7 +4,7 @@ class CheckoutsController < ApplicationController
 
   # GET /checkouts or /checkouts.json
   def index
-    @checkouts = Checkout.checkouts(current_user.id)
+    @checkouts = Checkout.active_for_user(current_user.id)
   end
 
   # GET /checkouts/1.json
