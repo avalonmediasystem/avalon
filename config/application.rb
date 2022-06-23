5cabd2e6f2cfa92cb9efd5a3b30cb3244abab505
Chris Colvard <chris.colvard@gmail.com>
2022-06-23 08:40:03 -0400

    Bump version number for release

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'resolv-replace'
 Bundler.require(*Rails.groups)
 
 module Avalon
-  VERSION = '7.3.0'
+  VERSION = '7.4.0'
 
   class Application < Rails::Application
     require 'avalon/configuration'
