9cf7d33075e2b6bdbebedc1521bd69fe748da1ec
dananji <dwithana@iu.edu>
2022-07-05 16:48:01 -0400

    Fix spacing and alignment in collection details

--- app/javascript/components/CollectionDetails.js
+++ app/javascript/components/CollectionDetails.js
@@ -1,12 +1,12 @@
-/* 
+/*
  * Copyright 2011-2022, The Trustees of Indiana University and Northwestern
  *   University.  Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
- * 
+ *
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed
  *   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
  *   CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -22,10 +22,9 @@ const expandBtn = {
 };
 
 const descriptionStyle = {
-  marginLeft: '10px',
   fontSize: '16px',
-  fontWeight: '200'
-}
+  fontWeight: '200',
+};
 
 const CollectionDetails = ({ content = '', email = '', website = '' }) => {
 
@@ -47,6 +46,7 @@ const CollectionDetails = ({ content = '', email = '', website = '' }) => {
     setExpanded(!expanded);
   };
 
+  console.log(website);
   return (
     <div>
       {words.length > 0 && (
@@ -58,18 +58,18 @@ const CollectionDetails = ({ content = '', email = '', website = '' }) => {
           )}
         </p>
       )}
-      <dl className="row">
-        { email &&
-          <>
-            <dt style={descriptionStyle}>Contact email:</dt>
-            <dd className='ml-2'><a href={`mailto:${email}`}>{email}</a></dd>
-          </>
+      <dl className="row collection-details">
+        {email &&
+          <React.Fragment>
+            <dt style={descriptionStyle} className="col-sm-3">Contact email:</dt>
+            <dd className='col-sm-9'><a href={`mailto:${email}`}>{email}</a></dd>
+          </React.Fragment>
         }
-        { website &&
-          <>
-            <dt style={descriptionStyle}>Website:</dt>
-            <dd className='ml-2' dangerouslySetInnerHTML={{ __html: website }}></dd>
-          </>
+        {website &&
+          <React.Fragment>
+            <dt style={descriptionStyle} className="col-sm-3">Website:</dt>
+            <dd className='col-sm-9' dangerouslySetInnerHTML={{ __html: website }}></dd>
+          </React.Fragment>
         }
       </dl>
     </div>
