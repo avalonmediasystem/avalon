77358dbbf03572894d96ef998407647a78241368
Chris Colvard <chris.colvard@gmail.com>
2022-07-06 10:40:10 -0400

    Allow google drive to be enabled along with either s3 or filesystem

--- config/initializers/browse_everything.rb
+++ config/initializers/browse_everything.rb
@@ -1,14 +1,15 @@
 Rails.application.config.to_prepare do
-  settings = if Settings.dropbox.google_drive
-    { 'google_drive' => { client_id: Settings.dropbox.google_drive.client_id,
-                          client_secret: Settings.dropbox.google_drive.client_secret
-                        }
-    }
-  elsif Settings.dropbox.path =~ %r{^s3://}
+  settings = {}
+  if Settings.dropbox.google_drive
+    settings['google_drive'] = { client_id: Settings.dropbox.google_drive.client_id,
+                                 client_secret: Settings.dropbox.google_drive.client_secret
+                               }
+  end
+  if Settings.dropbox.path =~ %r{^s3://}
     obj = FileLocator::S3File.new(Settings.dropbox.path).object
-    { 's3' => { name: 'AWS S3 Dropbox', bucket: obj.bucket_name, base: obj.key, response_type: :s3_uri, region: obj.client.config.region } }
+    settings['s3'] = { name: 'AWS S3 Dropbox', bucket: obj.bucket_name, base: obj.key, response_type: :s3_uri, region: obj.client.config.region }
   else
-    { 'file_system' => { name: 'File Dropbox', home: Settings.dropbox.path } }
+    settings['file_system'] = { name: 'File Dropbox', home: Settings.dropbox.path }
   end
   BrowseEverything.configure(settings)
 end
