71f32b737a909924391d66e85b2dec4405bc18ab
Mason Ballengee <masaball@iu.edu>
2023-03-06 16:33:27 -0500

    Add timeout to waveform ffmpeg process

--- app/services/waveform_service.rb
+++ app/services/waveform_service.rb
@@ -1,11 +1,11 @@
 # Copyright 2011-2022, The Trustees of Indiana University and Northwestern
 #   University.  Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
-# 
+#
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software distributed
 #   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 #   CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -88,7 +88,8 @@ private
   def get_wave_io(uri)
     headers = "-headers $'Referer: #{Rails.application.routes.url_helpers.root_url}\r\n'" if uri.starts_with? "http"
     normalized_uri = uri.starts_with?("file") ? Addressable::URI.unencode(uri) : uri
-    cmd = "#{Settings.ffmpeg.path} #{headers} -i '#{normalized_uri}' -f wav -ar 44100 - 2> /dev/null"
+    timeout = 3600000000 # Must be in microseconds. Current value = 1 hour.
+    cmd = "#{Settings.ffmpeg.path} #{headers} -rw_timeout #{timeout} -i '#{normalized_uri}' -f wav -ar 44100 - 2> /dev/null"
     IO.popen(cmd)
   end
 
