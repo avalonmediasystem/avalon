7c2bf97ad1005c059906f3643b0958c791217dd6
Mason Ballengee <masaball@iu.edu>
2023-04-05 11:32:29 -0400

    Cascade user delete to db tables with user.id foreign key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 # Copyright 2011-2023, The Trustees of Indiana University and Northwestern
 #   University.  Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
-# 
+#
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software distributed
 #   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 #   CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -14,6 +14,8 @@
 
 class User < ActiveRecord::Base
   has_many :checkouts, dependent: :destroy
+  has_many :playlists, dependent: :destroy
+  has_many :timelines, dependent: :destroy
   attr_writer :login
   # Connects this user object to Hydra behaviors.
   include Hydra::User
