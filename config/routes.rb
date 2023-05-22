6ccc6ec41e19d6ade751f9e7dddc3f8ee09aba70
Mason Ballengee <masaball@iu.edu>
2023-05-22 16:23:05 -0400

    Remove routes for MasterFIle Attach and Delete captions

--- config/routes.rb
+++ config/routes.rb
@@ -153,8 +153,6 @@ Rails.application.routes.draw do
       post 'still',     :to => 'master_files#set_frame', :defaults => { :format => 'html' }
       get :embed
       post 'attach_structure'
-      post 'attach_captions'
-      delete 'captions', action: :delete_captions, as: 'delete_captions'
       get :captions
       get :waveform
       match ':quality.m3u8', to: 'master_files#hls_manifest', via: [:get], as: :hls_manifest
