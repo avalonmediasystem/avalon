d43f7b5608175d61cc2a491b9b6d5134f6f1ee52
dananji <dwithana@iu.edu>
2023-08-01 17:17:56 -0400

    Downgrade react to 17, render rails templates inside React component

--- app/javascript/components/Ramp.jsx
+++ app/javascript/components/Ramp.jsx
@@ -5,29 +5,33 @@ import "@samvera/ramp/dist/ramp.css";
 import { Col, Row } from 'react-bootstrap';
 import './Ramp.scss';
 
-const Ramp = ({ base_url, mo_id, canvas_count }) => {
+const Ramp = ({ base_url, mo_id, canvas_count, share, timeline }) => {
   const [transcriptsProp, setTrancsriptProp] = React.useState([]);
   const [manifestUrl, setManifestUrl] = React.useState('');
 
   React.useEffect(() => {
-    setManifestUrl(`${base_url}/media_objects/${mo_id}/manifest.json`);
-    buildTranscripts();
+    let url = `${base_url}/media_objects/${mo_id}/manifest.json`;
+    setManifestUrl(url);
+    buildTranscripts(url);
   }, []);
 
-  const buildTranscripts = () => {
+  const buildTranscripts = (url) => {
     let trProps = [];
     for(let i = 0; i < canvas_count; i++) {
       let canvasTrs = { canvasId: i, items: [] };
-      canvasTrs.items = { title: '', url: manifestUrl }
+      canvasTrs.items = { title: '', url }
       trProps.push(canvasTrs);
     }
     setTrancsriptProp(trProps);
-    console.log(trProps)
   };
 
   return (
     <IIIFPlayer manifestUrl={manifestUrl}>
       <MediaPlayer enableFileDownload={false} />
+      <div className="ramp--rails-content">
+        <div className="mr-1" dangerouslySetInnerHTML={{ __html: timeline }} />
+        <div className="share-tabs" dangerouslySetInnerHTML={{ __html: share }} />
+      </div>
       <Row>
         <Col>
           <StructuredNavigation />
