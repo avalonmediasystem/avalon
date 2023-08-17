f0134a19ac0e648fc7521271499ca9fbba039f20
dananji <dwithana@iu.edu>
2023-08-17 17:36:42 -0400

    Add other Ramp components to item page

--- app/javascript/components/Ramp.jsx
+++ app/javascript/components/Ramp.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Transcript, IIIFPlayer, MediaPlayer, StructuredNavigation } from "@samvera/ramp";
+import { Transcript, IIIFPlayer, MediaPlayer, StructuredNavigation, MetadataDisplay, SupplmentalFiles } from "@samvera/ramp";
 import 'video.js/dist/video-js.css';
 import "@samvera/ramp/dist/ramp.css";
-import { Col, Row } from 'react-bootstrap';
+import { Col, Row, Tab, Tabs } from 'react-bootstrap';
 import './Ramp.scss';
 
-const Ramp = ({ base_url, mo_id, canvas_count, share, timeline }) => {
+const Ramp = ({ base_url, mo_id, canvas_count, share, timeline, cdl, progress }) => {
   const [transcriptsProp, setTrancsriptProp] = React.useState([]);
   const [manifestUrl, setManifestUrl] = React.useState('');
 
+  const { enabled, canStream, embed, destroyCDL } = cdl;
   React.useEffect(() => {
     let url = `${base_url}/media_objects/${mo_id}/manifest.json`;
     setManifestUrl(url);
@@ -27,20 +28,35 @@ const Ramp = ({ base_url, mo_id, canvas_count, share, timeline }) => {
 
   return (
     <IIIFPlayer manifestUrl={manifestUrl}>
-      <MediaPlayer enableFileDownload={false} />
-      <div className="ramp--rails-content">
-        { timeline.canCreate && <div className="mr-1" dangerouslySetInnerHTML={{ __html: timeline.content }} /> }
-        { share.canShare && <div className="share-tabs" dangerouslySetInnerHTML={{ __html: share.content }} /> }
-      </div>
       <Row>
         <Col>
-          <StructuredNavigation />
+          { (enabled && !canStream) && (<div dangerouslySetInnerHTML={{ __html: embed }} />) }
+          <React.Fragment>
+            <div dangerouslySetInnerHTML={{ __html: progress }} />
+            <MediaPlayer enableFileDownload={false} />
+            <div className="ramp--rails-content">
+              { timeline.canCreate && <div className="mr-1" dangerouslySetInnerHTML={{ __html: timeline.content }} /> }
+              { share.canShare && <div className="share-tabs" dangerouslySetInnerHTML={{ __html: share.content }} /> }
+            </div>
+            <StructuredNavigation />
+          </React.Fragment>
         </Col>
         <Col>
-          <Transcript
-            playerID="iiif-media-player"
-            transcripts={transcriptsProp}
-          />
+          { enabled && <div dangerouslySetInnerHTML={{ __html: destroyCDL }}/> }
+          <Tabs>
+            <Tab eventKey="details" title="Details">
+              <MetadataDisplay showHeading={false} displayTitle={false}/>
+            </Tab>
+            <Tab eventKey="transcripts" title="Transcripts">
+              <Transcript
+                playerID="iiif-media-player"
+                transcripts={transcriptsProp}
+              />
+            </Tab>
+            <Tab eventKey="files" title="Files">
+              <SupplmentalFiles showHeading={false} />
+            </Tab>
+          </Tabs>
         </Col>
       </Row>
     </IIIFPlayer>
