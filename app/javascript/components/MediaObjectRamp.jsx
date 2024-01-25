062642b8376a1be70098d7902cd77afb0a1a0140
Dananji Withana <dwithana@bl-libg-1006061.local>
2024-01-25 17:10:01 -0500

    Fix UI/UX issues on item page for mobile devices

--- app/javascript/components/MediaObjectRamp.jsx
+++ app/javascript/components/MediaObjectRamp.jsx
@@ -30,12 +30,12 @@ import './Ramp.scss';
 
 const ExpandCollapseArrow = () => {
   return (
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" className="expand-collapse-svg" fill="currentColor" viewBox="0 0 16 16">
-    <path
-      fillRule="evenodd"
-      d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z">
-    </path>
-  </svg>);
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" className="expand-collapse-svg" fill="currentColor" viewBox="0 0 16 16">
+      <path
+        fillRule="evenodd"
+        d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z">
+      </path>
+    </svg>);
 };
 
 const Ramp = ({
@@ -61,27 +61,27 @@ const Ramp = ({
   React.useEffect(() => {
     const { base_url, fullpath_url } = urls;
     // Split the current path from the time fragment in the format .../:id?t=time
-    let [ fullpath, start_time ] = fullpath_url.split('?t=');
+    let [fullpath, start_time] = fullpath_url.split('?t=');
     // Split the current path in the format /media_objects/:mo_id/section/:mf_id
-    let [ _, __, mo_id, ___, start_canvas ] = fullpath.split('/');
+    let [_, __, mo_id, ___, start_canvas] = fullpath.split('/');
     // Build the manifest URL
     let url = `${base_url}/media_objects/${mo_id}/manifest.json`;
-    
+
     // Set start Canvas ID and start time in the state for Ramp
     setStartCanvasId(
       start_canvas && start_canvas != undefined
-      ? `${base_url}/media_objects/${mo_id}/manifest/canvas/${start_canvas}` 
-      : undefined
-      );
+        ? `${base_url}/media_objects/${mo_id}/manifest/canvas/${start_canvas}`
+        : undefined
+    );
     setStartCanvasTime(
       start_time && start_time != undefined
-      ? parseFloat(start_time)
-      : undefined
-    )
+        ? parseFloat(start_time)
+        : undefined
+    );
     setManifestUrl(url);
 
     // Attach player event listeners when there's structure
-    if(has_structure) {
+    if (has_structure) {
       interval = setInterval(addPlayerEventListeners, 500);
     }
 
@@ -95,7 +95,7 @@ const Ramp = ({
    */
   const addPlayerEventListeners = () => {
     let player = document.getElementById('iiif-media-player');
-    if(player && player.player != undefined && !player.player.isDisposed()) {
+    if (player && player.player != undefined && !player.player.isDisposed()) {
       let playerInst = player.player;
       playerInst.on('loadedmetadata', () => {
         playerInst.on('timeupdate', () => {
@@ -107,7 +107,7 @@ const Ramp = ({
         setIsClosed(false);
       });
     }
-  }
+  };
 
   React.useEffect(() => {
     expandCollapseSections(isClosed);
@@ -115,14 +115,14 @@ const Ramp = ({
 
   const handleCollapseExpand = () => {
     setIsClosed(isClosed => !isClosed);
-  }
+  };
 
   const expandCollapseSections = (isClosing) => {
     const allSections = $('div[class*="ramp--structured-nav__section"]');
-    allSections.each(function(index, section) {
+    allSections.each(function (index, section) {
       let sectionUl = section.nextSibling;
-      if(sectionUl) {
-        if(isClosing) {
+      if (sectionUl) {
+        if (isClosing) {
           sectionUl.classList.remove('expanded');
           sectionUl.classList.add('closed');
           expandCollapseBtnRef.current.classList.remove('expanded');
@@ -135,116 +135,116 @@ const Ramp = ({
         }
       }
     });
-  }
+  };
 
   return (
-    <IIIFPlayer manifestUrl={manifestUrl} 
+    <IIIFPlayer manifestUrl={manifestUrl}
       customErrorMessage='This page encountered an error. Please refresh or contact an administrator.'
       startCanvasId={startCanvasId}
       startCanvasTime={startCanvasTime}>
       <Row className="ramp--all-components ramp--itemview">
         <Col sm={8}>
-          { (cdl.enabled && !cdl.can_stream)
+          {(cdl.enabled && !cdl.can_stream)
             ? (<div dangerouslySetInnerHTML={{ __html: cdl.embed }} />)
-            : ( <React.Fragment>
-                  { master_files_count > 0 &&
-                    <React.Fragment>
-                      <MediaPlayer enableFileDownload={false} />
-                      <div className="ramp--rails-title">
-                        { <div className="object-title" dangerouslySetInnerHTML={{ __html: title.content }} /> }
+            : (<React.Fragment>
+              {master_files_count > 0 &&
+                <React.Fragment>
+                  <MediaPlayer enableFileDownload={false} />
+                  <div className="ramp--rails-title">
+                    {<div className="object-title" dangerouslySetInnerHTML={{ __html: title.content }} />}
+                  </div>
+                  <div className="ramp--rails-content">
+                    <Col className="ramp-button-group-1">
+                      {timeline.canCreate &&
+                        <button
+                          id="timelineBtn"
+                          className="btn btn-outline mr-1 text-nowrap"
+                          type="button"
+                          data-toggle="modal"
+                          data-target="#timelineModal"
+                          aria-expanded="false"
+                          aria-controls="timelineModal"
+                          disabled={true}
+                        >
+                          Create Timeline
+                        </button>
+                      }
+                      {share.canShare &&
+                        <button
+                          className="btn btn-outline mr-1 text-nowrap"
+                          type="button"
+                          data-toggle="collapse"
+                          data-target="#shareResourcePanel"
+                          aria-expanded="false"
+                          aria-controls="shareResourcePanel"
+                          id="shareBtn"
+                        >
+                          <i className="fa fa-share-alt"></i>
+                          Share
+                        </button>
+                      }
+                      {playlist.canCreate &&
+                        <button className="btn btn-outline text-nowrap mr-1"
+                          id="addToPlaylistBtn"
+                          type="button"
+                          data-toggle="collapse"
+                          data-target="#addToPlaylistPanel"
+                          aria-expanded="false"
+                          aria-controls="addToPlaylistPanel"
+                          disabled={true}
+                        >
+                          {/* Static SVG image in /app/assets/images/add_to_playlist_icon.svg */}
+                          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                            viewBox="-293 386 24 24" xmlSpace="preserve">
+                            <path className="st1" fill="currentColor" d="M-279,395h-12v2h12V395z M-279,391h-12v2h12V391z M-275,399v-4h-2v4h-4v2h4v4h2v-4h4v-2H-275z M-291,401h8v-2h-8V401z" />
+                          </svg>
+                          Add to Playlist
+                        </button>
+                      }
+                    </Col>
+                    {has_structure &&
+                      <Col className="ramp-button-group-2">
+                        <button
+                          className="btn btn-outline expand-collapse-toggle-button expanded"
+                          id="expand_all_btn"
+                          onClick={handleCollapseExpand}
+                          ref={expandCollapseBtnRef}
+                        >
+                          <ExpandCollapseArrow />
+                          {isClosed ? ' Expand' : ' Close'} {master_files_count > 1 ? `${master_files_count} Sections` : 'Section'}
+                        </button>
+                      </Col>
+                    }
+                  </div>
+                  <Row className="mx-0">
+                    <Col>
+                      <div dangerouslySetInnerHTML={{ __html: timeline.content }} />
+                    </Col>
+                    <Col md={12} lg={12} sm={12} className="px-0">
+                      <div className="collapse" id="addToPlaylistPanel">
+                        <div className="card card-body" dangerouslySetInnerHTML={{ __html: playlist.tab }} />
                       </div>
-                      <div className="ramp--rails-content">
-                        <Col className="ramp-button-group-1">
-                          { timeline.canCreate &&
-                            <button
-                              id="timelineBtn"
-                              className="btn btn-outline mr-1 text-nowrap"
-                              type="button"
-                              data-toggle="modal"
-                              data-target="#timelineModal"
-                              aria-expanded="false"
-                              aria-controls="timelineModal"
-                              disabled={true}
-                            >
-                              Create Timeline
-                            </button>
-                          }
-                          { share.canShare &&
-                            <button
-                              className="btn btn-outline mr-1 text-nowrap"
-                              type="button"
-                              data-toggle="collapse"
-                              data-target="#shareResourcePanel"
-                              aria-expanded="false"
-                              aria-controls="shareResourcePanel"
-                              id="shareBtn"
-                            >
-                              <i className="fa fa-share-alt"></i>
-                                Share
-                            </button>
-                          }
-                          { playlist.canCreate &&
-                            <button className="btn btn-outline text-nowrap mr-1"
-                              id="addToPlaylistBtn"
-                              type="button"
-                              data-toggle="collapse"
-                              data-target="#addToPlaylistPanel"
-                              aria-expanded="false"
-                              aria-controls="addToPlaylistPanel"
-                              disabled={true}
-                            >
-                              {/* Static SVG image in /app/assets/images/add_to_playlist_icon.svg */}
-                              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                                  viewBox="-293 386 24 24" xmlSpace="preserve">
-                                  <path className="st1" fill="currentColor" d="M-279,395h-12v2h12V395z M-279,391h-12v2h12V391z M-275,399v-4h-2v4h-4v2h4v4h2v-4h4v-2H-275z M-291,401h8v-2h-8V401z"/>
-                              </svg>
-                              Add to Playlist
-                            </button>
-                          }
-                        </Col>
-                        { has_structure &&
-                          <Col className="ramp-button-group-2">
-                            <button
-                              className="btn btn-outline expand-collapse-toggle-button expanded"
-                              id="expand_all_btn"
-                              onClick={handleCollapseExpand}
-                              ref={expandCollapseBtnRef}
-                            >
-                              <ExpandCollapseArrow />
-                              {isClosed ? ' Expand' : ' Close'} {master_files_count > 1 ? `${master_files_count} Sections` : 'Section'}
-                            </button>
-                          </Col>
-                        }
+                    </Col>
+                    <Col md={12} lg={12} sm={12} className="px-0">
+                      <div className="collapse" id="shareResourcePanel">
+                        <div className="share-tabs" dangerouslySetInnerHTML={{ __html: share.content }} />
                       </div>
-                      <Row className="mx-0">
-                        <Col>
-                          <div dangerouslySetInnerHTML={{ __html: timeline.content}} />
-                        </Col>
-                        <Col md={12} lg={12} sm={12} className="px-0">
-                          <div className="collapse" id="addToPlaylistPanel">
-                            <div className="card card-body" dangerouslySetInnerHTML={{ __html: playlist.tab }} />
-                          </div>
-                        </Col>
-                        <Col md={12} lg={12} sm={12} className="px-0">
-                          <div className="collapse" id="shareResourcePanel">
-                            <div className="share-tabs" dangerouslySetInnerHTML={{ __html: share.content }} />
-                          </div>
-                        </Col>
-                      </Row>
-                      <StructuredNavigation />
-                    </React.Fragment>
-                  }
+                    </Col>
+                  </Row>
+                  <StructuredNavigation />
                 </React.Fragment>
-              )
+              }
+            </React.Fragment>
+            )
           }
         </Col>
-        <Col sm={ (master_files_count == 0) ? 12 : 4} className="ramp--tabs-panel">
-          { cdl.enabled && <div dangerouslySetInnerHTML={{ __html: cdl.destroy }}/> }
+        <Col sm={(master_files_count == 0) ? 12 : 4} className="ramp--tabs-panel">
+          {cdl.enabled && <div dangerouslySetInnerHTML={{ __html: cdl.destroy }} />}
           <Tabs>
             <Tab eventKey="details" title="Details">
-              <MetadataDisplay showHeading={false} displayTitle={false}/>
+              <MetadataDisplay showHeading={false} displayTitle={false} />
             </Tab>
-            { (cdl.can_stream && master_files_count != 0 && has_transcripts) &&
+            {(cdl.can_stream && master_files_count != 0 && has_transcripts) &&
               <Tab eventKey="transcripts" title="Transcripts" className="ramp--transcripts_tab">
                 <Transcript
                   playerID="iiif-media-player"
@@ -252,7 +252,7 @@ const Ramp = ({
                 />
               </Tab>
             }
-            { (has_files) &&
+            {(has_files) &&
               <Tab eventKey="files" title="Files">
                 <SupplementalFiles showHeading={false} />
               </Tab>
