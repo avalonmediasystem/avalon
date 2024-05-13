f7cb5c9fd3bcf590439943a550a7f17472fe528d
dwithana <dwithana@iu.edu>
2024-05-13 16:21:03 -0400

    Use asset_host for thumbnail id serialization instead of thumbnail endpoint
    Co-authored-by: Chris Colvard <cjcolvar@indiana.edu>

--- app/models/iiif_manifest_presenter.rb
+++ app/models/iiif_manifest_presenter.rb
@@ -211,7 +211,7 @@ class IiifManifestPresenter
       if video_count > 0
         Rails.application.routes.url_helpers.thumbnail_master_file_url(master_file_id)
       elsif audio_count > 0
-        Rails.application.routes.url_helpers.thumbnail_master_file_url(master_file_id)
+        ActionController::Base.helpers.asset_url('audio_icon.png')
       end
     elsif video_count > 0 && audio_count > 0
       ActionController::Base.helpers.asset_url('hybrid_icon.png')
--- config/initializers/default_host.rb
+++ config/initializers/default_host.rb
@@ -17,6 +17,7 @@ if server_options
     Rails.application.routes.default_url_options.merge!( server_options )
     ActionMailer::Base.default_url_options.merge!( server_options )
     ApplicationController.default_url_options = server_options
+    ActionController::Base.asset_host = URI("#{Settings.domain.protocol}://#{Settings.domain.host}:#{Settings.domain.port}").to_s
   end
 
   # Required for rails 6+
