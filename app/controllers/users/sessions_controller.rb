6548f38da20c22e32a5be408e82c15b5ad664b37
cjcolvar <chris.colvard@gmail.com>
2024-08-23 16:58:57 -0400

    Use auto-submitting form to workaround omniauth2 requirement for only POST requests to auth endpoints

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -15,10 +15,7 @@
 class Users::SessionsController < Devise::SessionsController
   def new
     if Avalon::Authentication::VisibleProviders.length == 1 && params[:admin].blank?
-      omniauth_params = params.reject { |k,v| ['controller','action'].include?(k) }
-      omniauth_params.permit!
-      login_path = user_omniauth_authorize_path(Avalon::Authentication::VisibleProviders.first[:provider], omniauth_params)
-      redirect_to login_path
+      render :omniauth_new, layout: false
     else
       super
     end
