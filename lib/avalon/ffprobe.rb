91f5b88f7f77ea46447fdd156a31c14746e0dfe7
Mason Ballengee <masaball@iu.edu>
2024-09-05 14:44:18 -0400

    Adjust non-media item filtering in ffprobe
    In QA it was found that a video was improperly processed as audio. This
    was a result of keying off display aspect ratio to filter out
    non-audio/video items, under the false assumption that all videos would
    have a display aspect ratio. Explicitly filtering out plain text files
    and using frame count to filter images will hopefully be more
    consistent.

--- lib/avalon/ffprobe.rb
+++ lib/avalon/ffprobe.rb
@@ -14,10 +14,11 @@
 
 module Avalon
   class FFprobe
-    # media_path should be the output of `FileLocator.new(file_location).location`
+    attr_reader :json_output, :video_stream, :audio_stream
+    # @param [String] media_path the full path to the media file
     def initialize(media_path)
       ffprobe = Settings&.ffprobe&.path || 'ffprobe'
-      raw_output = `#{ffprobe} -i "#{media_path}" -v quiet -show_format -show_streams -count_packets -of json`
+      raw_output = `#{ffprobe} -i "#{media_path}" -v quiet -show_format -show_streams -count_frames -of json`
       # $? is a variable for the exit status of the last executed process. 
       # Success == 0, any other value means the command failed in some way.
       unless $?.exitstatus == 0
@@ -26,35 +27,46 @@ module Avalon
         return
       end
       @json_output = JSON.parse(raw_output).deep_symbolize_keys
-      @video_stream = @json_output[:streams].select { |stream| stream[:codec_type] == 'video' }.first
+
+      # Plain text files should return from ffprobe as an empty hash.
+      # When the hash is not empty, the format name is 'tty' so we provide
+      # a catch here just in case.
+      return @json_output = {} if json_output[:format][:format_name] == 'tty'
+
+      @video_stream = json_output[:streams].select { |stream| stream[:codec_type] == 'video' }.first
+      @audio_stream = json_output[:streams].select { |stream| stream[:codec_type] == 'audio' }.first
     end
 
     def video?
-      # ffprobe treats plain text files as ANSI or ASCII art. This sets the codec type to video
-      # but leaves display aspect ratio `nil`. If display_aspect_ratio is nil, return false.
-      # ffprobe treats image files as a single frame video. This sets the codec type to video
-      # but the packet/frame count will equal 1. If packet count equals 1, return false.
-      return true if @video_stream && @video_stream[:display_aspect_ratio] && @video_stream[:nb_read_packets].to_i > 1
+      # ffprobe treats image files as a single frame video. This sets the codec type to video,
+      # but leaves the frame count at 1. If frame count is 1, return false.
+      return true if video_stream && video_stream[:nb_read_frames].to_i > 1
 
       false
     end
 
     def audio?
-      @json_output[:streams]&.any? { |stream| stream[:codec_type] == 'audio' }
+      return true if audio_stream
+
+      false
     end
 
     def duration
       return unless video? || audio?
       # ffprobe return duration as seconds. Existing Avalon logic expects milliseconds.
-      (@json_output[:format][:duration].to_f * 1000).to_i
+      (json_output[:format][:duration].to_f * 1000).to_i
     end
 
     def display_aspect_ratio
-      @video_stream[:display_aspect_ratio] if video?
+      if video? && video_stream[:display_aspect_ratio].present?
+        video_stream[:display_aspect_ratio]
+      elsif video?
+        video_stream[:width].to_f / video_stream[:height].to_f
+      end
     end
 
     def original_frame_size
-      "#{@video_stream[:width]}x#{@video_stream[:height]}" if video?
+      "#{video_stream[:width]}x#{video_stream[:height]}" if video?
     end
   end
 end
--- spec/lib/avalon/ffprobe_spec.rb
+++ spec/lib/avalon/ffprobe_spec.rb
@@ -121,6 +121,13 @@ describe Avalon::FFprobe do
       it 'returns the display aspect ratio' do
         expect(subject.display_aspect_ratio).to eq '20:11'
       end
+
+      context 'file missing display aspect ratio' do
+        it 'calculates the display aspect ratio' do
+          subject.instance_variable_set(:@video_stream, subject.video_stream.except!(:display_aspect_ratio))
+          expect(subject.display_aspect_ratio).to eq(200.to_f / 110.to_f)
+        end
+      end
     end
 
     context 'with audio' do
