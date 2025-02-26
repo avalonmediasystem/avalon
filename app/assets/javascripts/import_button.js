6ccf79fc6669cf7f128e3788caf2ff44178f58ea
cjcolvar <chris.colvard@gmail.com>
2025-02-26 16:53:58 -0500

    Refactor and support case where bib id present but no media object title
    Co-authored-by: Mason Ballengee <masaball@iu.edu>

--- app/assets/javascripts/import_button.js
+++ app/assets/javascripts/import_button.js
@@ -15,40 +15,33 @@
 
 $(document).ready(function() {
   var form = $('div.import-button').closest('form').prop('id');
-  var import_button_html = '<div class="input-group-append"><button id="media_object_bibliographic_id_btn" type="button" name="media_object[import_bib_record]" class="btn btn-outline" value="yes" >Import</button></div>';
-
+  var import_button_html = '<div class="input-group-append"><button id="media_object_bibliographic_id_btn" type="submit" name="media_object[import_bib_record]" class="btn btn-outline" value="yes" >Import</button></div>';
   $('div.import-button').append(import_button_html);
+  $('#media_object_bibliographic_id_btn').popover({
+    trigger: 'manual',
+    html: true,
+    sanitize: false,
+    placement: 'top',
+    container: 'body',
+    content: function() {
+      var button = `<button id="media_object_bibliographic_id_confirm_btn" class="btn btn-sm btn-danger btn-confirm" type="submit" name="media_object[import_bib_record]" value="yes" data-original-title="" title="" form="${form}" >Import</button>`
+      return `<p>Note: this will replace all metadata except for Other Identifiers</p> ${button} <button id=\'cancel_bibimport\' class=\'btn btn-sm btn-primary\'>No, Cancel</button>`
+    }
+  });
 
-  $('#media_object_bibliographic_id').on('keyup', function() {
+  $('#media_object_bibliographic_id_btn').on('click', function() {
     import_val = document.getElementById('media_object_bibliographic_id').value;
 
     if ($('input#media_object_title').val() != '' && import_val != '') {
-      $(document).on('click', '#cancel_bibimport', function() {
-        $('#media_object_bibliographic_id_btn').popover('hide');
-        return true;
-      });
-      $(function() {
-        $('#media_object_bibliographic_id_btn').popover({
-          trigger: 'manual',
-          html: true,
-          sanitize: false,
-          placement: 'top',
-          container: 'body',
-          content: function() {
-            var button = `<button id="media_object_bibliographic_id_confirm_btn" class="btn btn-sm btn-danger btn-confirm" type="submit" name="media_object[import_bib_record]" value="yes" data-original-title="" title="" form="${form}" >Import</button>`
-            return `<p>Note: this will replace all metadata except for Other Identifiers</p> ${button} <button id=\'cancel_bibimport\' class=\'btn btn-sm btn-primary\'>No, Cancel</button>`
-          }
-        }).popover('enable').on('click', function() {
-          $('.btn-confirmation').popover('hide');
-          $(this).popover('show');
-          form_attribute_fix('#media_object_bibliographic_id_confirm_btn');
-          return false;
-        });
-      });
-    } else {
-      $(function() {
-        $('#media_object_bibliographic_id_btn').popover('disable');
-      });
+      $('#media_object_bibliographic_id_btn').popover('show');
+      form_attribute_fix('#media_object_bibliographic_id_confirm_btn');
+      return false;
+    } else if (import_val == '') {
+      return false;
     }
+  });
+  $(document).on('click', '#cancel_bibimport', function() {
+    $('#media_object_bibliographic_id_btn').popover('hide');
+    return true;
   });
 });
