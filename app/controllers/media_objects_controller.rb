60d598dd4a95003d6b328ff6c099187b49b7feec
Mason Ballengee <masaball@iu.edu>
2025-04-18 14:51:22 -0400

    Use strong parameters for add_to_playlist action
    Co-authored-by: Chris Colvard <chris.colvard@gmail.com>

--- app/controllers/media_objects_controller.rb
+++ app/controllers/media_objects_controller.rb
@@ -109,9 +109,8 @@ class MediaObjectsController < ApplicationController
 
   # POST /media_objects/avalon:1/add_to_playlist
   def add_to_playlist
-    if params[:post].present?
-      @media_object = params[:post][:masterfile_id].present? ? SpeedyAF::Proxy::MediaObject.find(params[:id]) : SpeedyAF::Proxy::MediaObject.find(params[:id], load_reflections: true)
-    end
+    add_to_playlist_params = params.require(:post).permit(:masterfile_id, :playlist_id, :playlistitem_scope)
+    @media_object = add_to_playlist_params.present? ? SpeedyAF::Proxy::MediaObject.find(params[:id]) : SpeedyAF::Proxy::MediaObject.find(params[:id], load_reflections: true)
     authorize! :read, @media_object
     masterfile_id = params[:post][:masterfile_id]
     playlist_id = params[:post][:playlist_id]
