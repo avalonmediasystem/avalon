0418a4e427c3b530ce2f7fc81a1a88393692e9ab
Mason Ballengee <masaball@iu.edu>
2025-06-10 13:23:27 -0400

    Use 8.0 default framework

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Avalon
     end
 
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.2
+    config.load_defaults 8.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
