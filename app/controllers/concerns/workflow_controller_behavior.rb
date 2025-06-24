458002ebd28ceb61e1f420fec670c6537f6fb637
Mason Ballengee <masaball@iu.edu>
2025-06-24 09:42:38 -0400

    Memoize workflow model object to fix save/continue

--- app/controllers/concerns/workflow_controller_behavior.rb
+++ app/controllers/concerns/workflow_controller_behavior.rb
@@ -92,11 +92,11 @@ module WorkflowControllerBehavior
 
   # Use SpeedyAF for loading edit pages more quickly, switch to fedora for saves
   def model_object
-    @model_object = if params[:action] == "edit"
-                      SpeedyAF::Base.find(params[:id])
-                    else
-                      ActiveFedora::Base.find(params[:id], cast: true)
-                    end
+    @model_object ||= if params[:action] == "edit"
+                        SpeedyAF::Base.find(params[:id])
+                      else
+                        ActiveFedora::Base.find(params[:id], cast: true)
+                      end
   end
 
   def build_context
