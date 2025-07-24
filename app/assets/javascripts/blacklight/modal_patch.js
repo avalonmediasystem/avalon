b491d712e1f67931b2184d87f12e78597a0fa429
cjcolvar <chris.colvard@gmail.com>
2025-07-24 09:02:47 -0400

    Bring in patch for closing blacklight modals via escape key while preserving scrolling

---
+++ app/assets/javascripts/blacklight/modal_patch.js
@@ -0,0 +1,11 @@
+// Patch fix of https://github.com/projectblacklight/blacklight/pull/3694
+// TODO: Remove this when on a version of blacklight that includes that PR
+$(document).ready(function () {
+    // Make sure user-agent dismissal of html 'dialog', etc `esc` key, triggers
+    // our hide logic, including events and scroll restoration.
+    document.getElementById('blacklight-modal').addEventListener('cancel', (e) => {
+      e.preventDefault(); // 'hide' will close the modal unless cancelled
+
+      Blacklight.Modal.hide();
+    });
+});
