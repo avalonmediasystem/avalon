b628c8e1db6bf23a07a42753989d48bedf886bc3
Mason Ballengee <masaball@iu.edu>
2025-11-13 16:18:45 -0500

    Allow unit name to be used for relationship assignment

--- spec/features/capybara_admin_collection_spec.rb
+++ spec/features/capybara_admin_collection_spec.rb
@@ -39,7 +39,7 @@ describe 'Admin Collection' do
     visit '/admin/collections/'
     click_link('Create Collection')
     fill_in('admin_collection_name', with: 'Test Collection')
-    fill_in('admin_collection[unit_id]', with: @unit.id)
+    fill_in('admin_collection[unit_name]', with: @unit.name)
     click_on('Create Collection')
     visit '/admin/collections'
     expect(page).to have_content('Test Collection')
@@ -54,7 +54,7 @@ describe 'Admin Collection' do
     visit '/admin/collections'
     click_link('Create Collection')
     fill_in('admin_collection_name', with: 'Test Collection')
-    fill_in('admin_collection[unit_id]', with: @unit.id)
+    fill_in('admin_collection[unit_name]', with: @unit.name)
     click_on('Create Collection')
     visit '/admin/collections'
     click_on('Test Collection')
@@ -69,7 +69,7 @@ describe 'Admin Collection' do
     visit '/admin/collections'
     click_link('Create Collection')
     fill_in('admin_collection_name', with: 'Test Collection')
-    fill_in('admin_collection[unit_id]', with: @unit.id)
+    fill_in('admin_collection[unit_name]', with: @unit.name)
     click_on('Create Collection')
     visit '/admin/collections'
     click_link('Delete')
