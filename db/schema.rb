92cfb48df68423bb8a3e562fc97ca20a428a070a
cjcolvar <chris.colvard@gmail.com>
2025-11-18 15:00:47 -0500

    Really make playlist description be a text column and allow lengths greater than 255 characters

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_08_22_194731) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_18_195618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
