39e124fa194b158a8bcb5d9287f5eaa0eca5342c
cjcolvar <chris.colvard@gmail.com>
2026-03-31 12:27:12 -0400

    Retry Connection Refused (503) responses from solr

--- config/initializers/policy_aware_modification.rb
+++ config/initializers/policy_aware_modification.rb
@@ -26,6 +26,45 @@ module RSolr
   end
 end
 
+RSolr::Client.class_eval do
+  # OVERRIDE: add post method to retry config
+  def connection
+    @connection ||= begin
+      conn_opts = { request: {} }
+      conn_opts[:url] = uri.to_s
+      conn_opts[:proxy] = proxy if proxy
+      conn_opts[:request][:open_timeout] = options[:open_timeout] if options[:open_timeout]
+
+      if options[:read_timeout] || options[:timeout]
+        # read_timeout was being passed to faraday as timeout since Rsolr 2.0,
+        # it's now deprecated, just use `timeout` directly.
+        conn_opts[:request][:timeout] = options[:timeout] || options[:read_timeout]
+      end
+
+      conn_opts[:request][:params_encoder] = Faraday::FlatParamsEncoder
+
+      Faraday.new(conn_opts) do |conn|
+        if uri.user && uri.password
+          case Faraday::VERSION
+          when /^0/
+            conn.basic_auth uri.user, uri.password
+          when /^1/
+            conn.request :basic_auth, uri.user, uri.password
+          else
+            conn.request :authorization, :basic_auth, uri.user, uri.password
+          end
+        end
+
+        conn.response :raise_error
+        conn.request :retry, max: options[:retry_after_limit], interval: 0.05,
+                             interval_randomness: 0.5, backoff_factor: 2,
+                             exceptions: ['Faraday::Error', 'Timeout::Error'], methods: %i[delete get head options put post] if options[:retry_503]
+        conn.adapter options[:adapter] || Faraday.default_adapter || :net_http
+      end
+    end
+  end
+end
+
 module Hydra::AccessControlsEnforcement
   def escape_filter(key, value)
     [key, escape_value(value)].join(':')
