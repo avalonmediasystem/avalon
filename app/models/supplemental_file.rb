avalonmediasystem/avalon

commit 079b20e451a4e9ce7b49f1f60621531b5eb20486

Create caption files during transcoding
Adjust call to ActiveEncode to request subtitle extraction

Add extract_subtitles param to ActiveEncode related test expectations

Bump to ActiveEncode main branch

Improve output filtering and fix tests

Improvements to output filtering and tests

Filter outputs on format, improve tests

app/models/supplemental_file.rb
@@ -33,9 +33,14 @@ class SupplementalFile < ApplicationRecord
   after_update_commit :update_index, prepend: true
   after_destroy_commit :remove_from_index
 
-  def attach_file(new_file)
-    file.attach(new_file)
-    extension = File.extname(new_file.original_filename)
+  def attach_file(new_file, io: false)
+    if io
+      file.attach(io: File.open(new_file), filename: File.basename(new_file))
+      extension = File.extname(new_file)
+    else
+      file.attach(new_file)
+      extension = File.extname(new_file.original_filename)
+    end
     self.file.content_type = Mime::Type.lookup_by_extension(extension.slice(1..-1)).to_s if extension == '.srt'
     self.label = file.filename.to_s if label.blank?
     self.language ||= Settings.caption_default.language

app/models/watched_encode.rb
@@ -32,23 +32,37 @@ class WatchedEncode < ActiveEncode::Base
     if Settings.encoding.derivative_bucket &&
        (Settings.encoding.engine_adapter.to_sym == :ffmpeg || is_a?(PassThroughEncode))
       bucket = Aws::S3::Bucket.new(name: Settings.encoding.derivative_bucket)
+
       encode.output.collect! do |output|
-        file = FileLocator.new output.url
-        key = file.location.sub(/\/(.*?)\//, "")
-        obj = bucket.object key
-
-        if File.exist? file.location
-          obj.upload_file file.location
-          File.delete file.location
+        if output.format == "vtt"
+          new_file = SupplementalFile.new(tags: ['caption'], parent_id: record.master_file_id)
+          new_file.attach_file(FileLocator.new(output.url).location, io: true)
+          new_file.save
+          output.url = if Settings.active_storage.bucket.present?
+                      "s3://#{Settings.active_storage.bucket}/#{new_file.file.blob.key}"
+                    else
+                      new_file.file.blob.url
+                    end
+          output.id = new_file.to_global_id.to_s
+          output
         else
-          # Calls to Addressable::URI.escape here are to counter the unescaping that happens in FileLocator
-          # This is needed because files uploaded to minio (and probably AWS) escape spaces (%20) instead of keeping spaces
-          obj.upload_file Addressable::URI.escape(file.location)
-          File.delete Addressable::URI.escape(file.location)
-        end
+          file = FileLocator.new output.url
+          key = file.location.sub(/\/(.*?)\//, "")
+          obj = bucket.object key
 
-        output.url = "s3://#{obj.bucket.name}/#{obj.key}"
-        output
+          if File.exist? file.location
+            obj.upload_file file.location
+            File.delete file.location
+          else
+            # Calls to Addressable::URI.escape here are to counter the unescaping that happens in FileLocator
+            # This is needed because files uploaded to minio (and probably AWS) escape spaces (%20) instead of keeping spaces
+            obj.upload_file Addressable::URI.escape(file.location)
+            File.delete Addressable::URI.escape(file.location)
+          end
+
+          output.url = "s3://#{obj.bucket.name}/#{obj.key}"
+          output
+        end
       end
 
       # Save translated output urls

lib/avalon/batch/entry.rb
@@ -297,7 +297,7 @@ module Avalon
         machine_generated = Avalon::Batch.true_field?(datastream[:machine_generated]) ? 'machine_generated' : nil
         # Create SupplementalFile
         supplemental_file = SupplementalFile.new(label: label, tags: [type, treat_as_transcript, machine_generated].uniq.compact, language: language, parent_id: parent_id)
-        supplemental_file.file.attach(io: FileLocator.new(datastream[file_key]).reader, filename: filename)
+        supplemental_file.attach_file(FileLocator.new(datastream[file_key]).reader, io: true)
         supplemental_file.save ? supplemental_file : nil
       end
       private_class_method :process_datastream

spec/models/supplemental_file_spec.rb
@@ -15,162 +15,191 @@
 require 'rails_helper'
 
 describe SupplementalFile do
-  let(:subject) { FactoryBot.create(:supplemental_file) }
+  let(:supplemental_file) { FactoryBot.create(:supplemental_file) }
 
   describe 'validations' do
     describe 'file type' do
       context 'non-caption file' do
-        let(:subject) { FactoryBot.create(:supplemental_file, :with_attached_file) }
+        let(:supplemental_file) { FactoryBot.create(:supplemental_file, :with_attached_file) }
         it 'should skip validation' do
-          expect(subject.valid?).to be_truthy
+          expect(supplemental_file.valid?).to be_truthy
         end
       end
       context 'VTT caption file' do
-        let(:subject) { FactoryBot.create(:supplemental_file, :with_caption_file, :with_caption_tag) }
+        let(:supplemental_file) { FactoryBot.create(:supplemental_file, :with_caption_file, :with_caption_tag) }
         it 'should validate' do
-          expect(subject.valid?).to be_truthy
+          expect(supplemental_file.valid?).to be_truthy
         end
       end
       context 'SRT caption file' do
-        let(:subject) { FactoryBot.create(:supplemental_file, :with_caption_srt_file, :with_caption_tag) }
+        let(:supplemental_file) { FactoryBot.create(:supplemental_file, :with_caption_srt_file, :with_caption_tag) }
         it 'should validate' do
-          expect(subject.valid?).to be_truthy
+          expect(supplemental_file.valid?).to be_truthy
         end
       end
       context 'non-VTT/non-SRT caption file' do
-        let(:subject) { FactoryBot.build(:supplemental_file, :with_attached_file, :with_caption_tag) }
+        let(:supplemental_file) { FactoryBot.build(:supplemental_file, :with_attached_file, :with_caption_tag) }
         it 'should not validate' do
-          expect(subject.valid?).to be_falsey
-          expect(subject.errors[:file_type]).not_to be_empty
+          expect(supplemental_file.valid?).to be_falsey
+          expect(supplemental_file.errors[:file_type]).not_to be_empty
         end
-      end
-    end
-
-    describe 'scopes' do
-      describe 'with_tag' do
-        let!(:subject) { FactoryBot.create(:supplemental_file, :with_transcript_file, tags: ['transcript', 'machine_generated']) }
-        let!(:other_transcript) { FactoryBot.create(:supplemental_file, :with_transcript_file, tags: ['transcript']) }
-        let!(:another_file) { FactoryBot.create(:supplemental_file, :with_caption_file, tags: ['caption']) }
-
-        it 'filters for a single tag' do
-          expect(SupplementalFile.with_tag('transcript')).to include(subject,other_transcript)
-          expect(SupplementalFile.with_tag('transcript').count).to eq 2
-        end
-
-        it 'filters for multiple tags' do
-          expect(SupplementalFile.with_tag('transcript').with_tag('machine_generated').first).to eq subject
-          expect(SupplementalFile.with_tag('transcript').with_tag('machine_generated').count).to eq 1
-        end
-      end
-    end
-
-    describe '#update_index' do
-      let(:transcript) { FactoryBot.build(:supplemental_file, :with_transcript_file, :with_transcript_tag) }
-      context 'on create' do
-        it 'triggers callback' do
-          expect(transcript).to receive(:index_file)
-          transcript.save
-        end
-
-        it 'indexes the transcript' do
-          transcript.save
-          solr_doc = ActiveFedora::SolrService.query("id:#{RSolr.solr_escape(transcript.to_global_id.to_s)}").first
-          expect(solr_doc["transcript_tsim"]).to eq ["00:00:03.500 --> 00:00:05.000 Example captions"]
-        end
-      end
-
-      context 'on update' do
-        let(:transcript) { FactoryBot.create(:supplemental_file, :with_transcript_file, :with_transcript_tag) }
-        it 'triggers callback' do
-          transcript.file.attach(fixture_file_upload(Rails.root.join('spec', 'fixtures', 'chunk_test.vtt'), 'text/vtt'))
-          expect(transcript).to receive(:update_index)
-          transcript.save
-        end
-
-        it 'updates the indexed transcript' do
-          before_doc = ActiveFedora::SolrService.query("id:#{RSolr.solr_escape(transcript.to_global_id.to_s)}").first
-          expect(before_doc["transcript_tsim"].first).to eq "00:00:03.500 --> 00:00:05.000 Example captions"
-          transcript.file.attach(fixture_file_upload(Rails.root.join('spec', 'fixtures', 'chunk_test.vtt'), 'text/vtt'))
-          transcript.save
-          after_doc = ActiveFedora::SolrService.query("id:#{RSolr.solr_escape(transcript.to_global_id.to_s)}").first
-          expect(after_doc["transcript_tsim"].first).to eq("00:00:01.200 --> 00:00:21.000 [music]")
-        end
-
-        context 'caption as transcript' do
-          let(:transcript) { FactoryBot.create(:supplemental_file, :with_caption_file, tags: ['caption', 'transcript']) }
-
-          it 'removes the transcript_tsim content when transcript tag is removed' do
-            before_doc = ActiveFedora::SolrService.query("id:#{RSolr.solr_escape(transcript.to_global_id.to_s)}").first
-            expect(before_doc["transcript_tsim"].first).to eq "00:00:03.500 --> 00:00:05.000 Example captions"
-            transcript.tags = ['caption']
-            transcript.save
-            after_doc = ActiveFedora::SolrService.query("id:#{RSolr.solr_escape(transcript.to_global_id.to_s)}").first
-            expect(after_doc["transcript_tsim"]).to be_nil
-          end
-        end
-      end
-    end
-
-    describe "#remove_from_index" do
-      let(:transcript) { FactoryBot.create(:supplemental_file, :with_transcript_file, :with_transcript_tag) }
-
-      context 'on delete' do
-        it 'triggers callback' do
-          expect(transcript).to receive(:remove_from_index)
-          transcript.destroy
-        end
-
-        it 'removes the transcript from the index' do
-          before_doc = ActiveFedora::SolrService.query("id:#{RSolr.solr_escape(transcript.to_global_id.to_s)}").first
-          expect(before_doc['transcript_tsim']).to eq ["00:00:03.500 --> 00:00:05.000 Example captions"]
-          transcript.destroy
-          after_doc = ActiveFedora::SolrService.query("id:#{RSolr.solr_escape(transcript.to_global_id.to_s)}").first
-          expect(after_doc).to be_nil
-        end
-      end
-    end
-
-    describe '#to_solr' do
-      let(:caption) { FactoryBot.create(:supplemental_file, :with_caption_file, :with_caption_tag) }
-      let(:transcript) { FactoryBot.create(:supplemental_file, :with_transcript_file, :with_transcript_tag) }
-      let(:caption_transcript) { FactoryBot.create(:supplemental_file, :with_caption_file, tags: ['caption', 'transcript']) }
-
-      it "should solrize transcripts" do
-        expect(transcript.to_solr[ "transcript_tsim" ]).to be_a Array
-        expect(transcript.to_solr[ "transcript_tsim" ][0]).to eq "00:00:03.500 --> 00:00:05.000 Example captions"
-        expect(caption_transcript.to_solr[ "transcript_tsim" ]).to be_a Array
-        expect(transcript.to_solr[ "transcript_tsim" ][0]).to eq "00:00:03.500 --> 00:00:05.000 Example captions"
-      end
-
-      it "should not solrize non-transcripts" do
-        expect(caption.to_solr[ "transcript_tsim" ]).to be nil
-      end
-    end
-
-    describe 'language' do
-      it 'should validate valid language' do
-        subject.language = 'eng'
-        expect(subject.valid?).to be_truthy
-      end
-      it 'should not validate invalid language' do
-        subject.language = 'engl'
-        expect(subject.valid?).to be_falsey
       end
     end
   end
 
+  describe 'scopes' do
+    describe 'with_tag' do
+      let!(:supplemental_file) { FactoryBot.create(:supplemental_file, :with_transcript_file, tags: ['transcript', 'machine_generated']) }
+      let!(:other_transcript) { FactoryBot.create(:supplemental_file, :with_transcript_file, tags: ['transcript']) }
+      let!(:another_file) { FactoryBot.create(:supplemental_file, :with_caption_file, tags: ['caption']) }
+
+      it 'filters for a single tag' do
+        expect(SupplementalFile.with_tag('transcript')).to include(supplemental_file, other_transcript)
+        expect(SupplementalFile.with_tag('transcript').count).to eq 2
+      end
+
+      it 'filters for multiple tags' do
+        expect(SupplementalFile.with_tag('transcript').with_tag('machine_generated').first).to eq supplemental_file
+        expect(SupplementalFile.with_tag('transcript').with_tag('machine_generated').count).to eq 1
+      end
+    end
+  end
+
+  describe '#attach_file' do
+    subject { described_class.new }
+
+    context 'via file upload' do   
+      let(:file) { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'meow.wav'))}
+      before { subject.attach_file(file) }
+
+      it 'attaches the file and assigns metadata' do
+        expect(subject.file).to be_attached
+        expect(subject.file.content_type).to eq 'audio/x-wav'
+        expect(subject.label).to eq 'meow.wav'
+        expect(subject.language).to eq Settings.caption_default.language
+      end
+    end
+
+    context 'via io attachment' do
+      let(:io_file) { Rails.root.join('spec', 'fixtures', 'meow.wav') }
+      before { subject.attach_file(io_file, io: true) }
+
+      it 'attaches the file and assigns metadata' do
+        expect(subject.file).to be_attached
+        expect(subject.file.content_type).to eq 'audio/x-wav'
+        expect(subject.label).to eq 'meow.wav'
+        expect(subject.language).to eq Settings.caption_default.language
+      end
+    end
+  end
+
+  describe '#update_index' do
+    let(:transcript) { FactoryBot.build(:supplemental_file, :with_transcript_file, :with_transcript_tag) }
+    context 'on create' do
+      it 'triggers callback' do
+        expect(transcript).to receive(:index_file)
+        transcript.save
+      end
+
+      it 'indexes the transcript' do
+        transcript.save
+        solr_doc = ActiveFedora::SolrService.query("id:#{RSolr.solr_escape(transcript.to_global_id.to_s)}").first
+        expect(solr_doc["transcript_tsim"]).to eq ["00:00:03.500 --> 00:00:05.000 Example captions"]
+      end
+    end
+
+    context 'on update' do
+      let(:transcript) { FactoryBot.create(:supplemental_file, :with_transcript_file, :with_transcript_tag) }
+      it 'triggers callback' do
+        transcript.file.attach(fixture_file_upload(Rails.root.join('spec', 'fixtures', 'chunk_test.vtt'), 'text/vtt'))
+        expect(transcript).to receive(:update_index)
+        transcript.save
+      end
+
+      it 'updates the indexed transcript' do
+        before_doc = ActiveFedora::SolrService.query("id:#{RSolr.solr_escape(transcript.to_global_id.to_s)}").first
+        expect(before_doc["transcript_tsim"].first).to eq "00:00:03.500 --> 00:00:05.000 Example captions"
+        transcript.file.attach(fixture_file_upload(Rails.root.join('spec', 'fixtures', 'chunk_test.vtt'), 'text/vtt'))
+        transcript.save
+        after_doc = ActiveFedora::SolrService.query("id:#{RSolr.solr_escape(transcript.to_global_id.to_s)}").first
+        expect(after_doc["transcript_tsim"].first).to eq("00:00:01.200 --> 00:00:21.000 [music]")
+      end
+
+      context 'caption as transcript' do
+        let(:transcript) { FactoryBot.create(:supplemental_file, :with_caption_file, tags: ['caption', 'transcript']) }
+
+        it 'removes the transcript_tsim content when transcript tag is removed' do
+          before_doc = ActiveFedora::SolrService.query("id:#{RSolr.solr_escape(transcript.to_global_id.to_s)}").first
+          expect(before_doc["transcript_tsim"].first).to eq "00:00:03.500 --> 00:00:05.000 Example captions"
+          transcript.tags = ['caption']
+          transcript.save
+          after_doc = ActiveFedora::SolrService.query("id:#{RSolr.solr_escape(transcript.to_global_id.to_s)}").first
+          expect(after_doc["transcript_tsim"]).to be_nil
+        end
+      end
+    end
+  end
+
+  describe "#remove_from_index" do
+    let(:transcript) { FactoryBot.create(:supplemental_file, :with_transcript_file, :with_transcript_tag) }
+
+    context 'on delete' do
+      it 'triggers callback' do
+        expect(transcript).to receive(:remove_from_index)
+        transcript.destroy
+      end
+
+      it 'removes the transcript from the index' do
+        before_doc = ActiveFedora::SolrService.query("id:#{RSolr.solr_escape(transcript.to_global_id.to_s)}").first
+        expect(before_doc['transcript_tsim']).to eq ["00:00:03.500 --> 00:00:05.000 Example captions"]
+        transcript.destroy
+        after_doc = ActiveFedora::SolrService.query("id:#{RSolr.solr_escape(transcript.to_global_id.to_s)}").first
+        expect(after_doc).to be_nil
+      end
+    end
+  end
+
+  describe '#to_solr' do
+    let(:caption) { FactoryBot.create(:supplemental_file, :with_caption_file, :with_caption_tag) }
+    let(:transcript) { FactoryBot.create(:supplemental_file, :with_transcript_file, :with_transcript_tag) }
+    let(:caption_transcript) { FactoryBot.create(:supplemental_file, :with_caption_file, tags: ['caption', 'transcript']) }
+
+    it "should solrize transcripts" do
+      expect(transcript.to_solr[ "transcript_tsim" ]).to be_a Array
+      expect(transcript.to_solr[ "transcript_tsim" ][0]).to eq "00:00:03.500 --> 00:00:05.000 Example captions"
+      expect(caption_transcript.to_solr[ "transcript_tsim" ]).to be_a Array
+      expect(transcript.to_solr[ "transcript_tsim" ][0]).to eq "00:00:03.500 --> 00:00:05.000 Example captions"
+    end
+
+    it "should not solrize non-transcripts" do
+      expect(caption.to_solr[ "transcript_tsim" ]).to be nil
+    end
+  end
+
+  describe 'language' do
+    it 'should validate valid language' do
+      supplemental_file.language = 'eng'
+      expect(supplemental_file.valid?).to be_truthy
+    end
+    it 'should not validate invalid language' do
+      supplemental_file.language = 'engl'
+      expect(supplemental_file.valid?).to be_falsey
+    end
+  end
+  
+
   it "stores no tags by default" do
-    expect(subject.tags).to match_array([])
+    expect(supplemental_file.tags).to match_array([])
   end
 
   context "with valid tags" do
-    let(:subject) { FactoryBot.create(:supplemental_file, :with_caption_file)}
+    let(:supplemental_file) { FactoryBot.create(:supplemental_file, :with_caption_file)}
     let(:tags) { ["transcript", "caption", "machine_generated"] }
 
     it "can store tags" do
-      subject.tags = tags
-      subject.save
-      expect(subject.reload.tags).to match_array(tags)
+      supplemental_file.tags = tags
+      supplemental_file.save
+      expect(supplemental_file.reload.tags).to match_array(tags)
     end
   end
 
@@ -178,17 +207,17 @@ describe SupplementalFile do
     let(:bad_tags) { ["unallowed"] }
 
     it "does not store tags" do
-      subject.tags = bad_tags
-      expect(subject.save).to be_falsey
-      expect(subject.errors.messages[:tags]).to include("unallowed is not an allowed value")
+      supplemental_file.tags = bad_tags
+      expect(supplemental_file.save).to be_falsey
+      expect(supplemental_file.errors.messages[:tags]).to include("unallowed is not an allowed value")
     end
   end
 
   context 'language' do
     it "can be edited" do
-      subject.language = 'ger'
-      subject.save
-      expect(subject.reload.language).to eq "ger"
+      supplemental_file.language = 'ger'
+      supplemental_file.save
+      expect(supplemental_file.reload.language).to eq "ger"
     end
   end
 

Gemfile
@@ -75,7 +75,7 @@ gem 'omniauth-lti', git: "https://github.com/avalonmediasystem/omniauth-lti.git"
 gem "omniauth-saml", "~> 2.0", ">= 2.2.3"
 
 # Media Access & Transcoding
-gem 'active_encode', '~> 1.2'
+gem 'active_encode', git: "https://github.com/samvera-labs/active_encode.git", branch: 'main'
 gem 'audio_waveform-ruby', '~> 1.0.7', require: 'audio_waveform'
 gem 'browse-everything', git: "https://github.com/avalonmediasystem/browse-everything.git", branch: 'v1.4-avalon'
 gem 'fastimage'

app/jobs/active_encode_jobs.rb
@@ -20,7 +20,7 @@ module ActiveEncodeJobs
       return unless MasterFile.exists? master_file_id
       master_file = MasterFile.find(master_file_id)
       return if master_file.workflow_id.present?
-      master_file.encoder_class.create(input, options.merge!(master_file_id: master_file_id, preset: master_file.workflow_name))
+      master_file.encoder_class.create(input, options.merge!(master_file_id: master_file_id, preset: master_file.workflow_name, extract_subtitles: true))
     end
   end
 

app/models/master_file.rb
@@ -318,11 +318,11 @@ class MasterFile < ActiveFedora::Base
     # We can get the proper aspect ratio from the transcoded files, so we set the master file off the 
     # encode output.
     if is_video?
-      high_output = Array(encode.output).select { |out| out.label.include?("high") }.first
+      high_output = Array(encode.output).select { |out| out.label&.include?("high") }.first
       self.display_aspect_ratio = (high_output.width.to_f / high_output.height.to_f).to_s
     end
 
-    outputs = Array(encode.output).collect do |output|
+    outputs = Array(encode.output).reject { |output| output.format == "vtt" }.collect do |output|
       {
         id: output.id,
         label: output.label,
@@ -339,6 +339,13 @@ class MasterFile < ActiveFedora::Base
       }
     end
     update_derivatives(outputs)
+
+    supplemental_file_outputs = Array(encode.output).select { |out| out.format == 'vtt' }
+    supplemental_file_ids = supplemental_file_outputs.collect { |sf| sf.id }.compact
+    add_supplemental_files(supplemental_file_ids) if supplemental_file_ids.present?
+
+    save
+
     run_hook :after_transcoding
   end
 
@@ -352,8 +359,10 @@ class MasterFile < ActiveFedora::Base
         existing.delete
       end
     end
+  end
 
-    save
+  def add_supplemental_files(ids)
+    self.supplemental_files += ids.collect { |id| GlobalID::Locator.locate(id) }
   end
 
   alias_method :'_poster_offset', :'poster_offset'

spec/factories/encode.rb
@@ -26,29 +26,36 @@ FactoryBot.define do
       state { :running }
       percent_complete { 50.5 }
       current_operations { ['encoding'] }
-      input { FactoryBot.build(:encode_output) }
+      input { FactoryBot.build(:encode_input) }
     end
       
     trait :succeeded do
       state { :completed }
       percent_complete { 100 }
       current_operations { ['DONE'] }
-      input { FactoryBot.build(:encode_output) }
+      input { FactoryBot.build(:encode_input) }
       output { [ FactoryBot.build(:encode_output) ] }
+    end
+
+    trait :embedded_captions do
+      state { :completed }
+      percent_complete { 100 }
+      current_operations { ['DONE'] }
+      input { FactoryBot.build(:encode_input) }
+      output { [ FactoryBot.build(:encode_output), FactoryBot.build(:encode_supplemental_output) ] }
     end
 
     trait :failed do
       state { :failed }
       percent_complete { 50.5 }
       current_operations { ['FAILED'] }
-      input { FactoryBot.build(:encode_output) }
+      input { FactoryBot.build(:encode_input) }
       errors { ['Out of disk space.'] }
     end
   end
 
   factory :encode_input, class: ActiveEncode::Input do
     id { SecureRandom.uuid }
-    label { 'quality-high' }
     url { 'file://path/to/output.mp4' }
     duration { '21575.0' }
     audio_bitrate { '163842.0' }
@@ -57,6 +64,7 @@ FactoryBot.define do
     video_codec { 'AVC' }
     width { '1024' }
     height { '768' }
+    subtitles { [] }
   end
 
   factory :encode_output, class: ActiveEncode::Output do
@@ -70,5 +78,12 @@ FactoryBot.define do
     video_codec { 'AVC' }
     width { '1024' }
     height { '768' }
+    subtitles { [] }
+  end
+
+  factory :encode_supplemental_output, class: ActiveEncode::Output do
+    id { "gid://avalon/SupplementalFile/1" }
+    url { "file://#{Rails.root.join('spec', 'fixtures', 'caption.vtt')}"}
+    format { "vtt" }
   end
 end

commit 78c0e95d27452bcf72f8622e35cd62b025f39be9

Refactor SupplementalFile#attach_file
Related issue: #6820

A bug was introduced in commit 079b20e when the `io` option was added to
the #attach_file method. This involved us passing the wrong type to the
method in the case of batch processing, causing batches containing
captions/transcripts to error out.

This commit refactors the `io` mode to utilize a FileLocator instance
with filename provided. This should allow us to be more intentional,
clear, and consistent with how the method is used.

app/models/supplemental_file.rb
@@ -34,10 +34,11 @@ class SupplementalFile < ApplicationRecord
   after_destroy_commit :remove_from_index
   before_save :default_label
 
+  # If using io: true, new_file MUST be a FileLocator instance initialized with the filename opt
   def attach_file(new_file, io: false)
     if io
-      file.attach(io: File.open(new_file), filename: File.basename(new_file))
-      extension = File.extname(new_file)
+      file.attach(io: new_file.reader, filename: new_file.filename)
+      extension = File.extname(new_file.filename)
     else
       file.attach(new_file)
       extension = File.extname(new_file.original_filename)

app/models/watched_encode.rb
@@ -44,7 +44,7 @@ class WatchedEncode < ActiveEncode::Base
               new_file.language = nil
             end
           end
-          new_file.attach_file(FileLocator.new(output.url).location, io: true)
+          new_file.attach_file(FileLocator.new(output.url, filename: File.basename(output.url)), io: true)
           new_file.save
           output.url = if Settings.active_storage.bucket.present?
                       "s3://#{Settings.active_storage.bucket}/#{new_file.file.blob.key}"

lib/avalon/batch/entry.rb
@@ -297,7 +297,7 @@ module Avalon
         machine_generated = Avalon::Batch.true_field?(datastream[:machine_generated]) ? 'machine_generated' : nil
         # Create SupplementalFile
         supplemental_file = SupplementalFile.new(label: label, tags: [type, treat_as_transcript, machine_generated].uniq.compact, language: language, parent_id: parent_id)
-        supplemental_file.attach_file(FileLocator.new(datastream[file_key]).reader, io: true)
+        supplemental_file.attach_file(FileLocator.new(datastream[file_key], filename: File.basename(datastream[file_key])), io: true)
         supplemental_file.save ? supplemental_file : nil
       end
       private_class_method :process_datastream

spec/models/supplemental_file_spec.rb
@@ -94,8 +94,8 @@ describe SupplementalFile do
     end
 
     context 'via io attachment' do
-      let(:io_file) { Rails.root.join('spec', 'fixtures', 'meow.wav') }
-      before { subject.attach_file(io_file, io: true) }
+      let(:io_file) { Rails.root.join('spec', 'fixtures', 'meow.wav').to_s }
+      before { subject.attach_file(FileLocator.new(io_file, filename: 'meow.wav'), io: true) }
 
       it 'attaches the file and assigns metadata' do
         expect(subject.file).to be_attached