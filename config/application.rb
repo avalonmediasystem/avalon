6a571af7089d3ef9100a50ac0f099c0ddbaf68a2
cjcolvar <chris.colvard@gmail.com>
2026-05-11 16:07:16 -0400

    Bump version to prepare for release

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ require 'resolv-replace'
 Bundler.require(*Rails.groups)
 
 module Avalon
-  VERSION = '8.1.1'
+  VERSION = '8.2.0'
 
   class Application < Rails::Application
     require 'avalon/configuration'
