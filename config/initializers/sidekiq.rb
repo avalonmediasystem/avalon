bc0d775dd6de3025b13ec96f54c87d8d568b8a02
cjcolvar <chris.colvard@gmail.com>
2026-05-28 11:59:07 -0400

    Run batch ingest notification job every minute

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -19,7 +19,7 @@ Rails.application.config.to_prepare do
     # Only create cron jobs if Sidekiq can connect to Redis
     Sidekiq.redis(&:info)
     Sidekiq::Cron::Job.create(name: 'Scan for batches - every 1min', cron: '*/1 * * * *', class: 'BatchScanJob')
-    Sidekiq::Cron::Job.create(name: 'Status Checking and Email Notification of Existing Batches - every 15min', cron: '*/15 * * * *', class: 'IngestBatchStatusEmailJobs::IngestFinished')
+    Sidekiq::Cron::Job.create(name: 'Status Checking and Email Notification of Existing Batches - every 1min', cron: '*/1 * * * *', class: 'IngestBatchStatusEmailJobs::IngestFinished')
     Sidekiq::Cron::Job.create(name: 'Status Checking and Email Notification for Stalled Batches - every 1day', cron: '0 1 * * *', class: 'IngestBatchStatusEmailJobs::StalledJob')
     Sidekiq::Cron::Job.create(name: 'Clean out user sessions older than 7 days - every 6hour', cron: '0 */6 * * *', class: 'CleanupSessionJob')
     Sidekiq::Cron::Job.create(name: 'Clean out expired stream tokens - every 5min', cron: '*/5 * * * *', class: 'CleanupStreamTokenJob')
