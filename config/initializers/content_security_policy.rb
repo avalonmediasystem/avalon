4252fd19fce8c43a2314bb06653789ea1d4b9948
Mason Ballengee <masaball@iu.edu>
2026-08-11 17:49:40 -0400

    Switch to mini-css-extract-plugin for style loading

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -9,8 +9,8 @@ Rails.application.configure do
     # Rails defaults
     policy.default_src :self, :https
     policy.font_src    :self, :https, :data
-    policy.img_src     :self, :https, :data
     policy.object_src  :none
+    policy.style_src   :self, :https
     # Specify URI for violation reports
     # policy.report_uri "/csp-violation-report-endpoint"
 
@@ -18,29 +18,29 @@ Rails.application.configure do
     # JQuery requires this for compatibility with Firefox
     # TODO: Test removal after JQuery fully excised
     policy.script_src_attr :unsafe_inline
-    # There are many inline styles that are difficult, or unable, to provide
-    # the nonce value for. Leave unsafe for now.
-    # TODO: Clean up inline styles and provide nonce for any that cant be changed
-    policy.style_src   :unsafe_inline, :self, :https
     # Blob required for media streaming. worker_src from firefox, media_src from chrome
     policy.media_src   :self, :https, :blob
     policy.worker_src  :self, :https, :blob
+    # Blob required for image upload/cropping
+    policy.img_src     :self, :https, :data, :blob
     if Rails.env.development?
-      # :http is set for convenience in dev environment.
-      policy.connect_src :self, :http
+      # :http is set for convenience in dev environment. :blob required for poster upload
+      policy.connect_src :self, :http, :blob
       # unsafe_eval is necessary for dev environment. DO NOT enable in prod.
       policy.script_src  :self, :https, :unsafe_eval
+      # react-dom-client uses unsafe inlines for dev env
+      policy.style_src_attr :unsafe_inline
     else
-      policy.connect_src :self, :https
+      # blob required for poster image upload
+      policy.connect_src :self, :https, :blob
       policy.script_src  :self, :https
     end
   end
 
   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-  # TODO: Assess feasibility of adding stlye-src to nonce directives
-  config.content_security_policy_nonce_directives = %w(script-src)
+  config.content_security_policy_nonce_directives = %w(script-src style-src)
 
   # Report violations without enforcing the policy.
-  # config.content_security_policy_report_only = true
+  config.content_security_policy_report_only = true
 end
