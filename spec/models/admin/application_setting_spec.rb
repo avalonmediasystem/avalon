b9ae2c9b2847e708b3009d6bf01f6edcdda45ccc
Mason Ballengee <masaball@iu.edu>
2026-09-11 16:23:52 -0400

    Add tests for Application Settings

--- spec/models/admin/application_setting_spec.rb
+++ spec/models/admin/application_setting_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Admin::ApplicationSetting, type: :model do
+RSpec.describe Admin::ApplicationSetting, type: :model, skip_stubbing: true do
   describe 'validations' do
     it 'enforces singleton_guard == 0' do
       expect(described_class.new(singleton_guard: 1).save).to be_falsey
@@ -17,6 +17,19 @@ RSpec.describe Admin::ApplicationSetting, type: :model do
       setting = described_class.create
       expect { described_class.instance }.to_not change { described_class.count }
       expect(described_class.instance).to eq setting
+    end
+  end
+
+  describe 'data security' do
+    it 'encrypts the settings blob' do
+      settings = described_class.create
+      decrypted = described_class.instance.options
+      expect(decrypted).to eq settings.options
+
+      db_value = settings.read_attribute_before_type_cast(:options)
+      expect(settings.type_for_attribute(:options)).to be_a(ActiveRecord::Encryption::EncryptedAttributeType)
+      expect(db_value).to_not eq settings.options
+      expect(db_value).to include('{"p":')
     end
   end
 
@@ -40,7 +53,7 @@ RSpec.describe Admin::ApplicationSetting, type: :model do
       end
 
       it 'raises an error if the setting does not exist' do
-        expect { subject.mint }.to raise_error
+        expect { subject.mint }.to raise_error NoMethodError
       end
     end
   end
